9bc1970b0f1acbd246e8a0ec3047ea2ab45cc5eb
Odilitime <janesmith@airmail.cc>
2020-04-28 06:31:17 +0000

    sample modules

---
+++ backend/modules/anonname.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+// on new post event
+
+// so we have to know this board
+// are we enabled for this board?
+
+// is name set
+// if not
+// get anon name
+// replace name with our anon name
+
+*/
+
+/*
+hook BO options
+add anonymous name textbox
+write value here..
+*/
+?>
---
+++ backend/modules/early404.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+// on new post event
+
+// so we have to know this board
+// are we enabled for this board?
+
+// has board hit thread limit
+
+// how many threads on this board have less than 5 replies
+//   in the first third
+*/
+
+/*
+hook BO options
+add early 404 checkbox
+write value here..
+*/
+?>
