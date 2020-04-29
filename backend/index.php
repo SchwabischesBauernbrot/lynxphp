35e5b4d8a90ce3030d412b6529f7d7ec95be5c10
Odilitime <janesmith@airmail.cc>
2020-04-29 10:45:27 +0000

    start flushing out some endpoints

--- backend/index.php
+++ backend/index.php
@@ -8,4 +8,16 @@
 
 // read config
 
+// 4chan GET board threads
+// GET random board uri
+// GET board list (pageinated)
+// GET board thread list
+// GET thread
+// GET last X posts from thread
+// POST board search
+// POST thread search
+// POST thread/reply
+
+// how to handle authentication with frontend?
+
 ?>
