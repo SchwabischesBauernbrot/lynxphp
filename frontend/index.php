09b13adf122de6f709dd5724be59c0e5a1d56d27
Odilitime <janesmith@airmail.cc>
2020-04-29 10:45:49 +0000

    start flushing out endpoints

--- frontend/index.php
+++ frontend/index.php
@@ -4,7 +4,22 @@
 
 // set up backend url, cache
 
+// if OPTIONS
+
+// if POST
+
+// dispatch form data through post processing pipeline
+// well initially I think we'll just have the form post to the backend directly
+
+// if GET
 // determine page from PHP_INFO
+
+// interpolate boards list with template
+// interpolate board threads list with index template
+// interpolate board threads list with catalog template
+
+// interpolate board search list with board list template
+// interpolate thread search list with template
 
 // serve site
 
