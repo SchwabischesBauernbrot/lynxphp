15841b280af37959f6aee37b7dea4b543b3425e9
Odilitime <janesmith@airmail.cc>
2020-04-29 10:46:11 +0000

    createBoardSetting()

--- backend/lib/lib.board.php
+++ backend/lib/lib.board.php
@@ -4,6 +4,9 @@
 getBoardByUri($uri)
 getBoardSetting($field)
 
+// collect all these events don't need to put into a database
+createBoardSetting($field, $value)
+
 getBoardsWith($field, $value)
 getBoardSettingForm($uri)
 
