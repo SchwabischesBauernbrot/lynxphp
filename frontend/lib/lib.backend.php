f6828fddc271c753bab4cab8cb8b7a71f5cd906c
Odilitime <janesmith@airmail.cc>
2020-11-22 19:41:07 +0000

    sendFile

--- frontend/lib/lib.backend.php
+++ frontend/lib/lib.backend.php
@@ -24,4 +24,12 @@ function getBoardThread($boardUri, $threadNum) {
   return $result['posts'];
 }
 
+function sendFile($tmpfile, $type, $filename) {
+  $json  = curlHelper(BACKEND_BASE_URL . 'lynx/files', array(
+    'files' => curl_file_create($tmpfile, $type, $filename)
+  ), '', '', '', 'POST');
+  $result = json_decode($json, true);
+  return $result['data'];
+}
+
 ?>
