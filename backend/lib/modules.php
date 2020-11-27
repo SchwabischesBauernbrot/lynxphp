5d9c393d12d600da08293c5af276c47d0169e31c
Odilitime <janesmith@airmail.cc>
2020-11-27 15:48:34 +0000

    make module_registry not a singleton, orderable_module::__construct

--- backend/lib/modules.php
+++ backend/lib/modules.php
@@ -8,24 +8,28 @@ class module_registry {
   var $instance;
 
   /** make sure it's a singleton */
-  protected function __construct() {
+  /*protected */
+  function __construct() {
     $this->name  = get_class($this);
     $this->registry = array();
   }
 
   /** make sure it can't be cloned */
-  protected function __clone() {}
+  //protected function __clone() {}
 
   /** make sure it can't be unserialized */
+  /*
   public function __wakeup(){
     throw new Exception("Cannot unserialize singleton");
   }
+  */
 
   /**
    * php singleton hack copy this into the child class
    *
    * @returns string current class name
    */
+  /*
   static function singleton() {
     static $instance; // think like a global that doesn't leave this function
     if (!isset($instance)) {
@@ -33,6 +37,8 @@ class module_registry {
     }
     return $instance;
   }
+  */
+
   /*
   // php singleton hack put this in the child class
   function singleton() {
@@ -125,11 +131,87 @@ class module_registry {
     }
   }
 
+  function findAllNoDeps() {
+    return array_filter($this->registry, function ($m) {
+      return !(count($m->dependencies) || count($m->preempt));
+    });
+  }
+
+  function findPrereqs() {
+    return array_filter($this->registry, function ($m) {
+      return count($m->dependencies);
+    });
+  }
+  function findPostreqs() {
+    return array_filter($this->registry, function ($m) {
+      return count($m->preempt);
+    });
+  }
+
+  function expand_prerequirements($dep) {
+    $deps = $dep->dependencies;
+    foreach($dep->dependencies as $d) {
+      $newDeps = $this->expand_prerequirements($d);
+      $deps = array_merge($deps, $newDeps);
+    }
+    $deps = array_unique($deps);
+    return $deps;
+  }
+
+  function expand_preempt($srcMod) {
+    $prempts = $srcMod->preempt;
+    foreach($srcMod->preempt as $mod) {
+      $newPreempts = $this->expand_prequirements($mod);
+      $prempts = array_merge($prempts, $newPreempts);
+    }
+    $prempts = array_unique($prempts);
+    return $prempts;
+  }
+
+  function expand($name) {
+    $m = $this->registry[$name];
+    $expMod = array(
+      'prereq' => $this->expand_prequirements($m),
+      'preempt' => $this->expand_preempt($m),
+    );
+    // now within this scope
+    // any problems we can't resolve?
+    $clean = true;
+    foreach($expMod['prereq'] as $name) {
+      if (in_array($name, $expMod['preempt'])) {
+        $clean = false;
+        break;
+      }
+    }
+    if ($clean) {
+      foreach($expMod['preempt'] as $name) {
+        if (in_array($name, $expMod['prereq'])) {
+          $clean = false;
+          break;
+        }
+      }
+    }
+    if ($clean) {
+      return $expMod;
+    }
+
+    // fix internal ordering...
+
+    return $expMod;
+  }
+
   function resolve_all() {
     $list = array();
-    for($this->registry as $name => $obj) {
+    foreach($this->registry as $name => $obj) {
       $this->resolve($name, $obj, $list);
     }
+  }
+
+  function compile() {
+    $this->resolve_all();
+  }
+
+  function exec() {
   }
 }
 
@@ -140,9 +222,13 @@ class ui_registry extends module_registry {
 }
 
 class orderable_module {
-  var dependencies; // these have to be completed
-  var preempt; // I must be before these modules
-  var name; // what my name is
+  var $dependencies; // these have to be completed
+  var $preempt; // I must be before these modules
+  var $name; // what my name is
+  function __construct() {
+    $this->dependencies = array();
+    $this->preempt      = array();
+  }
 }
 
 // public base modules that modules can extend
