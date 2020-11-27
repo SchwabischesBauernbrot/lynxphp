ab6e06a122a706e72e21209b1ca2ef36672b3314
Odilitime <janesmith@airmail.cc>
2020-11-27 21:11:38 +0000

    backendLynxAccount(), backendCreateBoard(), backendLogin(), notes

--- frontend/lib/lib.backend.php
+++ frontend/lib/lib.backend.php
@@ -1,5 +1,8 @@
 <?php
 
+// is this file going to get huge
+// should we specialize or tie to handler?
+
 function getBoards() {
   $json   = curlHelper(BACKEND_BASE_URL . '4chan/boards.json');
   $boards = json_decode($json, true);
@@ -8,13 +11,16 @@ function getBoards() {
 
 function getBoard($boardUri) {
   $json      = curlHelper(BACKEND_BASE_URL . '4chan/' . $boardUri . '.json');
+  //echo "getBoardJson[$json]<br>\n";
   $boardData = json_decode($json, true);
   return $boardData;
 }
 
 function getBoardPage($boardUri, $page = 1) {
   $json  = curlHelper(BACKEND_BASE_URL . '4chan/' . $boardUri . '/' . $page . '.json');
+  //echo "getBoardPageJson[$json]<br>\n";
   $page1 = json_decode($json, true);
+  //print_r($page1);
   return $page1;
 }
 
@@ -38,5 +44,57 @@ function sendFile($tmpfile, $type, $filename) {
   $result = json_decode($json, true);
   return $result['data'];
 }
+
+// authed functions
+
+function backendAuthedGet($endpoint) {
+  if (!isset($_COOKIE['session'])) {
+    return json_encode(array('meta'=>array('code'=>401)));
+  }
+  $json   = curlHelper(BACKEND_BASE_URL . $endpoint, '',
+    array('sid' => $_COOKIE['session']));
+  return $json;
+}
+
+function checkSession() {
+  $json = backendAuthedGet('opt/session');
+  $ses = json_decode($json, true);
+  return $ses;
+}
+
+function backendLogin() {
+  $user  = $_POST['username'];
+  $pass  = $_POST['password'];
+  // login, password, email
+  $data = curlHelper(BACKEND_BASE_URL . 'lynx/login', array(
+    'login'    => $user,
+    'password' => $pass,
+  ), array('HTTP_X_FORWARDED_FOR' => getip()));
+  echo "data[$data]<br>\n";
+  $res = json_decode($data, true);
+  if (isset($res['data']['session']) && $res['data']['session']) {
+    setcookie('session', $res['data']['session'], $res['data']['ttl'], '/');
+    //redirectTo('control_panel.php');
+    return true;
+  }
+  return $res['meta'];
+}
+
+function backendCreateBoard() {
+  $data = curlHelper(BACKEND_BASE_URL . 'lynx/createBoard', array(
+    'boardUri'         => $_POST['uri'],
+    'boardName'        => $_POST['title'],
+    'boardDescription' => $_POST['description'],
+    // captcha?
+  ), array('sid' => $_COOKIE['session']));
+  return $data;
+}
+
+function backendLynxAccount() {
+  $json   = backendAuthedGet('lynx/account');
+  $account = json_decode($json, true);
+  return $account;
+}
+
 
 ?>
