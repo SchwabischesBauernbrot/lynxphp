9cecaa4039aec174a8869ea9e26704fde91576aa
Odilitime <janesmith@airmail.cc>
2020-11-29 23:26:44 +0000

    router now returns a router, use getCookie, banners module, notes, move logout down

--- frontend/index.php
+++ frontend/index.php
@@ -7,54 +7,107 @@ include 'config.php';
 
 // if OPTIONS
 
-// if POST
-
 // dispatch form data through post processing pipeline
 // well initially I think we'll just have the form post to the backend directly
+// couldn't do that because we need to navigate the user to the correct place
+// well the backend could...
 
-// if GET
-// determine page from PHP_INFO
+$router = include '../common/router.php';
 
-// interpolate boards list with template
-// interpolate board threads list with index template
-// interpolate board threads list with catalog template
-
-// interpolate board search list with board list template
-// interpolate thread search list with template
-
-// serve site
-
-include '../common/router.php';
 include '../common/post_vars.php';
-$router = new Router;
+if (!defined('BASE_HREF')) {
+  define('BASE_HREF', dirname(getServerField('SCRIPT_NAME', __FILE__)) . '/');
+}
+
+// nav, pages
+// routes make a page exist
+// but a page could have multiple routes
+// so page is like a handler...
+// an each page/handler needs a static page output...
+  // so there's some magic between apache/nginx access and non-rewrite...
+// and then a handler has queries, templates and transformations...
+// but page as a concept; homepage, boards, board page, thread page etc
+// page specific nav?
+
+// also page's content could be a nav...
+// so nav is just a data-driven template element
+// so queries are more than db/backend, but general arrays too
+// so routers <=> handle mappings can get crazy
+// and then the links themselves in the templates...
+
+// and then there's the js side vs non-js side
+// js settings...
+//
+
+include '../common/lib.modules.php'; // module functions and classes
+// pipelines...
+// - content page?
+// template pipelines
+// - siteNav (logged in vs logged out?)
+// - boardNav
+// - threadNav
+// - postNav
+// - panelNav
+// - catalog tile
+// - page tmpl
+// - thread tmpl
+// - reply tmpl
+// - boardListing
+// - board search
+// - boardSettingTmpl
+$pipelines = array(
+  'boardHeaderTmpl' =>new pipeline_registry,
+  'boardNav' => new pipeline_registry,
+  'boardSettingNav' => new pipeline_registry,
+  'boardSettingTmpl' => new pipeline_registry,
+  'boardDetailsTmpl' => new pipeline_registry,
+);
+// forms pipelines
+// - newThreadForm
+// - newReplyForm
+// - boardSettingsForm
+// handler pipelines (pipelines creating pipelines)
+// well maybe each module should leave it's own pipeline?
+// but what consequences does that mean for the eco-system...
+// - login
+// - logout
 
 // frontend libraries
-include 'lib/lib.cache.php';
-include 'lib/lib.http.php';
-include 'lib/lib.handler.php';
-include 'lib/lib.backend.php';
+include 'lib/lib.cache.php'; // memoization functions
+include 'lib/lib.http.php'; // comms lib
+include 'lib/lib.backend.php'; // comms lib
+include 'lib/lib.handler.php'; // output functions
+// structures
+include 'lib/nav.php'; // nav structure
+include 'lib/middlewares.php';
+
 
 // frontend handlers
+include 'handlers/mixins/board_header.php';
+include 'handlers/mixins/board_nav.php';
+
 include 'handlers/homepage.php';
 include 'handlers/login.php';
 include 'handlers/signup.php';
 include 'handlers/control_panel.php';
 include 'handlers/boards.php';
 
-if (!defined('BASE_HREF')) {
-  define('BASE_HREF', dirname($_SERVER['SCRIPT_NAME']) . '/');
-}
+// FIXME: function('board/banners');
+include '../common/modules/board/banners/frontend_handlers.php';
 
 // should a handler set a variables (data structure)
 // or define a set of functions
 // functions can be variables...
 
+// FIXME: we should be getting page conent and wrapping it here...
+// FIXME: move into routes and the caching layer can go here too
 $router->get('', function() {
   homepage();
 });
 $router->get('/boards.php', function() {
   getBoardsHandler();
 });
+// FIXME: move into module
 $router->get('/overboard.php', function() {
   getOverboardHandler();
 });
@@ -76,10 +129,6 @@ $router->get('/:uri/catalog', function($request) {
 $router->get('/:uri/settings', function($request) {
   $boardUri = $request['params']['uri'];
   getBoardSettingsHandler($boardUri);
-});
-$router->get('/:uri/banners', function($request) {
-  $boardUri = $request['params']['uri'];
-  getBoardBannerHandler($boardUri);
 });
 
 $router->get('/:uri/thread/:num', function($request) {
@@ -124,8 +173,8 @@ $router->post('/:uri/post', function($request) {
       'spoiler'  => empty($_POST['spoiler_all']) ? '' : $_POST['spoiler_all'],
       'files'    => json_encode($files),
       // flag
-    ), array('HTTP_X_FORWARDED_FOR' => getip(), 'sid' => $_COOKIE['session']));
-    echo "json[$json]<Br>\n";
+    ), array('HTTP_X_FORWARDED_FOR' => getip(), 'sid' => getCookie('session')));
+    //echo "json[$json]<Br>\n";
     $result = json_decode($json, true);
     if (is_numeric($result['data'])) {
       // success
@@ -148,8 +197,8 @@ $router->post('/:uri/post', function($request) {
       'spoiler'  => empty($_POST['spoiler_all']) ? '' : $_POST['spoiler_all'],
       // flag
       'files'    => json_encode($files),
-    ), array('HTTP_X_FORWARDED_FOR' => getip(), 'sid' => $_COOKIE['session']));
-    echo "json[$json]<Br>\n";
+    ), array('HTTP_X_FORWARDED_FOR' => getip(), 'sid' => getCookie('session')));
+    //echo "json[$json]<Br>\n";
     $result = json_decode($json, true);
     if (is_numeric($result['data'])) {
       // success
@@ -159,8 +208,6 @@ $router->post('/:uri/post', function($request) {
     }
   }
 });
-
-
 
 $router->get('/signup.php', function() {
   getSignup();
@@ -177,14 +224,15 @@ $router->post('/login.php', function() {
 $router->get('/control_panel.php', function() {
   getControlPanel();
 });
-$router->get('/logout.php', function() {
-  getLogout();
-});
 $router->get('/create_board.php', function() {
   getCreateBoard();
 });
 $router->post('/create_board.php', function() {
   postCreateBoard();
+});
+
+$router->get('/logout.php', function() {
+  getLogout();
 });
 
 // needs to go last...
@@ -195,5 +243,4 @@ $router->get('/:uri', function($request) {
 });
 
 $router->exec(getServerField('REQUEST_METHOD', 'GET'), getServerField('PATH_INFO'));
-
 ?>
