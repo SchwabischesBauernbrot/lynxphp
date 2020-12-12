a8cffe763364e86618f76c9bad20b5bbc7a75446
Odilitime <janesmith@airmail.cc>
2020-12-12 19:45:04 +0000

    abort if exit code isn't 0

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,17 @@
 pwd
 cd backend
 php index.php
+retVal=$?
+echo $retVal
+if [ $retVal -ne 0 ]; then
+  echo "Backend Failed"
+  exit $retVal
+fi
 cd ../frontend
 php index.php
+retVal=$?
+echo $retVal
+if [ $retVal -ne 0 ]; then
+  echo "Frontend Failed"
+  exit $retVal
+fi
