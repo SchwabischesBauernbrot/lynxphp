a58cb68f047e550786d573543db18aab92e62d99
Odilitime <janesmith@airmail.cc>
2020-12-12 05:42:31 +0000

    add fe handlers

---
+++ common/modules/board/banners/fe/handlers/public_list.php
@@ -0,0 +1,38 @@
+<?php
+
+// FIXME: we need access to package
+$params = $get();
+
+// $boardData = boardOwnerMiddleware($request);
+
+$boardUri = $request['params']['uri'];
+global $beRrsc_list;
+$call = $beRrsc_list;
+// FIXME:
+$call['endpoint'] .= '?boardUri=' . $boardUri;
+$banners = consume_beRsrc($call, array('boardUri' => $boardUri));
+
+$templates = moduleLoadTemplates('banner_listing', __DIR__);
+
+$tmpl = $templates['header'];
+$banner_tmpl = $templates['loop0'];
+$boardData = getBoard($boardUri);
+
+// FIXME: portal middleware?
+$boardHeader_html = renderBoardHeader($boardData);
+$boardNav_html = renderBoardNav($boardUri, $boardData['pageCount'], '[Banners]');
+$tmpl = $boardHeader_html . $boardNav_html . $tmpl;
+
+$banners_html = '';
+foreach($banners as $banner) {
+  $tmp = $banner_tmpl;
+  $tmp = str_replace('{{backend}}', 'backend', $tmp);
+  $tmp = str_replace('{{uri}}', $boardUri, $tmp);
+  $tmp = str_replace('{{id}}', $banner['bannerid'], $tmp);
+  $tmp = str_replace('{{image}}', $banner['image'], $tmp);
+  $banners_html .= $tmp;
+}
+$tmpl = str_replace('{{uri}}', $boardUri, $tmpl);
+$tmpl = str_replace('{{banners}}', $banners_html, $tmpl);
+wrapContent($tmpl);
+?>
---
+++ common/modules/board/banners/fe/handlers/settings_list.php
@@ -0,0 +1,40 @@
+<?php
+
+// FIXME: we need access to package
+$params = $get();
+
+// do we own this board?
+$boardUri = boardOwnerMiddleware($request);
+if (!$boardUri) return;
+// get a list of banners from backend
+
+global $beRrsc_list;
+$call = $beRrsc_list;
+// FIXME:
+$call['endpoint'] .= '?boardUri=' . $boardUri;
+$banners = consume_beRsrc($call, array('boardUri' => $boardUri));
+
+$templates = moduleLoadTemplates('banner_listing', __DIR__);
+
+// FIXME: include board header...
+// FIXME: include paged board nav...
+
+$header = $templates['header'];
+$banner_tmpl = $templates['loop1'];
+$tmpl = str_replace('{{banners}}', $header, $templates['loop2']);
+// add link
+// list
+$banners_html = '';
+foreach($banners as $banner) {
+  $tmp = $banner_tmpl;
+  $tmp = str_replace('{{backend}}', 'backend', $tmp);
+  $tmp = str_replace('{{uri}}', $boardUri, $tmp);
+  $tmp = str_replace('{{id}}', $banner['bannerid'], $tmp);
+  $tmp = str_replace('{{image}}', $banner['image'], $tmp);
+  $banners_html .= $tmp;
+}
+$tmpl = str_replace('{{uri}}', $boardUri, $tmpl);
+$tmpl = str_replace('{{banners}}', $banners_html, $tmpl);
+wrapContent($tmpl);
+
+?>
