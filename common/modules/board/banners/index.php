d326b6e35b3afb35c9768056ea975b625a5ba027
Odilitime <janesmith@airmail.cc>
2020-12-12 05:36:26 +0000

    pass dir to constructor, add addResource()s

--- common/modules/board/banners/index.php
+++ common/modules/board/banners/index.php
@@ -1,6 +1,54 @@
 <?php
 
-$package = new package('board_banners', 1);
+$package = new package('board_banners', 1, __dir__);
+
+// Lynx:
+// bannerManagement
+// createBanners
+// deleteBanner
+// randomBanner
+
+// frontend usually routes wrap around these...
+// so we can't just add more frontend resources
+// we need to attach a frontend to it?
+// and we don't need frontend attachments here...
+// there could be some benefits of documenting the frontend routes here...
+
+$package->addResource('random', array(
+  'endpoint' => 'lynx/randomBanner',
+  'unwrapData' => true,
+  'requires' => array(
+    'boardUri'
+  ),
+));
+
+$package->addResource('list', array(
+  'endpoint' => 'lynx/bannerManagement',
+  'unwrapData' => true,
+  'requires' => array(
+    'boardUri'
+  ),
+));
+
+$package->addResource('add', array(
+  'endpoint'    => 'lynx/createBanners',
+  'method'      => 'POST',
+  'sendSession' => true,
+  'unwrapData'  => true,
+  'requires'    => array(
+    'boardUri'
+  ),
+));
+
+$package->addResource('del', array(
+  'endpoint'    => 'lynx/deleteBanner',
+  'method'      => 'POST',
+  'sendSession' => true,
+  'unwrapData'  => true,
+  'requires'    => array(
+    'bannerId'
+  ),
+));
 
 return $package;
 
