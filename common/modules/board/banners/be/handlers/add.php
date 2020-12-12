ca84c1c5e0a2c71ce4ba05220c8641ed18ae7954
Odilitime <janesmith@airmail.cc>
2020-12-12 05:41:04 +0000

    add be handlers

---
+++ common/modules/board/banners/be/handlers/add.php
@@ -0,0 +1,31 @@
+<?php
+$params = $get();
+
+$boardUri = boardOwnerMiddleware($request);
+global $db, $models;
+// FIXME: validation that there is an upload...
+// handle file uploads...
+$bannersDir = 'storage/boards/' . $boardUri . '/banners';
+if (!file_exists($bannersDir)) {
+  mkdir($bannersDir);
+}
+$file = $bannersDir . '/' . basename($_FILES['files']['tmp_name']);
+move_uploaded_file($_FILES['files']['tmp_name'], $file);
+
+$boardData = getBoardByUri($boardUri);
+$sizes = getimagesize($file);
+
+$id = $db->insert($models['board_banner'], array(array(
+  'board_id' => $boardData['boardid'],
+  'image' => $file,
+  'w' => $sizes[0],
+  'h' => $sizes[1],
+  'weight' => 1,
+)));
+
+sendResponse(array(
+  'id'   => $id,
+  'path' => $file,
+));
+
+?>
