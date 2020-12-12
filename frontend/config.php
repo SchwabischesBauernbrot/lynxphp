a0298a933d63a7b5d440ab727bae90fce6a03cf6
Odilitime <janesmith@airmail.cc>
2020-12-12 19:53:43 +0000

    add BASE_HREF default

--- frontend/config.php
+++ frontend/config.php
@@ -11,10 +11,14 @@ if (file_exists($localConfig)) {
 
 // site wide config
 
-// backend_url
 
 // local server config
+
+// backend_url
 // needs to be HTTPS if the backend is not on the same server
 // must have trailing slash
 if (!defined('BACKEND_BASE_URL')) define('BACKEND_BASE_URL', 'http://localhost/backend/');
+
+// what request path is the site design to run under
+if (!defined('BASE_HREF')) define('BASE_HREF', '/');
 ?>
