a9130fb588592821f96b402cf979b99ed201d851
Odilitime <janesmith@airmail.cc>
2020-12-12 05:49:01 +0000

    flush out portal

--- frontend/lib/nav.php
+++ frontend/lib/nav.php
@@ -18,4 +18,67 @@ function getNav($navItems, $replaces, $selected = '', $list = true) {
   return $nav_html;
 }
 
+/*
+$portal = array(
+  'header'=>array(
+    'file => '',
+    // tag => code/constant
+    'replaces' => array(),
+    'nav' => array(
+      'items' => array(),
+      'replaces' => array(),
+      'selected' => '',
+      'displayOpts' => array(
+        'list' => true
+      )
+    )
+  ),
+  'footer'=>array(
+    'file => '',
+    'replaces' => array(),
+  ),
+);
+*/
+
+/*
+function renderPortal($portal) {
+  foreach($portal as $name => $section) {
+    renderSection($section);
+  }
+}
+
+function renderSection($section) {
+  $templates = loadTemplatesFile($section['file']);
+}
+*/
+
+// name is a lookup and part of what we're stored in (we store these objects in)
+class portal {
+  // little touch
+  function __construct($header_template, $footer_template, $nav) {
+    // even though we're sure we need to read both of these files
+    // 2 files to keep the memory footprint down
+    $this->headerTemplateFile = $header_template;
+    $this->footerTemplateFile = $footer_template;
+  }
+
+  function renderHeader() {
+    $templates = loadTemplatesFile($wrapper_template);
+    $template = $templates['header'];
+    // process template
+    echo $template;
+    unset($template); // free memory
+
+    $navTemplate = $templates['loop0'];
+    // process
+    echo $navTemplate;
+  }
+
+  function renderFooter() {
+    $template = file_get_contents($this->footerTemplateFile);
+    // process template
+    echo $template;
+  }
+}
+
 ?>
