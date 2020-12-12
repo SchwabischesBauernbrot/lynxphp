ba479dad547bc0253c198fc8a45d98549f6302dd
Odilitime <janesmith@airmail.cc>
2020-12-12 05:40:50 +0000

    add model using addModel, move out handlers

--- common/modules/board/banners/be/index.php
+++ common/modules/board/banners/be/index.php
@@ -1,82 +1,29 @@
 <?php
 
-// Lynx:
-// bannerManagement
-// createBanners
-// deleteBanner
-// randomBanner
-
 // pipelines:
 // - boardData
+
+//$bePkg = new backend_package($this);
+$bePkg = $this->makeBackend();
+
+
+$bePkg->addModel(array(
+  'name'   => 'board_banner',
+  'fields' => array(
+    'board_id' => array('type' => 'int'),
+    'image'    => array('type' => 'str'),
+    'w'        => array('type' => 'int'),
+    'h'        => array('type' => 'int'),
+    'weight'   => array('type' => 'int'),
+  )
+));
 
 $module = new pipeline_module('board_banners');
 $module->dependencies = array();
 $module->preempt      = array();
+// is this needed?
+// well we could inject this data into some other endpoints...
 $module->attach('boardData', function(&$row) {
-});
-
-// backend handlers...
-global $routers;
-$routers['lynx']->get('/randomBanner', function($request) {
-  $boardData = boardMiddleware($request);
-  global $db, $models;
-  $res = $db->find($models['board_banner'], array('criteria' => array(
-    array('board_id', '=', $boardData['boardid']),
-  )));
-  $banners = $db->toArray($res);
-  shuffle($banners);
-  sendResponse($banners[0]);
-});
-
-$routers['lynx']->get('/bannerManagement', function($request) {
-  $boardData = boardMiddleware($request);
-  global $db, $models;
-  $res = $db->find($models['board_banner'], array('criteria' => array(
-    array('board_id', '=', $boardData['boardid']),
-  )));
-  $banners = $db->toArray($res);
-  sendResponse($banners);
-});
-
-$routers['lynx']->post('/createBanners', function($request) {
-  $boardUri = boardOwnerMiddleware($request);
-  global $db, $models;
-  // FIXME: validation that there is an upload...
-  // handle file uploads...
-  $bannersDir = 'storage/boards/' . $boardUri . '/banners';
-  if (!file_exists($bannersDir)) {
-    mkdir($bannersDir);
-  }
-  $file = $bannersDir . '/' . basename($_FILES['files']['tmp_name']);
-  move_uploaded_file($_FILES['files']['tmp_name'], $file);
-
-  $boardData = getBoardByUri($boardUri);
-  $sizes = getimagesize($file);
-
-  $id = $db->insert($models['board_banner'], array(array(
-    'board_id' => $boardData['boardid'],
-    'image' => $file,
-    'w' => $sizes[0],
-    'h' => $sizes[1],
-    'weight' => 1,
-  )));
-
-  sendResponse(array(
-    'id'   => $id,
-    'path' => $file,
-  ));
-});
-
-$routers['lynx']->post('/deleteBanner', function($request) {
-  if (!hasPostVars(array('bannerId'))) {
-    return;
-  }
-  $bannerId = (int)$_POST['bannerId'];
-  global $db, $models;
-  $res = $db->delete($models['board_banner'],array('criteria'=>array(
-    array('bannerid', '=', $bannerId),
-  )));
-  sendResponse($res);
 });
 
 ?>
