5c04372e738565ee33264296794768bcc08303b6
Odilitime <janesmith@airmail.cc>
2020-12-21 05:30:05 +0000

    handle case where there are no banners to avoid warnings

--- common/modules/board/banners/be/handlers/random.php
+++ common/modules/board/banners/be/handlers/random.php
@@ -7,7 +7,9 @@ $res = $db->find($models['board_banner'], array('criteria' => array(
   array('board_id', '=', $boardData['boardid']),
 )));
 $banners = $db->toArray($res);
+if (!count($banners)) {
+  return sendResponse(array());
+}
 shuffle($banners);
 sendResponse($banners[0]);
-
 ?>
