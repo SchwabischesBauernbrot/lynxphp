f5b6f168a0ca5892990006ca5f74d0e738c8350c
odilitime <janesmith@airmail.cc>
2021-01-08 05:04:41 +0000

    set mysql driver as default

--- backend/config.php
+++ backend/config.php
@@ -21,5 +21,6 @@ if (!defined('DB_HOST')) define('DB_HOST', 'localhost');
 if (!defined('DB_USER')) define('DB_USER', 'root');
 if (!defined('DB_PWD')) define('DB_PWD', '');
 if (!defined('DB_NAME')) define('DB_NAME', 'lynxphp');
+if (!defined('DB_DRIVER')) define('DB_DRIVER', 'mysql');
 
 ?>
