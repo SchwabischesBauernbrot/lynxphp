6063fd2203e6b9b67c0e5d1e007b1f2f55f8a2a9
odilitime <janesmith@airmail.cc>
2021-01-08 05:00:34 +0000

    no boards guard

--- frontend/handlers/homepage.php
+++ frontend/handlers/homepage.php
@@ -5,13 +5,15 @@ function homepage() {
   $templates = loadTemplates('index');
   //echo "<pre>", print_r($templates, 1), "</pre>\n";
   $boards_html = '';
-  foreach($boards as $c=>$b) {
-    $tmp = $templates['loop0'];
-    $tmp = str_replace('{{uri}}', $b['uri'], $tmp);
-    $tmp = str_replace('{{title}}', $b['title'], $tmp);
-    $tmp = str_replace('{{description}}', $b['description'], $tmp);
-    $boards_html .= $tmp . "\n";
-    if ($c > 10) break;
+  if (is_array($boards)) {
+    foreach($boards as $c=>$b) {
+      $tmp = $templates['loop0'];
+      $tmp = str_replace('{{uri}}', $b['uri'], $tmp);
+      $tmp = str_replace('{{title}}', $b['title'], $tmp);
+      $tmp = str_replace('{{description}}', $b['description'], $tmp);
+      $boards_html .= $tmp . "\n";
+      if ($c > 10) break;
+    }
   }
 
   $content = $templates['header'];
