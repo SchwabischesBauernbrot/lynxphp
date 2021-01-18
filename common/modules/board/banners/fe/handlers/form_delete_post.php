cc0abc9211b338e0292684e191601dc8e0982454
odilitime <janesmith@airmail.cc>
2021-01-18 06:50:07 +0000

    include which banner to delete, change detection/failure logging

--- common/modules/board/banners/fe/handlers/form_delete_post.php
+++ common/modules/board/banners/fe/handlers/form_delete_post.php
@@ -8,12 +8,15 @@ $boardUri = boardOwnerMiddleware($request);
 if (!$boardUri) return;
 
 // call backend handler to delete banner
-$result = $pkg->useResource('del', array( 'boardUri' => $boardUri ));
-if ($result === '1') {
+$result = $pkg->useResource('del', array(
+  'bannerId' => $request['params']['id'],
+  'boardUri'=> $boardUri, // FIXME: shouldn't have to pass this but we do, fix backend middleware options
+));
+if ($result === true) {
   // success
   redirectTo(BASE_HREF . $boardUri . '/settings/banners');
 } else {
-  wrapContent('Something went wrong...');
+  wrapContent('Something went wrong... Error: ' . print_r($result, 1));
 }
 
 ?>
