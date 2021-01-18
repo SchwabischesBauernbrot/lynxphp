5a8626ff2e84fd3bb5ed72c932ac51cb511a8b09
odilitime <janesmith@airmail.cc>
2021-01-18 06:48:45 +0000

    handle if board doesn't exist

--- common/modules/board/banners/be/handlers/list.php
+++ common/modules/board/banners/be/handlers/list.php
@@ -2,6 +2,10 @@
 $params = $get();
 
 $boardData = boardMiddleware($request);
+if (!$boardData) {
+  return sendResponse(array());
+}
+
 global $db, $models;
 $res = $db->find($models['board_banner'], array('criteria' => array(
   array('board_id', '=', $boardData['boardid']),
