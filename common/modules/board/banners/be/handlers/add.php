7022abba8304d2d77d6a6fad7f083a7583403ba0
odilitime <janesmith@airmail.cc>
2021-01-18 06:48:03 +0000

    better error handling

--- common/modules/board/banners/be/handlers/add.php
+++ common/modules/board/banners/be/handlers/add.php
@@ -2,15 +2,26 @@
 $params = $get();
 
 $boardUri = boardOwnerMiddleware($request);
+if (!$boardUri) {
+  return;
+}
+
 global $db, $models;
 // FIXME: validation that there is an upload...
 // handle file uploads...
 $bannersDir = 'storage/boards/' . $boardUri . '/banners';
+
 if (!file_exists($bannersDir)) {
-  mkdir($bannersDir);
+  // try to recover bannersDir
+  if (!mkdir($bannersDir)) {
+    return sendResponse(array(), 500, $bannersDir . 'does not exist');
+  }
 }
 $file = $bannersDir . '/' . basename($_FILES['files']['tmp_name']);
 move_uploaded_file($_FILES['files']['tmp_name'], $file);
+if (!file_exists($file)) {
+  return sendResponse(array(), 500, 'could not make ' . $file);
+}
 
 $boardData = getBoardByUri($boardUri);
 $sizes = getimagesize($file);
