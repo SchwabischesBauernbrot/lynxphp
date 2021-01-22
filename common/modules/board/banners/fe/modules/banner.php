e1665221599b6e7a10f72b8b6b581685c66579b6
Odilitime <janesmith@airmail.cc>
2021-01-22 00:40:08 +0000

    calculated 640x240 banner limits

--- common/modules/board/banners/fe/modules/banner.php
+++ common/modules/board/banners/fe/modules/banner.php
@@ -10,7 +10,19 @@ $banner = $pkg->useResource('random', array('boardUri' => $boardUri));
 
 // add {{banner}} tag
 if (is_array($banner) && count($banner)) {
-  $io['tags']['banner'] = '<img src="'. BASE_HREF . 'backend/' . $banner['image']. '" width="'.$banner['w'].'" height="'.$banner['h'].'" style="max-height: 500px; max-width: 1000px;">';
+  $w = $banner['w'];
+  $h = $banner['h'];
+  while($w > 640) {
+    $h *= 0.9;
+    $w *= 0.9;
+  }
+  while($h > 240) {
+    $h *= 0.9;
+    $w *= 0.9;
+  }
+  $ih = (int)$h;
+  $iw = (int)$w;
+  $io['tags']['banner'] = '<img src="'. BASE_HREF . 'backend/' . $banner['image']. '" width="'.$w.'" height="'.$h.'">';
 } else {
   // array() just means no banners
   $io['tags']['banner'] = 'No banners, make a banners thread';
