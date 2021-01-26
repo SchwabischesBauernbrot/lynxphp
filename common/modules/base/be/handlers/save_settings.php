4a2c583e9c33ff13c8a796488a0442ce37a623bb
odilitime <janesmith@airmail.cc>
2021-01-26 06:42:25 +0000

    clean up, and only clear if logo_clear is set, otherwise pass thru logo

--- common/modules/base/be/handlers/save_settings.php
+++ common/modules/base/be/handlers/save_settings.php
@@ -9,23 +9,21 @@ if (!userInGroupMiddleware($request, array('admin'))) {
 global $db, $models;
 $settings = array();
 $settings['json'] = getAllSiteSettings();
-//if (!is_array($settings['json'])) $settings['json'] = array();
 
-//echo "<pre>GET[", print_r($_GET, 1), "]</pre>\n";
-//echo "<pre>POST[", print_r($_POST, 1), "]</pre>\n";
-//echo "<pre>FILES[", print_r($_FILES, 1), "]</pre>\n";
+$ok = true;
 
-//echo "settings[", gettype($settings), "][", print_r($settings, 1), "]<br>\n";
-//echo "json[", gettype($settings['json']), "][", print_r($settings['json'], 1), "]<br>\n";
 foreach($_POST as $k => $v) {
-  //echo "set [$k=$v]<br>\n";
-  $settings['json'][$k] = $v;
+  if ($k === 'logo') {
+  } else
+  if ($k === 'logo_clear') {
+    $settings['json']['logo'] = false;
+  } else {
+    $settings['json'][$k] = $v;
+  }
 }
 
-$settings['json']['logo'] = false;
 if ($_POST['logo']) {
   $logo = json_decode($_POST['logo'], true);
-  //echo "<pre>logos[", print_r($logos, 1), "]</pre>\n";
   if (!empty($logo['hash'])) {
     $srcPath = 'storage/tmp/'.$logo['hash'];
     if (file_exists($srcPath)) {
@@ -40,12 +38,10 @@ if ($_POST['logo']) {
   }
 }
 
-//echo "settings[", gettype($settings), "][", print_r($settings, 1), "]<br>\n";
 $ok = $db->update($models['setting'], $settings, array('criteria'=>array('settingid'=>1)));
 
 sendResponse(array(
   'success' => $ok ? 'true' : 'false',
-  //'settings' => $settings,
 ));
 
 ?>
