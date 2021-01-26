beedacf6c13a05d6fb909ea4c549262b8a5cff1c
odilitime <janesmith@airmail.cc>
2021-01-26 06:21:11 +0000

    add threads.json / rules

--- backend/routes/4chan.php
+++ backend/routes/4chan.php
@@ -2,6 +2,13 @@
 
 $router = new router;
 
+/*
+Do not make more than one request per second.
+Thread updating should be set to a minimum of 10 seconds, preferably higher.
+Use If-Modified-Since when doing your requests.
+Make API requests using the same protocol as the app. Only use SSL when a user is accessing your app over HTTPS.
+*/
+
 // https://a.4cdn.org/boards.json
 $router->get('/boards.json', function($request) {
   $boards = listBoards();
@@ -28,23 +35,56 @@ $router->get('/:board/catalog.json', function($request) {
   echo json_encode($res);
 });
 
-// FIXME: https://a.4cdn.org/po/threads.json
-// FIXME: https://a.4cdn.org/archive.json
-
-// https://a.4cdn.org/po/thread/570368.json
-$router->get('/:board/thread/:thread', function($request) {
+// Thread list
+// https://a.4cdn.org/po/threads.json
+$router->get('/:board/threads.json', function($request) {
+  global $tpp;
   $boardUri = $request['params']['board'];
-  $threadNum = (int)str_replace('.json', '', $request['params']['thread']);
-  $posts = getThread($boardUri, $threadNum);
-  echo json_encode(array('posts'=>$posts));
+  $page = boardCatalog($boardUri);
+  if (!is_array($page)) {
+    sendResponse(array(), 404, 'Board not found');
+    return;
+  }
+  $pages = count($page);
+  $res = array();
+  for($i = 1; $i <= $pages; $i++) {
+    $threads = array();
+    foreach($page[$i] as $t) {
+      // no, last_modified, replies
+      $thread = array(
+        'no'            => $t['no'],
+        //'replies'       => empty($t['reply_count']) ? null : $t['reply_count'],
+        'replies'       => $t['reply_count'],
+        'last_modified' => $t['updated_at']
+      );
+      $threads[] = $thread;
+    }
+    $res[] = array(
+      'page' => $i,
+      'threads' => $threads,
+    );
+  }
+  echo json_encode($res);
 });
 
+// FIXME: https://a.4cdn.org/archive.json
+
+// Indexes
 // https://a.4cdn.org/po/2.json
 $router->get('/:board/:page', function($request) {
   $boardUri = $request['params']['board'];
   $page = str_replace('.json', '', $request['params']['page']);
   $threads = boardPage($boardUri, $page);
   echo json_encode($threads);
+});
+
+// Thread endpoint
+// https://a.4cdn.org/po/thread/570368.json
+$router->get('/:board/thread/:thread', function($request) {
+  $boardUri = $request['params']['board'];
+  $threadNum = (int)str_replace('.json', '', $request['params']['thread']);
+  $posts = getThread($boardUri, $threadNum);
+  echo json_encode(array('posts'=>$posts));
 });
 
 return $router;
