b821e13d571157a7f743dd77167ec3b09522c899
odilitime <janesmith@airmail.cc>
2021-01-26 06:21:32 +0000

    testBoardThreadsJson test

--- tests/integration/test_fourchan_Test.php
+++ tests/integration/test_fourchan_Test.php
@@ -3,27 +3,28 @@ use PHPUnit\Framework\TestCase;
 
 final class test_fourchan_Test extends TestCase
 {
-    public function testBoardsJson(): void
-    {
+    public function testBoardsJson(): void {
       $boards = getExpectJson('4chan/boards.json');
       $this->assertIsArray($boards);
     }
 
-    public function testBoardCatalogJson(): void
-    {
-      $boards = getExpectJson('/board/catalog.json');
-      $this->assertIsArray($boards);
-    }
-
-    public function testBoardThreadXJson(): void
-    {
-      $threads = getExpectJson('/board/thread/X.json');
+    public function testBoardCatalogJson(): void {
+      $threads = getExpectJson('/board/catalog.json');
       $this->assertIsArray($threads);
     }
 
-    public function testBoardPage1Json(): void
-    {
+    public function testBoardThreadsJson(): void {
+      $pages = getExpectJson('/board/threads.json');
+      $this->assertIsArray($pages);
+    }
+
+    public function testBoardPage1Json(): void {
       $threads = getExpectJson('/board/1.json');
+      $this->assertIsArray($threads);
+    }
+
+    public function testBoardThreadXJson(): void {
+      $threads = getExpectJson('/board/thread/X.json');
       $this->assertIsArray($threads);
     }
 
