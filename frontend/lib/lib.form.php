f9f9a4c07dd923a50d04dbba73b4230828f6927d
odilitime <janesmith@airmail.cc>
2021-01-26 06:31:46 +0000

    image clear support, new types: select, password, number/integer

--- frontend/lib/lib.form.php
+++ frontend/lib/lib.form.php
@@ -10,11 +10,31 @@ function generateForm($action, $fields, $values) {
     }
     $html .= '<dt><label for="' . $field . '">'. $details['label'] . ': &nbsp;</label>';
     // can be stomped if needed
+    // do we need to escape?
     $value = empty($values[$field]) ? '' : $values[$field];
     $html .= '<dd>';
     switch($details['type']) {
       case 'text':
         $html .= '<input type=text name="'.$field.'" value="' . $value . '">';
+      break;
+      case 'password':
+        // always blank and can't be cleared
+        $html .= '<input type=text name="'.$field.'">';
+      break;
+      case 'integer':
+      case 'number':
+        $html .= '<input type=number name="'.$field.'" value="' . $value . '">';
+      break;
+      case 'textarea':
+        // do we need to escape?
+        $html .= '<textarea name="'.$field.'">'.$value.'</textarea>';
+      break;
+      case 'select':
+        $html .= '<select name="' . $field . '">';
+        foreach($details['options'] as $v => $l) {
+          $html .= '<option value="' . $v . '">' . $l;
+        }
+        $html .= '</select>';
       break;
       case 'checkbox':
         $checked = $value ? ' CHECKED' : '';
@@ -24,8 +44,9 @@ function generateForm($action, $fields, $values) {
         if ($value) {
           // FIXME: BACKEND_BASE_URL
           // if not set it will clear it, so we need a clear checkbox...
-          $html .= '<img height=100 src="backend/' . $value . '">';
+          $html .= '<img height=100 src="backend/' . $value . '"><br>';
         }
+        $html .= '<label><input type=checkbox name="'.$field.'_clear"> Reset back to default</label><br>';
         $html .= '<input type=file name="'.$field.'">';
       break;
       default:
