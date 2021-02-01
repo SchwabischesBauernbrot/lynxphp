1f4d46c85a765d1edd8ad4479d589ead1a1648a9
odilitime <janesmith@airmail.cc>
2021-02-01 03:11:14 +0000

    remove double slash

--- frontend/handlers/login.php
+++ frontend/handlers/login.php
@@ -3,25 +3,9 @@
 function getLoginForm() {
   $templates = loadTemplates('login');
   $tags = array(
-    'action' => 'https://' . BASE_HOST . '/' . BASE_HREF . 'forms/login',
+    'action' => 'https://' . BASE_HOST . BASE_HREF . 'forms/login',
   );
   return replace_tags($templates['header'], $tags);
-
-  $secure_login_link = 'login.php';
-  $secure_signup_link = 'signup.php';
-  $content = <<< EOB
-<form action="$secure_login_link" method="POST">
-  <dl>
-    <dt>Username:
-    <dd><input type=text name="username">
-    <dt>Password:
-    <dd><input type=password name="password">
-  </dl>
-  <input type=submit value="log in">
-</form>
-<a href="$secure_signup_link">create account</a>
-EOB;
-  return $content;
 }
 
 function getLogin() {
