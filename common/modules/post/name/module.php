1fc2c3d844b4c6cc05f39c497fe74f1e77c5da4d
odilitime <janesmith@airmail.cc>
2021-02-05 05:26:33 +0000

    name module

---
+++ common/modules/post/name/module.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+  'name' => 'post_name',
+  'version' => 1,
+  'resources' => array(
+    /*
+    array(
+      'name' => 'settings',
+      'params' => array(
+        'endpoint' => 'opt/settings',
+        'sendSession' => true,
+        'unwrapData' => true,
+        'requires' => array(),
+        'params' => 'querystring',
+      ),
+    ),
+    */
+  ),
+);
+?>
