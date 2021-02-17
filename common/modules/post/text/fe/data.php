71e5ac0361796d17451f02ab73c11c7dafedfee3
odilitime <janesmith@airmail.cc>
2021-02-17 01:27:15 +0000

    text formating module

---
+++ common/modules/post/text/fe/data.php
@@ -0,0 +1,35 @@
+<?php
+return array(
+  array(
+    'handlers' => array(
+      /*
+      array(
+        'method'  => 'GET',
+        'route'   => '/:uri/banners',
+        'handler' => 'public_list',
+      ),
+      array(
+        'method'  => 'GET',
+        'route'   => '/:uri/settings/banners',
+        'handler' => 'settings_list',
+      ),
+      */
+    ),
+    'forms' => array(
+      /*
+      array(
+        'route' => '/:uri/settings/banners/add',
+        'handler' => 'add',
+      ),
+      */
+    ),
+    'modules' => array(
+      // format text
+      array(
+        'pipeline' => 'PIPELINE_POST_TEXT_FORMATTING',
+        'module' => 'format',
+      ),
+    ),
+  ),
+);
+?>
---
+++ common/modules/post/text/fe/modules/format.php
@@ -0,0 +1,8 @@
+<?php
+
+$params = $getModule();
+
+$io['com'] = str_replace('subject', 'name', $io['com']);
+
+
+?>
