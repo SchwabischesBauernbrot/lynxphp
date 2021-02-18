25664fd5f39ff9b3aec496dcfb3a5036bf03ef12
Odilitime <janesmith@airmail.cc>
2021-02-18 04:39:35 +0000

    audio thumbs, fileDBtoAPI takes boardUri and fixes as much as it can, getThumbnailSize(), disable some debug

--- backend/interfaces/files.php
+++ backend/interfaces/files.php
@@ -1,21 +1,76 @@
 <?php
 
-function fileDBtoAPI(&$row) {
+function fileDBtoAPI(&$row, $boardUri) {
   // expect file_ fields and strip the file_
   $row = key_map(function($v) { return substr($v, 5); }, array_filter($row, function($v, $k) {
     $f5 = substr($k, 0, 5);
     return $f5 ==='file_';
   }, ARRAY_FILTER_USE_BOTH));
+
+  // fix size
+  if (empty($row['size']) && $row['fileid']) {
+    global $db;
+    $post_files_model = getPostFilesModel($boardUri);
+    $size = filesize($row['path']);
+    $urow = array('size' => $size);
+    $db->update($post_files_model, $urow, array('criteria' =>  array('fileid' => $row['fileid'])));
+    $row['size'] = $size;
+  }
+  // fix mime_type since it drives thumbnailing
+  if (empty($row['mime_type']) && $row['fileid']) {
+    global $db;
+    $post_files_model = getPostFilesModel($boardUri);
+
+    $finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
+    // php 5.3+ has this by default...
+    $mime = finfo_file($finfo, $row['path']);
+    $urow = array('mime_type' => $mime);
+    $db->update($post_files_model, $urow, array('criteria' =>  array('fileid' => $row['fileid'])));
+    $row['mime_type'] = $mime;
+  }
+  $m6 = substr($row['mime_type'], 0, 6);
+  $isImage = $m6 === 'image/';
+  // fix image size
+  if ((empty($row['w']) || empty($row['h'])) && $row['fileid'] && $isImage) {
+    global $db;
+    $post_files_model = getPostFilesModel($boardUri);
+    $sizes = getimagesize($row['path']);
+    $urow = array('w' => $sizes[0], 'h' => $sizes[1]);
+    $db->update($post_files_model, $urow, array('criteria' =>  array('fileid' => $row['fileid'])));
+    $row['w'] = $sizes[0];
+    $row['h'] = $sizes[1];
+  }
+
   $path = parsePath($row['path']);
   $thumb = $path['thumb'];
   $fp = getcwd() . '/' .  $thumb;
+
   //echo "path[$thumb] [", getcwd(), "] fp[$fp]<br>\n";
   if (file_exists($fp) && filesize($fp)) {
     //echo "[$thumb] exists<br>\n";
     $row['thumbnail_path'] = $thumb;
+    // size fix up
+    if ($row['fileid'] && (empty($row['tn_w']) || empty($row['tn_h']))) {
+      //echo "Updating thumbnail size on [", $row['fileid'], "][",$row['tn_w'],"]x[",$row['tn_h'],"]<Br>\n";
+      global $db;
+      $post_files_model = getPostFilesModel($boardUri);
+      if (0) {
+        $sizes = getimagesize($fp);
+        $row['w'] = $sizes[0];
+        $row['h'] = $sizes[1];
+      }
+      $sizes = getThumbnailSize($row);
+      $urow = array('tn_w' => $sizes[0], 'tn_h' => $sizes[1]);
+      //echo "<pre>", print_r($urow, 1), "</pre>\n";
+      //echo "<pre>", print_r($row, 1), "</pre>\n";
+      $db->update($post_files_model, $urow, array('criteria' =>  array('fileid' => $row['fileid'])));
+      $row['tn_w'] = $sizes[0];
+      $row['tn_h'] = $sizes[1];
+    }
   } else {
     // request generation
     global $workqueue;
+    $row['boardUri'] = $boardUri;
     $workqueue->addWork(PIPELINE_FILE, $row);
     // but how do we not loop?
   }
@@ -74,6 +129,26 @@ function parsePath($filePath) {
   );
 }
 
+function getThumbnailSize($row) {
+  $m6 = substr($row['mime_type'], 0, 6);
+
+  $isImage = $m6 === 'image/';
+  $isVideo = $m6 === 'video/';
+  $isAudio = $m6 === 'audio/';
+
+  $urow = array();
+  //echo "mime[", $row['mime_type'], "] isImage[$isImage]<br>\n";
+  if ($isImage) {
+    list($urow['tn_w'], $urow['tn_h']) = scaleSize($row['w'], $row['h'], 240);
+    $urow['tn_w'] = (int)$urow['tn_w'];
+    $urow['tn_h'] = (int)$urow['tn_h'];
+  } else {
+    $urow['tn_w'] = 240;
+    $urow['tn_h'] = 240;
+  }
+  return array($urow['tn_w'], $urow['tn_h']);
+}
+
 function scaleSize($w, $h, $maxh = 240) {
   // calculate thumbnail size
   $tn_w = $w;
@@ -100,26 +175,50 @@ function make_thumbnail($fileData, $duration = 1) {
   $isVideo = $m6 === 'video/';
   $isAudio = $m6 === 'audio/';
 
-  echo "isImage[$isImage]<br>\n";
+  //echo "isImage[$isImage]<br>\n";
   echo "<pre>", print_r($fileData, 1), "</pre>\n";
 
   $updateThumbSize = false;
+  $sizes = getThumbnailSize($fileData);
   if (empty($fileData['tn_w']) || empty($fileData['tn_h'])) {
-    list($fileData['tn_w'], $fileData['tn_h']) = scaleSize($fileData['w'], $fileData['h'], 240);
-    echo "Calcing thumbnail size [", $fileData['tn_w'], "x", $fileData['tn_h'], "]<br>\n";
     $updateThumbSize = true;
   }
+  $fileData['tn_w'] = $sizes[0];
+  $fileData['tn_h'] = $sizes[1];
+  if ($isImage) {
+    if (!make_image_thumbnail_ffmpeg($fileData['path'], $fileData['tn_w'], $fileData['tn_h'], $duration)) {
+      // fail
+      echo "Failed<br>\n";
+      return;
+    }
+  } elseif ($isAudio) {
+    // the image will actually extract album artwork I think
 
-  if (!make_image_thumbnail_ffmpeg($fileData['path'], $fileData['tn_w'], $fileData['tn_h'], $duration)) {
-    // fail
-    echo "Failed<br>\n";
-    return;
+    // FIXME: move this logic into make_audio_thumbnail_ffmpeg
+
+    //echo "Making audio thumb<br>\n";
+    if (!make_image_thumbnail_ffmpeg($fileData['path'], $fileData['tn_w'], $fileData['tn_h'], $duration)) {
+      // fail
+      //echo "no album artwork<br>\n";
+      if (!make_audio_thumbnail_ffmpeg($fileData['path'], $fileData['tn_w'], $fileData['tn_h'], $duration)) {
+        // fail
+        echo "Failed<br>\n";
+        return;
+      }
+    }
+  } elseif ($isVideo) {
+    // should work fine...
+    if (!make_image_thumbnail_ffmpeg($fileData['path'], $fileData['tn_w'], $fileData['tn_h'], $duration)) {
+      // fail
+      echo "Failed<br>\n";
+      return;
+    }
   }
   // get final size?
   // may not save any writes at all if the size differes
   if ($updateThumbSize) {
     // write thumbnail size to db
-    if (!$fileData['boardUri']) {
+    if (empty($fileData['boardUri'])) {
       echo "Would update thumbsize but no boardUri<br>\n";
       return;
     }
@@ -167,6 +266,61 @@ function make_image_thumbnail_ffmpeg($filePath, $width, $height, $duration = 1) 
   $try = floor($duration / 2);
   //exec('$ffmpegPath -strict -2 -ss ' . $try . ' -i ' . $fileIn . ' -v quiet -an -vframes 1 -f mjpeg -vf scale=' . $width . ':' . $height .' ' . $fileOut . ' 2>&1', $ffmpeg_out, $ret);
   exec($ffmpegPath . ' -i ' . $sFileIn . ' -vf scale=' . $width . ':' . $height .' ' . $sFileOut . ' 2>&1', $ffmpeg_out, $ret);
+  echo "ret[$ret]<br>\n";
+  // failure seems to be 1 (if the file already exists)
+  // ret === 0 on success
+  //if (!$ret) {
+    echo "<pre>", print_r($ffmpeg_out, 1), "</pre>\n";
+  //}
+  // if duration fails
+  if (!file_exists($fileOut) || !filesize($fileOut)) {
+    echo "file does not exist or empty [$fileOut] ret[$ret]<br>\n";
+    return false;
+    //  && $trg
+    /*
+    exec("$ffmpegPath -y -strict -2 -ss 0 -i $filename -v quiet -an -vframes 1 -f mjpeg -vf scale=$width:$height $thumbnailfc 2>&1", $ffmpeg_out, $ret);
+    clearstatcache();
+    if (!filesize($fileOut)) {
+      return false;
+    }
+    */
+  }
+  return true;
+}
+
+function make_audio_thumbnail_ffmpeg($filePath, $width, $height, $duration = 1) {
+  $fileIn = $filePath;
+  if (!$fileIn || !file_exists($fileIn)) {
+    echo "Source file does not exists[$fileIn]<br>\n";
+    return false;
+  }
+  $sFileIn = escapeshellarg($filePath);
+
+  $path = parsePath($filePath);
+
+  $fileOut = $path['thumb'];
+
+  // clean up zero byte files to prevent prompt
+  $outExists = file_exists($fileOut);
+  if ($outExists && !filesize($fileOut)) {
+    unlink($fileOut);
+    $outExists = false;
+  }
+  if ($outExists) {
+    echo "File[$fileOut] already exists<br>\n";
+    return false;
+  }
+
+  $sFileOut = escapeshellarg($fileOut);
+  $ffmpegPath = '/usr/bin/ffmpeg';
+
+  $width = (int)$width;
+  $height = (int)$height;
+
+  $ffmpeg_out = array();
+  $try = floor($duration / 2);
+  //exec('$ffmpegPath -strict -2 -ss ' . $try . ' -i ' . $fileIn . ' -v quiet -an -vframes 1 -f mjpeg -vf scale=' . $width . ':' . $height .' ' . $fileOut . ' 2>&1', $ffmpeg_out, $ret);
+  exec($ffmpegPath . ' -i ' . $sFileIn . ' -filter_complex "showwavespic=s='.$width.'x'.$height.':split_channels=1" -frames:v 1 ' . $sFileOut . ' 2>&1', $ffmpeg_out, $ret);
   echo "ret[$ret]<br>\n";
   // failure seems to be 1 (if the file already exists)
   // ret === 0 on success
@@ -307,6 +461,7 @@ function processFiles($boardUri, $files_json, $threadid, $postid) {
     print_r($extFileData);
     //$workqueue->addWork(PIPELINE_FILE, $extFileData);
     */
+    $fileData['boardUri'] = $boardUri;
     $workqueue->addWork(PIPELINE_FILE, $fileData);
 
   }
