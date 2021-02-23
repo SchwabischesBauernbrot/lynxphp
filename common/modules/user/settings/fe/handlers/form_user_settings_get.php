425120f0a776e8c7d109228bd66849ed870b7e1c
odilitime <janesmith@airmail.cc>
2021-02-23 04:33:01 +0000

    load settings into form

--- common/modules/user/settings/fe/handlers/form_user_settings_get.php
+++ common/modules/user/settings/fe/handlers/form_user_settings_get.php
@@ -2,23 +2,13 @@
 
 $params = $getHandler();
 
-// FIXME: are we logged in?
-
-$values = $pkg->useResource('settings');
-
 global $pipelines;
-$fields = $shared['fields']; // imported from fe/common.php
-
-foreach($fields as $fn => $f) {
-  if (substr($fn, 0, 9) === 'settings_') {
-    //$k = substr($fn, 9);
-    //echo "k[$k]<br>\n";
-    $values[$fn] = $values['json'][$fn];
-  }
-}
-
+$fields = $shared['fields']; // imported from shared.php
 // handle hooks for additional settings
 $pipelines[PIPELINE_BOARD_SETTING_GENERAL]->execute($fields);
+
+$data = $pkg->useResource('settings');
+$values = $data['settings'];
 
 $html = generateForm($params['action'], $fields, $values);
 
