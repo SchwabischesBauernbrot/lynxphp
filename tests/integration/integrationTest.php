caf65c533490615d0c56f4fedb2539682d839265
odilitime <janesmith@airmail.cc>
2021-02-25 04:05:02 +0000

    usesSendResponse(), deleteBoard(), USE_CONFIG env or argv to set HTTP_HOST

--- tests/integration/integrationTest.php
+++ tests/integration/integrationTest.php
@@ -1,5 +1,30 @@
 <?php
 
+$host = getenv('USE_CONFIG');
+// argument overrides environment
+if (isset($GLOBALS['argv'][3])) {
+  $host = $GLOBALS['argv'][3];
+}
+
+function deleteBoard($boardUri) {
+  global $db, $models;
+
+  // delete files table
+  $fm = getPostFilesModel($boardUri);
+  $db->delete($fm, array());
+
+  // delete posts table
+  $pm = getPostsModel($boardUri);
+  $db->delete($pm, array());
+
+  // delete board row
+  $db->delete($models['board'], array('criteria'=>array('uri' => $boardUri)));
+}
+
+if ($host) {
+  $_SERVER['HTTP_HOST'] = $host;
+}
+
 chdir('frontend');
 include '../common/post_vars.php';
 // how do we get the correct server name?
@@ -11,4 +36,13 @@ chdir('..');
 
 function wrapContent($content) {
   echo "wrapContent called[$content]\n";
+}
+
+function usesSendResponse($t, $res) {
+  $t->assertIsArray($res);
+  $t->assertArrayHasKey('meta', $res);
+  $t->assertIsArray($res['meta']);
+  $t->assertArrayHasKey('code', $res['meta']);
+  $t->assertArrayHasKey('data', $res);
+  $t->assertIsArray($res['data']);
 }
