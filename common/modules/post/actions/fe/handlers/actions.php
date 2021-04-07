32605c0be8a3a3e60f8f667643cd940a0c31f243
odilitime <janesmith@airmail.cc>
2021-04-07 04:00:03 +0000

    refactor to switch, handle checkedposts

--- common/modules/post/actions/fe/handlers/actions.php
+++ common/modules/post/actions/fe/handlers/actions.php
@@ -14,60 +14,111 @@ $boardUri = $request['params']['uri'];
 // ip deletion?
 // purge files vs remove from post
 
+// not bad, it gets hidden
 echo "Please wait...<br>\n"; flush();
 
-if (getOptionalPostField('delete')) {
-  // FIXME: could be a ban-delete too
-  if (is_array($_POST['checkedposts'])) {
-    $threadNum = getOptionalPostField('thread') ? getOptionalPostField('thread') : 'ThreadNum';
-    $postFields = array();
-    foreach($_POST['checkedposts'] as $postNum) {
-      $postFields[$boardUri . '-' . $threadNum . '-' . $postNum] = true;
+// can only prepare one action...
+$action = $_POST['action'];
+switch($action) {
+  case 'delete':
+    // FIXME: could be a ban-delete too
+    if (is_array($_POST['checkedposts'])) {
+      $threadNum = getOptionalPostField('thread') ? getOptionalPostField('thread') : 'ThreadNum';
+      $postFields = array();
+      foreach($_POST['checkedposts'] as $postNum) {
+        $postFields[$boardUri . '-' . $threadNum . '-' . $postNum] = true;
+      }
+      // how is multiple handled?
+      $result = $pkg->useResource('content_actions',
+        array('action' => 'delete', 'password' => getOptionalPostField('password')),
+        array('addPostFields' => $postFields)
+      );
+      if ($result['removedPosts'] + $result['removedThreads'] === count($postFields)) {
+        echo "Successful!<bR>\n"; flush();
+        if ($threadNum === 'ThreadNum') {
+          return redirectTo('/' . $boardUri . '/');
+        } else {
+          return redirectTo('/' . $boardUri . '/thread/' . $threadNum . '.html');
+        }
+      }
+    } else {
+      echo "write me!<br>\n";
     }
-    // how is multiple handled?
+  break;
+  case 'report':
+    // send report request to BE
+    // is reason required?
+    $postFields = array();
+    $threadNum = getOptionalPostField('thread') ? getOptionalPostField('thread') : 'ThreadNum';
+    if (is_array($_POST['checkedposts'])) {
+      foreach($_POST['checkedposts'] as $postNum) {
+        $postFields[$boardUri . '-' . $threadNum . '-' . $postNum] = true;
+      }
+    } else {
+      $postFields[$boardUri . '-' . $threadNum . '-' . $_POST['checkedposts']] = true;
+    }
+    //echo "<pre>", print_r($postFields, 1), "</pre>\n";
     $result = $pkg->useResource('content_actions',
-      array('action' => 'delete', 'password' => getOptionalPostField('password')),
+      array('action' => 'report'),
       array('addPostFields' => $postFields)
     );
-    if ($result['removedPosts'] + $result['removedThreads'] === count($postFields)) {
-      echo "Successful!<bR>\n"; flush();
-      if ($threadNum === 'ThreadNum') {
-        return redirectTo('/' . $boardUri . '/');
-      } else {
-        return redirectTo('/' . $boardUri . '/thread/' . $threadNum . '.html');
-      }
-    }
-  } else {
-    echo "write me!<br>\n";
-  }
+  break;
+  default:
+    wrapContent('Error: unknown action [' . $action . ']');
+    return;
+  break;
 }
 
-if (getOptionalPostField('report')) {
-  // send report request to BE
-  // is reason required?
-  $result = $pkg->useResource('content_actions',
-    array('action' => 'report'),
-    array('addPostFields' => array( $boardUri . '-ThreadNum-'.$_POST['checkedposts'] => true))
-  );
-}
+/*
 if (getOptionalPostField('global_report')) {
   // send report request to BE
   // is reason required?
+  $postFields = array();
+  $threadNum = getOptionalPostField('thread') ? getOptionalPostField('thread') : 'ThreadNum';
+  if (is_array($_POST['checkedposts'])) {
+    foreach($_POST['checkedposts'] as $postNum) {
+      $postFields[$boardUri . '-' . $threadNum . '-' . $postNum] = true;
+    }
+  } else {
+    $postFields[$boardUri . '-' . $threadNum . '-' . $_POST['checkedposts']] = true;
+  }
   $result = $pkg->useResource('content_actions',
     array('action' => 'report', 'globalReport'=>1),
-    array('addPostFields' => array( $boardUri . '-ThreadNum-'.$_POST['checkedposts'] => true))
+    array('addPostFields' => $postFields)
   );
 }
+*/
 
 if (count($result['issues'])) {
   wrapContent(print_r($result['issues'], 1)."<br>\n<pre>".print_r($result, 1)."</pre>\n");
 } else {
   $boardUri = $result['request'][0]['board'];
-  //wrapContent('<pre>' . print_r($result, 1) . '</pre>');
-  if ($result['request'][0]['threadid'] !== 'ThreadNum') {
-    redirectTo('/'. $boardUri . '/thread/' . $result['request'][0]['threadid'] . '.html');
+
+  // confirm the number matches...
+  $ok = true;
+  if (is_array($_POST['checkedposts'])) {
+    $ok = false;
+    if (count($result['request']) === count($_POST['checkedposts'])) {
+      $ok = true;
+    }
+  }
+
+  // how do we know they were added?
+  if (getOptionalPostField('report')) {
+    $ok = false;
+    if ($result['reportsAdded'] === count($_POST['checkedposts'])) {
+      $ok = true;
+    }
+  }
+
+  if ($ok) {
+    if ($result['request'][0]['threadid'] !== 'ThreadNum') {
+      redirectTo('/'. $boardUri . '/thread/' . $result['request'][0]['threadid'] . '.html');
+    } else {
+      redirectTo('/'. $boardUri . '/');
+    }
   } else {
-    redirectTo('/'. $boardUri . '/');
+    wrapContent('<pre>' . print_r($result, 1) . '</pre>');
   }
 }
 
