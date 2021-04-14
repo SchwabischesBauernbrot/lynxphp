52ddd978161acd67f5de2d35330d74ccef99cb1f
odilitime <janesmith@airmail.cc>
2021-04-14 03:38:56 +0000

    increase precision on , checkHeaders(), sendResponse2(), sendResponse add 4th meta param

--- backend/index.php
+++ backend/index.php
@@ -14,7 +14,7 @@ include 'config.php';
 $router = include '../common/router.php';
 
 // one syscall to get the current time
-$now = time();
+$now = microtime(true);
 
 // connect to db
 include 'lib/database_drivers/' . DB_DRIVER . '.php';
@@ -27,6 +27,7 @@ $db = new $driver_name;
 include '../common/queue_implementations/db.php';
 $queue_type_class = 'db' . '_queue_driver';
 $queue = new $queue_type_class;
+
 // set up workqueue
 include '../common/workqueue.php';
 $workqueue = new work_queue;
@@ -91,10 +92,108 @@ $response_template = array(
   ),
 );
 
-function sendResponse($data, $code = 200, $err = '') {
+function checkHeaders($mtime, $filesize = false) {
+  // cached and non-cached need this
+  $headers=array();
+  if (function_exists('apache_request_headers')) {
+    $headers = apache_request_headers();
+  }
+  // we don't always know the size
+  $etag = false;
+  if ($filesize) { // don't generate if not needed
+    $etag = dechex($mtime) . '-' . dechex($filesize);
+  }
+  if ((!isset($headers['Cache-Control']) || (isset($headers['Cache-Control']) && $headers['Cache-Control'] !== 'no-cache'))
+      && (
+        ($etag && isset($headers['If-None-Match']) && $headers['If-None-Match']
+           && strpos($headers['If-None-Match'], $etag)!==false) ||
+        (isset($headers['If-Modified-Since']) && $headers['If-Modified-Since']
+           && $lastmod == $headers['If-Modified-Since']))
+      ) {
+    header('HTTP/1.1 304 Not Modified');
+    // why is this empty?
+    header('Content-Type: ');
+    // maybe just exit?
+    return true;
+  }
+  // maybe return etag so it doesn't have to be generated?
+  return false;
+}
+
+function sendResponse2($data, $options = array()) {
+  global $response_template, $now;
+  $code  = 200;
+  $err   = '';
+  $mtime = $now;
+  $meta = array();
+  if (isset($options['err']))     $err   = $options['err'];
+  if (isset($options['code']))    $code  = $options['code'];
+  if (isset($options['lastMod'])) $mtime = $options['lastMod'];
+  if (isset($options['meta']))    $meta  = $options['meta'];
+
+  $resp = $response_template;
+  $resp['meta']['code'] = $code;
+  foreach($meta as $k => $v) {
+    $resp['meta'][$k] = $v;
+  }
+  $resp['data'] = $data;
+  if ($err) {
+    $resp['meta']['err'] = $err;
+  }
+  if (getQueryField('prettyPrint')) {
+    $output = '<pre>' . json_encode($resp, JSON_PRETTY_PRINT) . "</pre>\n";
+  } else {
+    $output = json_encode($resp);
+  }
+  $filesize = strlen($output);
+
+  if ($mtime === $now) {
+    // dont cache
+    header ('Expires: '.gmdate('D M d H:i:s Y', 1)); // old
+    header ('Proxy-Connection: keep-alive');
+    header ('Cache-Control: no-store, must-revalidate, post-check=1, pre-check=2');
+  } else {
+    // take control over caching
+
+    $httpauth = isset($_SERVER['PHP_AUTH_USER']);
+    $ssl = false;
+    if (isset($_SERVER['HTTPS'])) {
+      $ssl = $_SERVER['HTTPS'] === 'on';
+    }
+    $public='';
+    if ($httpauth || $ssl) {
+      // public says to cache through SSL & httpauth
+      // otherwise just cached in memory only
+      $public = 'public, ';
+    }
+    $etag = false;
+    if ($filesize) { // don't generate if not needed
+      $etag = dechex($mtime) . '-' . dechex($filesize);
+    }
+    $lastmod = gmdate('D, d M Y H:i:s', $mtime).' GMT';
+    header('Last-Modified: ' . $lastmod);
+    if ($filesize) {
+      header('Content-Length: ' . $filesize);
+    }
+    header('Cache-Control: ' . $public . 'must-revalidate');
+    header('Vary: Accept-Encoding');
+    if ($etag) {
+      header('ETag: "' . $etag . '"');
+    }
+  }
+  echo $output;
+  return true;
+}
+
+function sendResponse($data, $code = 200, $err = '', $meta = false) {
   global $response_template;
   $resp = $response_template;
   $resp['meta']['code'] = $code;
+  if ($meta) {
+    foreach($meta as $k => $v) {
+      $resp['meta'][$k] = $v;
+    }
+  }
   $resp['data'] = $data;
   if ($err) {
     $resp['meta']['err'] = $err;
