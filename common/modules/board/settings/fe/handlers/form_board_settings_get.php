a525255a6f4a3c05b010c6997692f001a6d0eed5
odilitime <janesmith@airmail.cc>
2021-04-14 03:43:38 +0000

    move PIPELINE_BOARD_SETTING_GENERAL up, handle new board settings format

--- common/modules/board/settings/fe/handlers/form_board_settings_get.php
+++ common/modules/board/settings/fe/handlers/form_board_settings_get.php
@@ -11,6 +11,17 @@ $values = $pkg->useResource('list', array('boardUri' => $boardUri));
 global $pipelines;
 $fields = $shared['fields']; // imported from fe/common.php
 
+// handle hooks for additional settings
+$pipelines[PIPELINE_BOARD_SETTING_GENERAL]->execute($fields);
+
+// fields will keep the settings_ prefix
+// so we need to flatten our settings
+foreach($values['settings'] as $k => $v) {
+  $values['settings_' . $k] = $v;
+}
+unset($values['settings']);
+
+/*
 foreach($fields as $fn => $f) {
   if (substr($fn, 0, 9) === 'settings_') {
     //$k = substr($fn, 9);
@@ -20,9 +31,7 @@ foreach($fields as $fn => $f) {
     }
   }
 }
-
-// handle hooks for additional settings
-$pipelines[PIPELINE_BOARD_SETTING_GENERAL]->execute($fields);
+*/
 
 $html = generateForm($params['action'], $fields, $values);
 
