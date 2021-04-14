1c589d7cfb8d93de29aca88d144089e2b1be67e3
odilitime <janesmith@airmail.cc>
2021-04-14 03:37:26 +0000

    clean up notes

--- common/queue_implementations/base.php
+++ common/queue_implementations/base.php
@@ -5,20 +5,13 @@
 // well it could...
 // we would need a per frontend container...
 
-// persisent across requests
-// so like sessions but unique across the app not browser
-
 // could also plug into the cache/db drivers for backing stores...
-
-// table
-// pub/sub? lists
 
 // pub/sub
 interface queue_implementation_interface {
   function subscribe($queue);
   function receive($queue, $waitformsg = false);
   function send($queue, $message);
-
 }
 
 class queue_implementation_base_class implements queue_implementation_interface {
