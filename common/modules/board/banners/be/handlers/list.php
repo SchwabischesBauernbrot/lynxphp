528787fa35c402ac3f888173b453f09cce8eae79
odilitime <janesmith@airmail.cc>
2021-04-14 03:44:38 +0000

    boardData in meta

--- common/modules/board/banners/be/handlers/list.php
+++ common/modules/board/banners/be/handlers/list.php
@@ -6,11 +6,17 @@ if (!$boardData) {
   return sendResponse(array());
 }
 
-global $db, $models;
+global $db, $models, $tpp;
 $res = $db->find($models['board_banner'], array('criteria' => array(
   array('board_id', '=', $boardData['boardid']),
 )));
 $banners = $db->toArray($res);
-sendResponse($banners);
+// just pass through the settings for now...
+boardRowFilter($boardData, $boardData['json'], array('jsonFields' => 'settings'));
+// I don't think this is required
+$boardData['threadCount'] = getBoardThreadCount($boardData['uri']);
+$boardData['pageCount'] = ceil($boardData['threadCount']/$tpp);
+
+sendResponse($banners, 200, '', array('board' => $boardData));
 
 ?>
