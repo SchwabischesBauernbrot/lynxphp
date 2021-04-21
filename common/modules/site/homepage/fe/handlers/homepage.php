254616a4acbfa9ed8d8fe8d46ae573f7f8c298f5
odilitime <janesmith@airmail.cc>
2021-04-21 03:20:13 +0000

    load settings correctly, pass it through

--- common/modules/site/homepage/fe/handlers/homepage.php
+++ common/modules/site/homepage/fe/handlers/homepage.php
@@ -31,14 +31,14 @@ if (is_array($boards)) {
 }
 
 $logo = 'images/default_logo.png';
-if (!empty($settings['logo'])) {
+if (!empty($settings['site']['logo'])) {
   // FIXME: BACKEND_BASE_URL
-  $logo = 'backend/' . $settings['logo'];
+  $logo = 'backend/' . $settings['site']['logo'];
 }
 
 $tags = array(
-  'siteName' => empty($settings['siteName']) ? 'New PHPLynx Site' : $settings['siteName'],
-  'slogan' => empty($settings['slogan']) ? 'Go into <a href="admin/settings">Account > Admin interface > Settings</a> to set Name/Slogan' : $settings['slogan'],
+  'siteName' => empty($settings['site']['siteName']) ? 'New PHPLynx Site' : $settings['site']['siteName'],
+  'slogan' => empty($settings['site']['slogan']) ? 'Go into <a href="admin/settings">Account > Admin interface > Settings</a> to set Name/Slogan' : $settings['site']['slogan'],
   'logoURL' => $logo,
   'boards' => $boards_html,
 );
@@ -48,6 +48,7 @@ if (count($boards) > 10) {
   $content .= $moreBoards;
 }
 
-wrapContent($content);
+// , array('settings' => array('user'=>$settings, 'site'=>))
+wrapContent($content, array('settings' => $settings));
 
 ?>
