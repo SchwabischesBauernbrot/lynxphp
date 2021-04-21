6435c857fffb955ddf2365bbee2ccfa802d0f4ff
odilitime <janesmith@airmail.cc>
2021-04-21 03:22:18 +0000

    require BACKEND_KEY, make sure IN_TEST is set

--- backend/config.php
+++ backend/config.php
@@ -10,6 +10,13 @@ if (file_exists($localConfig)) {
 }
 
 // site wide config
+if (!defined('BACKEND_KEY') || BACKEND_KEY === '') {
+  echo json_encode(array(
+    'err' => 'BACKEND_KEY',
+    'message' => 'BACKEND_KEY is not set in the backend configuration. Used to hash things in the database, needs to be set before users are created!')
+  );
+  exit;
+}
 
 // CORS configuration
 
@@ -22,5 +29,6 @@ if (!defined('DB_USER')) define('DB_USER', 'root');
 if (!defined('DB_PWD')) define('DB_PWD', '');
 if (!defined('DB_NAME')) define('DB_NAME', 'lynxphp');
 if (!defined('DB_DRIVER')) define('DB_DRIVER', 'mysql');
+if (!defined('IN_TEST')) define('IN_TEST', false);
 
 ?>
