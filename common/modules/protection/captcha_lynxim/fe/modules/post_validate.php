71673bc63accbb1448c937b3c8d1315e6d1adf81
odilitime <janesmith@airmail.cc>
2021-04-21 04:41:51 +0000

    only enforce CAPTCHAs if we need to

--- common/modules/protection/captcha_lynxim/fe/modules/post_validate.php
+++ common/modules/protection/captcha_lynxim/fe/modules/post_validate.php
@@ -3,43 +3,71 @@
 $params = $getModule();
 
 // should this be enabled?
-$challenge = $_POST['captcha'];
-//echo "challenge[$challenge]<br>\n";
-if (empty($challenge)) {
-  $io['error'] = 'CAPTCHA is required';
-  return;
-}
-$captcha_id = $_POST['captcha_id'];
-if (empty($captcha_id)) {
-  $io['error'] = 'CAPTCHA has no ID';
-  return;
-}
-global $scratch, $now;
-$captchas = $scratch->get('captchas');
-if (!is_array($captchas)) {
-  $io['error'] = 'No CAPTCHAs active';
-  return;
+global $board_settings;
+//echo "Settings [", gettype($board_settings), print_r($board_settings, 1),"]<br>\n";
+if ($board_settings === false) {
+  // get board settings...
+  $boardData = getBoard($io['boardUri']);
+  // don't commit with this
+  if (isset($boardData['settings'])) {
+    $board_settings = $boardData['settings'];
+  }
 }
 
-if (!isset($captchas[$captcha_id])) {
-  $io['error'] = 'This CAPTCHA ID not found, please try again';
-  return;
-}
+if (isset($board_settings['captcha_mode'])) {
+  $mode = $board_settings['captcha_mode']; // posts / thread / no
+  $enable = false;
+  if ($io['endpoint'] === 'lynx/newThread') {
+    if ($mode !== 'no') {
+      $enable = true;
+    }
+  } else {
+    // Reply
+    if ($mode === 'posts') {
+      $enable = true;
+    }
+  }
 
-if ($captchas[$captcha_id]['expires'] < $now) {
-  $io['error'] = 'CAPTCHA expired, please try again';
-  unset($captchas[$captcha_id]);
-  $scratch->set('captchas', $captchas);
-  return;
-}
+  if ($enable) {
+    $challenge = $_POST['captcha'];
+    //echo "challenge[$challenge]<br>\n";
+    if (empty($challenge)) {
+      $io['error'] = 'CAPTCHA is required';
+      return;
+    }
+    $captcha_id = $_POST['captcha_id'];
+    if (empty($captcha_id)) {
+      $io['error'] = 'CAPTCHA has no ID';
+      return;
+    }
+    global $scratch, $now;
+    $captchas = $scratch->get('captchas');
+    if (!is_array($captchas)) {
+      $io['error'] = 'No CAPTCHAs active';
+      return;
+    }
 
-if (strtolower($challenge) !== $captchas[$captcha_id]['value']) {
-  $io['error'] = 'CAPTCHA is wrong, please try again';
-  return;
-}
+    if (!isset($captchas[$captcha_id])) {
+      $io['error'] = 'This CAPTCHA ID not found, please try again';
+      return;
+    }
 
-// success, remove it
-unset($captchas[$captcha_id]);
-$scratch->set('captchas', $captchas);
+    if ($captchas[$captcha_id]['expires'] < $now) {
+      $io['error'] = 'CAPTCHA expired, please try again';
+      unset($captchas[$captcha_id]);
+      $scratch->set('captchas', $captchas);
+      return;
+    }
+
+    if (strtolower($challenge) !== $captchas[$captcha_id]['value']) {
+      $io['error'] = 'CAPTCHA is wrong, please try again';
+      return;
+    }
+
+    // success, remove it
+    unset($captchas[$captcha_id]);
+    $scratch->set('captchas', $captchas);
+  }
+}
 
 ?>
