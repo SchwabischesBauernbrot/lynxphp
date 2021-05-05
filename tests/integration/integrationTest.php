df2af158f4fe6fcecd4e816fec17e22e663fda6a
odilitime <janesmith@airmail.cc>
2021-05-05 04:16:56 +0000

    frontend_lib change

--- tests/integration/integrationTest.php
+++ tests/integration/integrationTest.php
@@ -39,8 +39,8 @@ include '../common/post_vars.php';
 // how do we get the correct server name?
 // there is only one config on the frontend side...
 include 'config.php';
-include 'lib/lib.http.php';
-include 'lib/lib.backend.php';
+include '../frontend_lib/lib/lib.http.php';
+include '../frontend_lib/lib/lib.backend.php';
 chdir('..');
 
 function wrapContent($content) {
