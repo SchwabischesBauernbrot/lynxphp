19c21a6ba3ca5c066a68ff313f2c7d4807021e27
odilitime <janesmith@airmail.cc>
2021-05-07 03:15:40 +0000

    category support, change PIPELINE_MODULE_USER_SETTINGS_FIELDS (PIPELINE_BOARD_SETTING_GENERAL) api

--- common/modules/user/settings/fe/handlers/form_user_settings_get.php
+++ common/modules/user/settings/fe/handlers/form_user_settings_get.php
@@ -2,16 +2,32 @@
 
 $params = $getHandler();
 
+$category = 'general';
+if (isset($params['request']['params']['category'])) {
+  $category = $params['request']['params']['category'];
+}
+
 global $pipelines;
-$fields = $shared['fields']; // imported from shared.php
+if (!isset($shared['CategoryFields'][$category])) {
+  return wrapContent(renderUserPortalHeader() .
+    'Invalid user setting category<br>' . "\n");
+}
+$fields = $shared['CategoryFields'][$category]; // imported from shared.php
+
 // handle hooks for additional settings
-$pipelines[PIPELINE_BOARD_SETTING_GENERAL]->execute($fields);
+$io = array(
+  'category' => $category,
+  'fields' => $fields,
+  // could pass values
+);
+$pipelines[PIPELINE_MODULE_USER_SETTINGS_FIELDS]->execute($io);
 
 $data = $pkg->useResource('settings');
 $values = $data['settings'];
+//echo '<pre>values[', print_r($values, 1), "]</pre>\n";
 
-$html = generateForm($params['action'], $fields, $values);
+$html = generateForm($params['action'], $io['fields'], $values);
 
-wrapContent('User Settings'. $html);
+wrapContent(renderUserPortalHeader() . 'User Settings'. $html);
 
 ?>
