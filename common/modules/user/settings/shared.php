adbf939187bb46b89972730d463acd98df18f7fe
odilitime <janesmith@airmail.cc>
2021-05-07 03:14:30 +0000

    move out themes

--- common/modules/user/settings/shared.php
+++ common/modules/user/settings/shared.php
@@ -1,40 +1,17 @@
 <?php
 
 // this data and functions used for all module php code
-
 // function are automatically exported
-
 // allow export of data as $shared in your handlers and modules
+
 return array(
-  'fields' => array(
+  'CategoryFields' => array(
+    'general' => array (
+/*
     'current_theme' => array(
       'label' => 'Theme',
       'type'  => 'select',
       'options' => array(
-        'default' => 'Default',
-        'yotsuba-b' => 'Yotsuba-B',
-        'yotsuba' => 'Yotsuba',
-        'amoled' => 'Amoled',
-        'army-green' => 'Army-green',
-        'cancer' => 'Cancer',
-        'chaos' => 'Chaos',
-        'choc' => 'Choc',
-        'darkblue' => 'Dark Blue',
-        'gurochan' => 'Gurochan',
-        'lain' => 'Lain',
-        'miku' => 'Miku',
-        'mushroom' => 'Mushroom',
-        'navy' => 'Navy',
-        'pink' => 'Pink',
-        'rei-zero' => 'Robot',
-        'solarized-dark' => 'Solarized Dark',
-        'solarized-light' => 'Solarized Light',
-        'tempus-cozette' => 'Tempus Cozette',
-        'tomorrow' => 'Tomorrow',
-        'tomorrow2' => 'Tomorrow 2',
-        'vapor' => 'Vapor',
-        'win95' => 'Windows 95',
-        'snerx' => 'SnerxWerx',
       ),
     ),
     'code_theme' => array(
@@ -42,54 +19,59 @@ return array(
       'type'  => 'select',
       'options' => array(),
     ),
-    'name' => array(
-      'label' => 'Default post name',
-      'type'  => 'text',
+*/
+      'name' => array(
+        'label' => 'Default post name',
+        'type'  => 'text',
+        'placeholder' => 'anonymous',
+      ),
+      'postpass' => array(
+        'label' => 'Default post password',
+        'type'  => 'textpass',
+      ),
+      'volume' => array(
+        'label' => 'Default volume, enter number between 0 to 100',
+        'type'  => 'integer',
+      ),
+      'nsfw' => array(
+        'label' => 'Show Not Safe For Work content',
+        'type'  => 'checkbox',
+      ),
+      'noncolorids' => array(
+        'label' => 'Non color IDs',
+        'type'  => 'checkbox',
+      ),
+      'time' => array(
+        'label' => 'Display time as',
+        'type'  => 'select',
+        'options' => array(),
+      ),
+      'miltime' => array(
+        'label' => 'Use 24h (military) time',
+        'type'  => 'checkbox',
+      ),
+      'nojs' => array(
+        'label' => 'Disable all JavaScript',
+        'type'  => 'checkbox',
+      ),
+      // should be per board
+      'disablecustomcss' => array(
+        'label' => 'Disable board custom CSS',
+        'type'  => 'checkbox',
+      ),
+      'sitecustomcss' => array(
+        'label' => 'Site custom CSS',
+        'type'  => 'textarea',
+      ),
+      /*
+      'logo' => array(
+        'label' => 'Site Logo',
+        'type'  => 'image',
+      ),
+      */
     ),
-    'postpass' => array(
-      'label' => 'Default post password',
-      'type'  => 'password',
+    'theme' => array (
     ),
-    'volume' => array(
-      'label' => 'Default volume, enter number between 0 to 100',
-      'type'  => 'integer',
-    ),
-    'nsfw' => array(
-      'label' => 'Show Not Safe For Work content',
-      'type'  => 'checkbox',
-    ),
-    'noncolorids' => array(
-      'label' => 'Non color IDs',
-      'type'  => 'checkbox',
-    ),
-    'time' => array(
-      'label' => 'Display time as',
-      'type'  => 'select',
-      'options' => array(),
-    ),
-    'miltime' => array(
-      'label' => 'Use 24h (military) time',
-      'type'  => 'checkbox',
-    ),
-    'nojs' => array(
-      'label' => 'Disable all JavaScript',
-      'type'  => 'checkbox',
-    ),
-    // should be per board
-    'disablecustomcss' => array(
-      'label' => 'Disable board custom CSS',
-      'type'  => 'checkbox',
-    ),
-    'sitecustomcss' => array(
-      'label' => 'Site custom CSS',
-      'type'  => 'textarea',
-    ),
-    /*
-    'logo' => array(
-      'label' => 'Site Logo',
-      'type'  => 'image',
-    ),
-    */
   ),
 );
 
