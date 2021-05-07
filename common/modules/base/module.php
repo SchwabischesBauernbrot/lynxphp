71ccc9d17bebd334d6e27b6c30aedec04ab4e59f
odilitime <janesmith@airmail.cc>
2021-05-07 03:16:28 +0000

    move get_challenge/verify_account out of opt

--- common/modules/base/module.php
+++ common/modules/base/module.php
@@ -9,6 +9,7 @@ return array(
         'endpoint' => 'opt/work',
       ),
     ),
+    // admin settings
     array(
       'name' => 'settings',
       'params' => array(
@@ -27,6 +28,31 @@ return array(
         'requireSession' => true,
         'unwrapData' => true,
         'requires' => array('logo'),
+        'params' => 'postdata',
+      ),
+    ),
+    // login
+    array(
+      'name' => 'get_challenge',
+      'params' => array(
+        'endpoint' => 'opt/getChallenge',
+        'method' => 'POST',
+        'sendIP' => true,
+        'unwrapData' => true,
+        'requires' => array('i'),
+        'params' => 'postdata',
+      ),
+    ),
+    // should only work over TLS unless same ip/localhost
+    array(
+      'name' => 'verify_account',
+      'params' => array(
+        'endpoint' => 'opt/verifyAccount',
+        'method' => 'POST',
+        'sendIP' => true,
+        'unwrapData' => true,
+        // u, p are optional
+        'requires' => array('chal', 'sig'),
         'params' => 'postdata',
       ),
     ),
