48274253d8e0786a93357f91b0acb9bcf45f20f4
odilitime <janesmith@airmail.cc>
2021-05-07 03:29:39 +0000

    update posts_model prototype changes, move out signup/change_*/get_challenge routes, cache some get routes

--- backend/routes/opt.php
+++ backend/routes/opt.php
@@ -18,7 +18,10 @@ $router->get('/session', function($request) {
     return;
   }
   sendResponse(array('session' => 'ok'));
-});
+}, array(
+  'contentType' => 'application/json',
+  'databaseTables' => array('user_sessions'),
+));
 
 /*
 function handleCFE($request) {
@@ -36,12 +39,15 @@ $router->get('/boards/:uri/:page', function($request) {
   $boardUri = $request['params']['uri'];
   $pageNum = $request['params']['page'] ? (int)$request['params']['page'] : 1;
 
+
   $boardData = getBoard($boardUri, array('jsonFields' => 'settings'));
   if (!$boardData) {
     return sendResponse(array(), 404, 'Board does not exist');
   }
-  $threadCount = getBoardThreadCount($boardUri);
-  $threads = boardPage($boardUri, $pageNum);
+  $posts_model = getPostsModel($boardUri);
+
+  $threadCount = getBoardThreadCount($boardUri, $posts_model);
+  $threads = boardPage($boardUri, $pageNum, $posts_model);
   sendResponse(array(
     'board' => $boardData,
     'page1' => $threads,
@@ -49,7 +55,12 @@ $router->get('/boards/:uri/:page', function($request) {
     'threadCount' => $threadCount,
     'pageCount' => ceil($threadCount/$tpp),
   ));
-});
+}, array(
+  'contentType' => 'application/json',
+  'databaseTables' => array('user_sessions', 'board_{{uri}}_public_posts',
+    'board_{{uri}}_public_post_files', 'boards'
+  ),
+));
 
 // board data + thread data
 // would be good to include the banners data too
@@ -62,10 +73,11 @@ $router->get('/:board/thread/:thread', function($request) {
     echo '[]';
     return;
   }
+  $posts_model = getPostsModel($boardUri);
   $threadNum = (int)str_replace('.json', '', $request['params']['thread']);
-  $boardData['threadCount'] = getBoardThreadCount($boardUri);
+  $boardData['threadCount'] = getBoardThreadCount($boardUri, $posts_model);
   $boardData['pageCount'] = ceil($boardData['threadCount']/$tpp);
-  $boardData['posts'] = getThread($boardUri, $threadNum);
+  $boardData['posts'] = getThread($boardUri, $threadNum, $posts_model);
   sendResponse($boardData);
 });
 
@@ -114,11 +126,15 @@ $router->get('/boards.json', function($request) {
   foreach($boards as $b) {
     // FIXME: N+1s... (yea page is almost at 1s for 40 boards)
     // include posts, threads, last_activity
-    $b['threads'] = getBoardThreadCount($b['uri']); // 1 query
-    $b['posts'] = getBoardPostCount($b['uri']); // 1 query
+    $posts_model = getPostsModel($b['uri']);
+    if (!$posts_model) {
+      return sendResponse(array(), 500, 'Board database integrity error ' . $b['uri']);
+    }
+    $b['threads'] = getBoardThreadCount($b['uri'], $posts_model); // 1 query
+    $b['posts'] = getBoardPostCount($b['uri'], $posts_model); // 1 query
 
     if ($b['threads']) {
-      $posts_model = getPostsModel($b['uri']);
+      //$posts_model = getPostsModel($b['uri']);
       $newestThreadRes = $db->find($posts_model, array('criteria'=>array(
           array('threadid', '=', 0),
       ), 'limit' => '1', 'order'=>'updated_at desc')); // 1 query
@@ -165,47 +181,6 @@ $router->get('/perms/:perm', function($request) {
     'access' => $access,
     'user_id' => $user_id,
   ));
-});
-
-$router->post('/getChallenge', function($request) {
-  if (!hasPostVars(array('i'))) {
-    // hasPostVars already outputs
-    return;
-  }
-  include '../common/sodium/autoload.php';
-
-  // so you claim to have this identity, prove it
-  $edSrvKp = \Sodium\crypto_box_keypair();
-  $edSrvSk = \Sodium\crypto_box_secretkey($edSrvKp);
-  $edSrvPk = \Sodium\crypto_box_publickey($edSrvKp);
-  $token =  md5(uniqid());
-  $destEdPk = base64_decode($_POST['i']); // edPk stored as b64
-  $destXPkBin = \Sodium\crypto_sign_ed25519_pk_to_curve25519($destEdPk);
-
-  $symKey = \Sodium\crypto_box_keypair_from_secretkey_and_publickey(
-    $edSrvSk,
-    $destXPkBin
-  );
-  $iv = \Sodium\randombytes_buf(\Sodium\CRYPTO_BOX_NONCEBYTES);
-  $cipherText = \Sodium\crypto_box($token, $iv, $symKey);
-  global $db, $models, $now;
-  $db->insert($models['auth_challenge'], array(array(
-    'challenge' => $token,
-    'publickey' => $_POST['i'], // edPk stored as b64
-    'expires'   => (int)$now,
-    'ip'        => getip(),
-  )));
-
-  // also could send a server public key to encrypt the verify payload
-  // but the TLS transport should take care of that if needed
-  // generate id
-  $data = array(
-    'cipherText64' => base64_encode($iv . $cipherText),
-    'serverPubkey64' => base64_encode($edSrvPk),
-  );
-  // storage it temporarily w/expiration
-  // return it
-  sendResponse($data);
 });
 
 function verifyChallengedSignatureHandler() {
@@ -259,109 +234,6 @@ function loginResponseMaker($user_id, $upgradedAccount = false) {
   sendResponse($data);
 }
 
-// should only work over TLS unless same ip/localhost
-$router->post('/verifyAccount', function($request) {
-  $edPkBin = verifyChallengedSignatureHandler();
-  if (!$edPkBin) {
-    return;
-  }
-  global $db, $models;
-
-  // process account upgrades, remove code later
-  $upgradedAccount = false;
-  if (1) {
-    $u = strtolower(getOptionalPostField('u'));
-    //echo "u[$u]<br>\n";
-    if ($u && isset($_POST['p'])) {
-      $p = $_POST['p'];
-      //echo "Trying to locate user [$u]<br>\n";
-      $res = $db->find($models['user'], array('criteria' => array(
-        array('username', '=', $u),
-      )));
-      $row = $db->get_row($res);
-      $db->free($res);
-      //echo "id[", $row['userid'], "] pk[", $row['publickey'], "] p[$p]<br>\n";
-      if ($row && $row['userid'] && !$row['publickey'] && password_verify($p, $row['password']) && strpos($row['email'], '@') !== false) {
-        // convert users - ONLY DO THIS ONCE
-        // Should we clear out the email?
-        $db->updateById($models['user'], $row['userid'], array(
-          'username' => '', 'password' => '', 'publickey' => bin2hex($edPkBin),
-          'email' => hash('sha512', BACKEND_KEY . $row['email'] . BACKEND_KEY)));
-        $upgradedAccount = true;
-      }
-    }
-  }
-
-  $res = $db->find($models['user'], array('criteria' => array(
-    array('publickey', '=', bin2hex($edPkBin)),
-  )));
-  if (!$db->num_rows($res)) {
-    $db->free($res);
-    return sendResponse(array(), 401, 'Incorrect login - key is not registered, please sign up');
-  }
-  $row = $db->get_row($res);
-  $db->free($res);
-  $id = $row['userid'];
-  loginResponseMaker($id, $upgradedAccount);
-});
-
-$router->post('/registerAccount', function($request) {
-  $edPkBin = verifyChallengedSignatureHandler();
-  if (!$edPkBin) {
-    return;
-  }
-  global $db, $models;
-
-  $res = $db->find($models['user'], array('criteria' => array(
-    array('publickey', '=', bin2hex($edPkBin)),
-  )));
-  if ($db->num_rows($res)) {
-    // FIXME: should we just log in, they proved their key...
-    return sendResponse(array(), 403, 'Already registered');
-  }
-
-  //echo "Creating<br>\n";
-  $row = array('publickey' => bin2hex($edPkBin));
-  $em = getOptionalPostField('email');
-  if ($em) $row['email'] = hash('sha512', BACKEND_KEY . $em . BACKEND_KEY);
-  $id = $db->insert($models['user'], array($row));
-  loginResponseMaker($id);
-});
-
-$router->post('/migrateAccount', function($request) {
-  if (!hasPostVars(array('pk'))) {
-    // hasPostVars already outputs
-    return;
-  }
-  // require being logged in
-  $user_id = loggedIn();
-  if (!$user_id) {
-    return;
-  }
-  global $db, $models;
-  // pass it in as hex, so you can't easily correlate it with challenge request
-  $row = array('publickey' => $_POST['pk']);
-  $res = $db->updateById($models['user'], $user_id, $row);
-  sendResponse($res);
-});
-
-$router->post('/changeEmail', function($request) {
-  if (!hasPostVars(array('em'))) {
-    // hasPostVars already outputs
-    return;
-  }
-  $em = $_POST['em'];
-  // require being logged in
-  $user_id = loggedIn();
-  if (!$user_id) {
-    return;
-  }
-  global $db, $models;
-  $row = array('email' => hash('sha512', BACKEND_KEY . $em . BACKEND_KEY));
-  $res = $db->updateById($models['user'], $user_id, $row);
-  sendResponse($res);
-});
-
 // has to be last...
 // non-standard 4chan api - lets disable for now
 // /opt should have replaced this
@@ -373,7 +245,8 @@ $router->get('/:board', function($request) {
     echo '[]';
     return;
   }
-  $boardData['threadCount'] = getBoardThreadCount($boardUri);
+  $posts_model = getPostsModel($boardUri);
+  $boardData['threadCount'] = getBoardThreadCount($boardUri, $posts_model);
   $boardData['pageCount'] = ceil($boardData['threadCount']/$tpp);
   sendResponse($boardData);
 });
