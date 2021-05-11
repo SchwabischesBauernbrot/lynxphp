40584748ad206ef469257d52a8e19af1cc50f155
odilitime <janesmith@airmail.cc>
2021-05-11 04:01:56 +0000

    select general if no cat passed in

--- frontend_lib/handlers/mixins/user_portal.php
+++ frontend_lib/handlers/mixins/user_portal.php
@@ -1,14 +1,19 @@
 <?php
 
 function renderUserPortalHeader() {
-  return renderPortalHeader('user', array(
+  $test = substr($_SERVER['REQUEST_URI'], 1);
+  $portalOptions = array(
     'headerPipeline' => PIPELINE_USER_HEADER_TMPL,
     'navPipeline'    => PIPELINE_USER_NAV,
     'navItems' => array(
       'general' => 'user/settings/general',
       'theme' => 'user/settings/theme',
     ),
-  ));
+  );
+  if ($test === 'user/settings') {
+    $portalOptions['useNavFirstItem'] = true;
+  }
+  return renderPortalHeader('user', $portalOptions);
 }
 
 ?>
