01be84700b399862771fc0e808f2429b4a24a6cf
Odilitime <janesmith@airmail.cc>
2021-05-11 03:10:50 +0000

    new file

---
+++ common/modules/site/themes/fe/modules/site_head.php
@@ -0,0 +1,35 @@
+<?php
+
+$params = $getModule();
+
+$userSettings = $io['userSettings'];
+
+$allThemes = $shared['themes'];
+
+unset($allThemes['default']); // erase default as a possible final option
+$themes = array_keys($allThemes);
+
+// FIXME: set default theme in siteSettings
+// translate default to first theme...
+if (empty($userSettings['current_theme']) || $userSettings['current_theme'] === 'default') $userSettings['current_theme'] = $themes[0];
+
+if (!empty($allThemes[$userSettings['current_theme']])) {
+  $themesHtml = '<link id="theme" rel="stylesheet" data-theme="' . $theme . '" href="css/themes/' . $userSettings['current_theme'] . '.css">';
+}
+
+/*
+$themesHtml = '';
+foreach($themes as $theme) {
+  //echo $userSettings['current_theme'], '===', $theme, "<br>\n";
+  if ($userSettings['current_theme'] === $theme) {
+    $themesHtml .= '<link id="theme" rel="stylesheet" data-theme="' . $theme . '" href="css/themes/' . $theme . '.css">';
+  } else {
+    // these are always downloaded in chrome... ugh
+    //$themesHtml .= '<link rel="alternate stylesheet" type="text/css" data-theme="' . $theme . '" title="' . $theme . '" href="css/themes/' . $theme . '.css">';
+  }
+}
+*/
+
+$io['head_html'] .= $themesHtml;
+
+?>
