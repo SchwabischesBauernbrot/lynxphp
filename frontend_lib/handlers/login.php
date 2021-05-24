61b428ab8087217e0184f6aed443de73a9f5161e
odilitime <janesmith@airmail.cc>
2021-05-24 23:12:58 +0000

    use

--- frontend_lib/handlers/login.php
+++ frontend_lib/handlers/login.php
@@ -1,15 +1,16 @@
 <?php
 
 function getLoginForm($goto = false) {
+  global $BASE_HREF;
   $templates = loadTemplates('login');
   if ($goto === false) $goto = '';
   $tags = array(
     // force TLS
-    'action' => 'https://' . BASE_HOST . BASE_HREF . 'forms/login',
+    'action' => 'https://' . BASE_HOST . $BASE_HREF . 'forms/login',
     'goto' => $goto,
     // FIXME get named route
-    'forgot_account' => BASE_HREF . 'forgot_account',
-    'signup' => BASE_HREF . 'signup',
+    'forgot_account' => $BASE_HREF . 'forgot_account',
+    'signup' => $BASE_HREF . 'signup',
   );
   return replace_tags($templates['header'], $tags);
 }
@@ -23,7 +24,8 @@ function getLogin() {
 function getLogout() {
   setcookie('session', '', 1, '/');
   //wrapContent('You are now logged out');
-  redirectTo(BASE_HREF . 'forms/login');
+  global $BASE_HREF;
+  redirectTo($BASE_HREF . 'forms/login');
 }
 
 function postLogin() {
@@ -42,13 +44,14 @@ function postLogin() {
     //echo "goto[", $_POST['goto'], "]<br>\n";
     //redirectTo('/control_panel.php');
     //wrapContent('Login successful');
+    global $BASE_HREF;
     if (!empty($_POST['goto'])) {
       // I wonder if we should filter this
       // only needed if we ever check HTTP_REFERRER
       // FIXME get named route
-      redirectTo(BASE_HREF . $_POST['goto']);
+      redirectTo($BASE_HREF . $_POST['goto']);
     } else {
-      redirectTo(BASE_HREF . 'control_panel');
+      redirectTo($BASE_HREF . 'control_panel');
     }
   } else {
     $tmpl = "Error: Log In incorrect or other error: " . print_r($res, 1) . "<br>\n";
