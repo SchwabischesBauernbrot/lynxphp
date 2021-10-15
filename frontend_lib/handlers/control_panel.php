d8974ce3c3f412111ad6f0d29be8034bf61fb619
odilitime <janesmith@airmail.cc>
2021-10-15 04:58:30 +0000

    move out a bunch into modules, getAccountSettings => getAccountSettingsHandler, fix URLs

--- frontend_lib/handlers/control_panel.php
+++ frontend_lib/handlers/control_panel.php
@@ -7,7 +7,7 @@ function getControlPanel() {
   // you only get meta if error
   if (!$account || (!empty($account['meta']) && $account['meta']['code'] === 401)) {
     // FIXME get named route
-    redirectTo($BASE_HREF . 'forms/login');
+    redirectTo($BASE_HREF . 'forms/login.html');
     return;
   }
 
@@ -26,10 +26,10 @@ function getControlPanel() {
     $isGlobal = in_array('global', $account['groups']);
     // FIXME get named route
     if ($isAdmin) {
-      $admin_html = str_replace('{{admin}}', $BASE_HREF . 'admin', $admin_tmpl);
+      $admin_html = str_replace('{{admin}}', $BASE_HREF . 'admin.php', $admin_tmpl);
     }
     if ($isGlobal) {
-      $global_html = str_replace('{{global}}', $BASE_HREF . 'global', $global_tmpl);
+      $global_html = str_replace('{{global}}', $BASE_HREF . 'global.php', $global_tmpl);
     }
   }
 
@@ -45,9 +45,9 @@ function getControlPanel() {
   $tags = array(
     'login' => $account['login'],
     // FIXME get named route
-    'account' => $BASE_HREF . 'account',
-    'create_board' => $BASE_HREF . 'create_board',
-    'logout' => $BASE_HREF . 'logout',
+    'account' => $BASE_HREF . 'account.php',
+    'create_board' => $BASE_HREF . 'create_board.html',
+    'logout' => $BASE_HREF . 'logout.php',
     'admin' => $isAdmin ? $admin_html : '',
     'global' => $isGlobal ? $global_html : '',
     'ownedBoards' => $boards_html,
@@ -55,115 +55,7 @@ function getControlPanel() {
   wrapContent(replace_tags($tmpl, $tags));
 }
 
-function getCreateBoardForm() {
-  $formFields = array(
-    'uri' => array('type' => 'text', 'label' => 'Board URI'),
-    'title' => array('type' => 'text', 'label' => 'Board title'),
-    'description' => array('type' => 'textarea', 'label' => 'Board description'),
-  );
-  // FIXME: pipeline
-  // FIXME get named route
-  global $BASE_HREF;
-  return simpleForm($BASE_HREF . 'create_board', $formFields, 'Create board');
-}
-
-function getCreateBoard() {
-  wrapContent(getCreateBoardForm());
-}
-
-function postCreateBoard() {
-  $result = backendCreateBoard();
-  if ($result['data'] === 'ok') {
-    // maybe not display this?
-    //wrapContent('Board created!');
-    // FIXME get named route
-    global $BASE_HREF;
-    redirectTo($BASE_HREF . 'control_panel');
-    /*
-    $uri = $_POST['uri'];
-    redirectTo($uri . '/settings');
-    */
-    return;
-  }
-  $tmpl = "Error: Board creation error: " . $result['meta']['err'] . "<br>\n";
-  wrapContent($tmpl . getCreateBoardForm());
-}
-
-function getAccountPortalNav() {
-  global $BASE_HREF;
-  // FIXME get named route
-  $navItems = array(
-    'Change username/password' => $BASE_HREF . 'account/change_userpass',
-    'Change recovery email' => $BASE_HREF . 'account/change_email',
-  );
-  // FIXME: pipeline
-  return getNav($navItems);
-}
-
-function getAccountPortal($options = false) {
-  return array(
-    'header' => '' . getAccountPortalNav(),
-    'footer' => ''
-  );
-}
-
-function getChangeUserPassForm() {
-  // set up form
-  $formFields = array(
-    'username' => array('type' => 'text', 'label' => 'New Username'),
-    'password' => array('type' => 'password', 'label' => 'New Password (Minimum 16 chars, we recommend using a pass phrase)'),
-  );
-  global $BASE_HREF;
-  // FIXME: pipeline
-  // FIXME get named route
-  return simpleForm($BASE_HREF . 'account/change_userpass', $formFields, 'Migrate account');
-}
-
-function getChangeEmailForm() {
-  // set up form
-  $formFields = array(
-    'email' => array('type' => 'email', 'label' => 'Recovery Email (we suggest using a burner/temp one)'),
-  );
-  global $BASE_HREF;
-  // FIXME: pipeline
-  // FIXME get named route
-  return simpleForm($BASE_HREF . 'account/change_email', $formFields, 'Change recovery email');
-}
-
-function getChangeUserPass() {
-  $accountPortal = getAccountPortal();
-  wrapContent($accountPortal['header'] . getChangeUserPassForm() . $accountPortal['footer']);
-}
-
-function postChangeUserPass() {
-  $eKp = getEdKeypair($user, $pass);
-  $res = backendMigrateAccount($eKp['pk']);
-  if (!empty($res['data'])) {
-    // FIXME get named route
-    global $BASE_HREF;
-    redirectTo($BASE_HREF . 'account?message=' . urlencode('Account migrated'));
-  } else {
-    wrapContent('Error: ' . print_r($res) . getChangeEmailForm());
-  }
-}
-
-function getChangeEmail() {
-  $accountPortal = getAccountPortal();
-  wrapContent($accountPortal['header'] . getChangeEmailForm() . $accountPortal['footer']);
-}
-
-function postChangeEmail() {
-  $res = backendChangeEmail($_POST['email']);
-  if (!empty($res['data'])) {
-    // FIXME get named route
-    global $BASE_HREF;
-    redirectTo($BASE_HREF . 'account?message=' . urlencode('Recovery email changed'));
-  } else {
-    wrapContent('Error: ' . print_r($res) .  getChangeEmailForm());
-  }
-}
-
-function getAccountSettings() {
+function getAccountSettingsHandler() {
   $msg = getQueryField('message');
   wrapContent($msg . getAccountPortalNav());
 }
