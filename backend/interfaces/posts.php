d013b4297ead6beabf72e050e05aabb9bc91d9bd
odilitime <janesmith@airmail.cc>
2021-10-15 04:23:48 +0000

    handle post states correctly, getPost()

--- backend/interfaces/posts.php
+++ backend/interfaces/posts.php
@@ -35,6 +35,16 @@ function postDBtoAPI(&$row) {
   $row['no'] = $row['postid'];
   unset($row['postid']);
   unset($row['json']);
+  // ensure frontend doesn't have to worry about database differences
+  $bools = array('deleted', 'sticky', 'closed');
+  foreach($bools as $f) {
+    // follow 4chan spec
+    if ($db->isTrue($row[$f])) {
+      $row[$f] = 1;
+    } else {
+      unset($row[$f]);
+    }
+  }
   // decode user_id
 }
 
@@ -117,6 +127,85 @@ function getThread($boardUri, $threadNum, $posts_model) {
   }
   $posts = array_values($posts);
   return $posts;
+}
+
+function getPost($boardUri, $postNo, $posts_model) {
+  global $db;
+  if ($posts_model === false) {
+    $posts_model = getPostsModel($boardUri);
+    if ($posts_model === false) {
+      // this board does not exist
+      sendResponse(array(), 404, 'Board not found');
+      return;
+    }
+  }
+  $post_files_model = getPostFilesModel($boardUri);
+
+  //$filesFields = array_keys($post_files_model['fields']);
+  //$filesFields[] = 'fileid';
+
+  $filesFields = array('postid', 'sha256', 'path', 'browser_type', 'mime_type',
+    'type', 'filename', 'size', 'ext', 'w', 'h', 'filedeleted', 'spoiler',
+    'tn_w', 'tn_h', 'fileid');
+
+  // get OP
+  // FIXME: only gets first image on OP
+  // why? /:board/thread/:thread uses this too
+  /*
+  $res = $db->find($posts_model, array('criteria'=>array(
+    array('postid', '=', $postNo),
+    array('deleted', '=', 0),
+  )));
+  $row = $db->get_row($res);
+  */
+  $posts = array();
+  $row = $db->findById($posts_model, $postNo);
+  if ($row['deleted']) return array();
+  //echo "<pre>Thread/File", print_r($row, 1), "</pre>\n";
+  if (!isset($posts[$row['postid']])) {
+    //echo "<pre>Thread", print_r($row, 1), "</pre>\n";
+    $posts[$row['postid']] = $row;
+    postDBtoAPI($posts[$row['postid']]);
+    //echo "<pre>4chan", print_r($row, 1), "</pre>\n";
+    $posts[$row['postid']]['files'] = array();
+  }
+  if (!empty($row['file_fileid'])) {
+    if (!isset($posts[$row['postid']]['files'][$row['file_fileid']])) {
+      $frow = $row;
+      fileDBtoAPI($frow, $boardUri);
+      $posts[$row['postid']]['files'][$row['file_fileid']] = $frow;
+    }
+  }
+  //$db->free($res);
+
+  /*
+  $res = $db->find($posts_model, array('criteria'=>array(
+    array('threadid', '=', $postNo),
+    array('deleted', '=', 0),
+  ), 'order' => 'created_at'));
+  while($row = $db->get_row($res)) {
+    //echo "<pre>", print_r($row, 1), "</pre>\n";
+    $orow = $row;
+    if (!isset($posts[$row['postid']])) {
+      postDBtoAPI($row);
+      $posts[$row['no']] = $row;
+      $posts[$row['no']]['files'] = array();
+    }
+    if (!empty($orow['file_fileid'])) {
+      if (!isset($posts[$orow['postid']]['files'][$orow['file_fileid']])) {
+        $frow = $orow;
+        fileDBtoAPI($frow, $boardUri);
+        $posts[$orow['postid']]['files'][$orow['file_fileid']] = $frow;
+      }
+    }
+  }
+  */
+  foreach($posts as $pk => $p) {
+    $posts[$pk]['files'] = array_values($posts[$pk]['files']);
+  }
+  $posts = array_values($posts);
+  $post = count($posts) ? $posts[0] : array();
+  return $post;
 }
 
 // option.deleteReplies:bool
