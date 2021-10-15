e808194d151138302e49d63de6c9812d3f9f36f9
odilitime <janesmith@airmail.cc>
2021-10-15 04:55:29 +0000

    remove frontendData, move import into base and change parameters here

--- frontend/router.php
+++ frontend/router.php
@@ -13,58 +13,8 @@ class FrontendRouter extends Router {
     // front doesn't need to set up backend routes
   }
   */
-  // dynamic vs static (captcha/banners)
-  // expiration: backend routes, files
-  function import($routes) {
-    foreach($routes as $group => $groupData) {
-      // groupData: file, routes
-      foreach($groupData['routes'] as $routeData) {
-        // routeData: func, options, loggedin(, method, route)
-        $method = empty($routeData['method']) ? 'GET' : $routeData['method'];
-        //echo "method[$method][", $routeData['route'], "]<br>\n";
-        if (isset($this->methods[$method][$routeData['route']])) {
-          echo "frontend_router::import - Warning, route already defined<br>\n";
-        }
-        $route = $routeData['route'];
-        $this->methods[$method][$route] = function($request) use ($routeData, $groupData) {
-          $file = empty($groupData['file']) ? false : $groupData['file'];
-          if ($file && empty($this->included[$file])) {
-            include '../frontend_lib/handlers/' . $file . '.php';
-            $this->included[$file] = true;
-          }
-          if ($routeData['func']) {
-            $func = $routeData['func'];
-            $func($request);
-          } else {
-            echo "No function defined<br>\n";
-          }
-        };
-        if (isset($routeData['options'])) {
-          $this->routeOptions[$method . '_' . $routeData['route']] = $routeData['options'];
-        }
-        // trade some cpu for memory
-        if (isset($routeData['route'])) unset($routeData['route']);
-        if (isset($routeData['method'])) unset($routeData['method']);
-        // normalize some options
-        //if (!isset($routeData['options'])) $routeData['options'] = array();
-        //if (!isset($routeData['loggedIn'])) $routeData['loggedIn'] = false;
-
-        $methodRoute = $method . '_' . $route;
-
-        // move loggedIn and cacheSettings into routeOptions?
-        if (isset($routeData['cacheSettings'])) {
-          $this->routeOptions[$methodRoute]['cacheSettings'] = $routeData['cacheSettings'];
-          unset($routeData['cacheSettings']);
-        }
-        if (isset($routeData['loggedIn'])) {
-          $this->routeOptions[$methodRoute]['loggedin'] = $routeData['loggedIn'];
-          unset($routeData['loggedIn']);
-        }
-        $this->routeOptions[$methodRoute]['module'] = 'frontend';
-        $this->routeOptions[$methodRoute]['name'] = $group;
-        $this->routeOptions[$methodRoute]['address'] = $routeData['func'] . '@' . $groupData['file'];
-      }
-    }
+  function import($routes, $module = 'frontend', $dir = '../frontend_lib/handlers') {
+    return parent::import($routes, $module, $dir);
   }
   // should extractTokens be a separate function?
   function extractParams($route) {
@@ -102,12 +52,6 @@ class FrontendRouter extends Router {
     //echo "skipDontGen[$skipDontGen]\n";
     $res = array();
     foreach($this->methods[$method] as $r => $f) {
-      if (isset($this->frontendData[$method . '_' . $r]['route'])) {
-        $rData = $this->frontendData[$method . '_' . $r]['route'];
-        //print_r($rData);
-        if ($skipLoggedIn && !empty($rData['loggedIn'])) continue;
-        if ($skipDontGen && !empty($rData['dontGen'])) continue;
-      }
       $rOpts = array();
       if (isset($this->routeOptions[$method . '_' . $r])) {
         $rOpts = $this->routeOptions[$method . '_' . $r];
