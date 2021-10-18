bcacccd7997de40adc2c179dd7bd508922dc567e
odilitime <janesmith@airmail.cc>
2021-10-18 22:22:21 +0000

    getAccountPortal(), getAccountPortalNav(), link ext html/php

--- frontend_lib/handlers/mixins/user_portal.php
+++ frontend_lib/handlers/mixins/user_portal.php
@@ -6,14 +6,34 @@ function renderUserPortalHeader() {
     'headerPipeline' => PIPELINE_USER_HEADER_TMPL,
     'navPipeline'    => PIPELINE_USER_NAV,
     'navItems' => array(
-      'general' => 'user/settings/general',
-      'theme' => 'user/settings/theme',
+      'general' => 'user/settings/general.html',
+      'theme' => 'user/settings/theme.html',
     ),
   );
   if ($test === 'user/settings') {
     $portalOptions['useNavFirstItem'] = true;
   }
   return renderPortalHeader('user', $portalOptions);
+}
+
+// was happy in control_panel
+// but now modules need to be able to call it too
+function getAccountPortalNav() {
+  global $BASE_HREF;
+  // FIXME get named route
+  $navItems = array(
+    'Change username/password' => $BASE_HREF . 'account/change_userpass',
+    'Change recovery email' => $BASE_HREF . 'account/change_email',
+  );
+  // FIXME: pipeline
+  return getNav($navItems);
+}
+
+function getAccountPortal($options = false) {
+  return array(
+    'header' => '' . getAccountPortalNav(),
+    'footer' => ''
+  );
 }
 
 ?>
