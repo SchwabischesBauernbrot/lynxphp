3ca760b91fcd5d699120c527efe5126fdd258633
odilitime <janesmith@airmail.cc>
2021-11-18 22:47:54 +0000

    move out getThreadHandler/getThread, note

--- frontend_lib/handlers/boards.php
+++ frontend_lib/handlers/boards.php
@@ -226,6 +226,7 @@ function getOverboardHandler() {
   wrapContent($content);
 }
 
+// probably shoudl be moved into a lib or inlined...
 function preprocessPost(&$p) {
   global $pipelines;
   $pipelines[PIPELINE_POST_PREPROCESS]->execute($p);
@@ -348,12 +349,6 @@ function getBoardSettingsHandler($request) {
   getBoardSettings($boardUri);
 }
 
-function getThreadHandler($request) {
-  $boardUri = $request['params']['uri'];
-  $threadNum = str_replace('.html', '', $request['params']['num']);
-  getThread($boardUri, $threadNum);
-}
-
 function makePostHandler($request) {
   global $pipelines, $max_length;
   $boardUri = $request['params']['uri'];
@@ -451,80 +446,6 @@ function getBoardThreadListing($boardUri, $pagenum = 1) {
   getBoardThreadListingRender($boardUri, $boardThreads, $pagenum);
 }
 
-function getThread($boardUri, $threadNum) {
-  $threadNum = (int)$threadNum;
-  $templates = loadTemplates('thread_details');
-  $tmpl = $templates['header'];
-
-  //$boardNav_template = $templates['loop0'];
-  $file_template = $templates['loop1'];
-  $hasReplies_template = $templates['loop2'];
-  $reply_template = $templates['loop3'];
-  $post_template = $templates['loop4'];
-
-  /*
-  $tmp = $boardNav_template;
-  $tmp = str_replace('{{uri}}', $boardUri, $tmp);
-  $boardnav_html = $tmp;
-  */
-
-  $boardData = getBoardThread($boardUri, $threadNum);
-
-  foreach($boardData['posts'] as $j => $post) {
-    preprocessPost($boardData['posts'][$j]);
-  }
-  global $pipelines;
-  $data = array(
-    'posts' => $boardData['posts'],
-    'boardData' => $boardData,
-    'threadNum' => $threadNum
-  );
-  $pipelines[PIPELINE_POST_POSTPREPROCESS]->execute($data);
-
-  $posts_html = '';
-  $files = 0;
-  foreach($boardData['posts'] as $post) {
-    //echo "<pre>", print_r($post, 1), "</pre>\n";
-    $tmp = $post_template;
-    $posts_html .= renderPost($boardUri, $post, array('checkable' => true));
-    $files += count($post['files']);
-  }
-
-  $p = array(
-    'boardUri' => $boardUri,
-    'tags' => array(
-      // need this for form actions
-      'uri' => $boardUri,
-      'threadNum' => $threadNum,
-      'title' => htmlspecialchars($boardData['title']),
-      'description' => htmlspecialchars($boardData['description']),
-      //$tmpl = str_replace('{{boardNav}}', $boardnav_html, $tmpl);
-      'posts' => $posts_html,
-      'replies' => count($boardData['posts']) - 1,
-      'files' => $files,
-      // mixins
-      //'postform' => renderPostForm($boardUri, $boardUri . '/catalog'),
-      'postactions' => renderPostActions($boardUri),
-    )
-  );
-  global $pipelines;
-  $pipelines[PIPELINE_BOARD_DETAILS_TMPL]->execute($p);
-  $tmpl = replace_tags($tmpl, $p['tags']);
-
-  // FIXME: move into a pipeline
-  $closed = false;
-  if (count($boardData['posts'])) {
-    $closed = empty($boardData['posts'][0]['closed']) ? false : true;
-  }
-
-  $boardPortal = getBoardPortal($boardUri, $boardData, array(
-    'isThread' => true,
-    'threadNum' => $threadNum,
-    'threadClosed' => $closed,
-  ));
-  wrapContent($boardPortal['header'] . $tmpl . $boardPortal['footer']);
-}
-
 function renderBoardCatalog($boardUri) {
   $data = getBoardCatalog($boardUri);
   $catalog = $data['pages'];
@@ -618,6 +539,7 @@ function renderBoardCatalog($boardUri) {
   //$boardData = getBoard($boardUri);
   //$boardData['pageCount'] = $boardThreads['pageCount'];
   $boardData['pageCount'] = $maxPage;
+  // but no footer...
   $boardHeader = renderBoardPortalHeader($boardUri, $boardData, array(
     'isCatalog' => true,
   ));
