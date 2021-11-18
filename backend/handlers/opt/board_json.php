18ef007426cc53c7755a3ed2fe9d06517e4afb9e
odilitime <janesmith@airmail.cc>
2021-11-18 21:35:15 +0000

    use sendResponse2

--- backend/handlers/opt/board_json.php
+++ backend/handlers/opt/board_json.php
@@ -6,10 +6,12 @@ global $db, $models, $tpp;
 $boardUri = str_replace('.json', '', $request['params']['board']);
 $boardData = getBoard($boardUri, array('jsonFields' => 'settings'));
 if (!$boardData) {
-  echo '[]';
-  return;
+  return sendResponse2(array(), array(
+    'code' => 404,
+    'err'  => 'Board does not exist',
+  ));
 }
 $posts_model = getPostsModel($boardUri);
 $boardData['threadCount'] = getBoardThreadCount($boardUri, $posts_model);
 $boardData['pageCount'] = ceil($boardData['threadCount']/$tpp);
-sendResponse($boardData);
+sendResponse2($boardData);
