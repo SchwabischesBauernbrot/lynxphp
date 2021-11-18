4b513281377159a2402f930475544b40415dc0e4
odilitime <janesmith@airmail.cc>
2021-11-18 23:49:11 +0000

    not need for variable

---
+++ common/modules/base/site/settings/fe/data.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+  array(
+    'handlers' => array(
+    ),
+    'forms' => array(
+      array(
+        'route'   => '/admin/settings',
+        'handler' => 'admin_settings',
+      ),
+    ),
+    'modules' => array(
+      /*
+      // add [Banner] to board naviagtion
+      array(
+        'pipeline' => 'PIPELINE_BOARD_NAV',
+        'module' => 'nav',
+      ),
+      */
+    ),
+  ),
+);
+
+?>
