550248ffb96dec91fc161acbc85525d43d57f7b3
odilitime <janesmith@airmail.cc>
2021-11-18 21:50:13 +0000

    getPost fix delete flag check, move out getThread, postDBtoAPI decode json for cyclic

--- backend/interfaces/posts.php
+++ backend/interfaces/posts.php
@@ -34,6 +34,17 @@ function postDBtoAPI(&$row) {
 
   $row['no'] = $row['postid'];
   unset($row['postid']);
+
+  $data = json_decode($row['json'], true);
+
+  // FIXME: pipeline
+  if (empty($data['cyclic'])) {
+    // is this even needed
+    unset($row['cyclic']);
+  } else {
+    $row['cyclic'] = 1;
+  }
+
   unset($row['json']);
   // ensure frontend doesn't have to worry about database differences
   $bools = array('deleted', 'sticky', 'closed');
@@ -46,87 +57,6 @@ function postDBtoAPI(&$row) {
     }
   }
   // decode user_id
-}
-
-function getThread($boardUri, $threadNum, $posts_model) {
-  global $db;
-  if ($posts_model === false) {
-    $posts_model = getPostsModel($boardUri);
-    if ($posts_model === false) {
-      // this board does not exist
-      sendResponse(array(), 404, 'Board not found');
-      return;
-    }
-  }
-  $post_files_model = getPostFilesModel($boardUri);
-
-  //$filesFields = array_keys($post_files_model['fields']);
-  //$filesFields[] = 'fileid';
-
-  $filesFields = array('postid', 'sha256', 'path', 'browser_type', 'mime_type',
-    'type', 'filename', 'size', 'ext', 'w', 'h', 'filedeleted', 'spoiler',
-    'tn_w', 'tn_h', 'fileid');
-
-
-  $posts_model['children'] = array(
-    array(
-      'type' => 'left',
-      'model' => $post_files_model,
-      'pluck' => array_map(function ($f) { return 'ALIAS.' . $f . ' as file_' . $f; }, $filesFields)
-    )
-  );
-
-  // get OP
-  // FIXME: only gets first image on OP
-  // why? /:board/thread/:thread uses this too
-  $res = $db->find($posts_model, array('criteria'=>array(
-    array('postid', '=', $threadNum),
-  )));
-  $posts = array();
-  while($row = $db->get_row($res)) {
-    //echo "<pre>Thread/File", print_r($row, 1), "</pre>\n";
-    if (!isset($posts[$row['postid']])) {
-      //echo "<pre>Thread", print_r($row, 1), "</pre>\n";
-      $posts[$row['postid']] = $row;
-      postDBtoAPI($posts[$row['postid']]);
-      //echo "<pre>4chan", print_r($row, 1), "</pre>\n";
-      $posts[$row['postid']]['files'] = array();
-    }
-    if (!empty($row['file_fileid'])) {
-      if (!isset($posts[$row['postid']]['files'][$row['file_fileid']])) {
-        $frow = $row;
-        fileDBtoAPI($frow, $boardUri);
-        $posts[$row['postid']]['files'][$row['file_fileid']] = $frow;
-      }
-    }
-  }
-  $db->free($res);
-
-  $res = $db->find($posts_model, array('criteria'=>array(
-    array('threadid', '=', $threadNum),
-    array('deleted', '=', 0),
-  ), 'order' => 'created_at'));
-  while($row = $db->get_row($res)) {
-    //echo "<pre>", print_r($row, 1), "</pre>\n";
-    $orow = $row;
-    if (!isset($posts[$row['postid']])) {
-      postDBtoAPI($row);
-      $posts[$row['no']] = $row;
-      $posts[$row['no']]['files'] = array();
-    }
-    if (!empty($orow['file_fileid'])) {
-      if (!isset($posts[$orow['postid']]['files'][$orow['file_fileid']])) {
-        $frow = $orow;
-        fileDBtoAPI($frow, $boardUri);
-        $posts[$orow['postid']]['files'][$orow['file_fileid']] = $frow;
-      }
-    }
-  }
-  foreach($posts as $pk => $p) {
-    $posts[$pk]['files'] = array_values($posts[$pk]['files']);
-  }
-  $posts = array_values($posts);
-  return $posts;
 }
 
 function getPost($boardUri, $postNo, $posts_model) {
@@ -160,8 +90,8 @@ function getPost($boardUri, $postNo, $posts_model) {
   */
   $posts = array();
   $row = $db->findById($posts_model, $postNo);
-  if ($row['deleted']) return array();
   //echo "<pre>Thread/File", print_r($row, 1), "</pre>\n";
+  if ($db->isTrue($row['deleted'])) return array();
   if (!isset($posts[$row['postid']])) {
     //echo "<pre>Thread", print_r($row, 1), "</pre>\n";
     $posts[$row['postid']] = $row;
