73891f45f6e87511f70bace37377b8f8bbac541d
odilitime <janesmith@airmail.cc>
2021-11-18 22:48:10 +0000

    link BE routes

--- frontend_lib/handlers/mixins/admin_portal.php
+++ frontend_lib/handlers/mixins/admin_portal.php
@@ -8,6 +8,7 @@ function renderAdminPortal() {
     'navItems' => array(
       'Settings' => 'admin/settings.html',
       'FE Routes' => 'admin/fe_routes.php',
+      'BE Routes' => 'admin/be_routes.php',
       'Modules' => 'admin/modules.php',
       // should be iframe'd or more integrated...
       'System' => 'admin/install.php',
