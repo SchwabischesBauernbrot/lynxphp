caa4da83d7e11eb7c42029877a33dd564d7a8c7f
odilitime <janesmith@airmail.cc>
2021-11-18 22:34:33 +0000

    add forms

---
+++ common/modules/user/change_email/fe/data.php
@@ -0,0 +1,43 @@
+<?php
+
+return array(
+  'user_change_email' => array(
+    'handlers' => array(
+    ),
+    'forms' => array(
+      array(
+        'portal' => 'account',
+        'route' => '/account/change_email',
+        'handler' => 'change_email',
+        'options' => array(
+          'get_options'=> array(
+            'cacheSettings' => array(
+              'files' => array(
+                'templates/header.tmpl', // wrapContent
+                'templates/footer.tmpl', // wrapContent
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+    'modules' => array(
+      /*
+      // add [users] to admin nav
+      array(
+        'pipeline' => 'PIPELINE_ADMIN_NAV',
+        'module'   => 'nav',
+      ),
+      */
+    ),
+    'pipelines' => array(
+      /*
+      array(
+        'name' => 'PIPELINE_MODULE_USER_SETTINGS_GENERAL',
+      ),
+      */
+    ),
+  ),
+);
+
+?>
