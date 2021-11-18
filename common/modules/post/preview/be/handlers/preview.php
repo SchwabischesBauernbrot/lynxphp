cfd715c06077abd6186cdf51de560655a59f82b0
odilitime <janesmith@airmail.cc>
2021-11-18 22:16:45 +0000

    implement preview

--- common/modules/post/preview/be/handlers/preview.php
+++ common/modules/post/preview/be/handlers/preview.php
@@ -4,9 +4,43 @@
 
 $params = $get();
 
-sendResponse(array(
-  'success' => 'ok',
-  'params' => print_r($params, 1),
-));
+$boardUri = $params['params']['board'];
+/*
+$boardData = getBoardByUri($boardUri);
+if (!$boardData) {
+  sendResponse(array(), 404, 'Board does not exist');
+  return;
+}
+*/
+
+$id = ''.(int)str_replace('.json', '', $params['params']['id']);
+if (!$id) {
+  sendResponse(array(), 400, 'Invalid ID');
+  return;
+}
+
+/*
+$posts_model = getPostsModel($boardUri);
+if ($posts_model === false) {
+  // this board does not exist
+  sendResponse(array(), 404, 'Board not found');
+  return;
+}
+$post_files_model = getPostFilesModel($boardUri);
+*/
+
+$post = getPost($boardUri, $id, false);
+/*
+$posts = array_filter($posts, function($p) use ($id) {
+  //echo "p[", print_r($p, 1) ,"] id[$id]<Br>\n";
+  return $p['no'] === $id;
+});
+$post = count($posts) ? $posts[0] : array();
+*/
+
+$res = array(
+  'success' => 'ok', 'final' => $post, 'boardUri' => $boardUri, 'id' => $id,
+);
+sendRawResponse($res);
 
 ?>
