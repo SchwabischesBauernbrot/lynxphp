739f2cf8fb583068d13eef4c2f10d5021e917a0f
odilitime <janesmith@airmail.cc>
2021-11-18 22:28:43 +0000

    try to add cacheSettings

--- common/modules/site/homepage/fe/data.php
+++ common/modules/site/homepage/fe/data.php
@@ -7,6 +7,23 @@ $fePkgs = array(
         'method'  => 'GET',
         'route'   => '/',
         'handler' => 'homepage',
+        'cacheSettings' => array(
+          'resource' => array(
+            'homepage',
+          ),
+          'files' => array(
+            // theme is also would affect this caching
+            'templates/header.tmpl', // wrapContent
+            '../common/modules/site/homepage/fe/views/index.tmpl', // homepage
+            'templates/footer.tmpl', // wrapContent
+          ),
+          'config' => array(
+            'BACKEND_PUBLIC_URL',
+          ),
+          'sets' => array(
+            'wrapContent'
+          ),
+        ),
       ),
     ),
     'forms' => array(),
