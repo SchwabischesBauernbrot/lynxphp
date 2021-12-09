a311cc2f31e033caccc91bff9812b57678c5b8fb
Odilitime <janesmith@airmail.cc>
2021-12-09 02:06:56 +0000

    new module

---
+++ common/modules/post/preview/fe/modules/post_link.php
@@ -0,0 +1,21 @@
+<?php
+
+$params = $getModule();
+
+//print_r($io);
+
+// FIXME: SEO slug
+
+// nginx
+// REDIRECT_URL
+// REQUEST_URI
+// PATH_INFO
+// PATH_TRANSLATED
+// PHPS_ELF
+$on_preview_page = strpos($_SERVER['REQUEST_URI'], '/preview/') !== false;
+if (!$on_preview_page) {
+  $io['links'][] = array('label' => '[Preview]', 'link' => '/' . $io['boardUri'] . '/preview/' . $io['p']['no']);
+}
+// else we already have No and number on the page...
+
+?>
