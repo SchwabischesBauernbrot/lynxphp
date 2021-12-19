88426b64bb6ae04e66f3c4c2f34ffd030af8602b
Odilitime <janesmith@airmail.cc>
2021-12-19 08:03:39 +0000

    handle multiple forms

--- frontend/js/jschan/counter.js
+++ frontend/js/jschan/counter.js
@@ -1,36 +1,64 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-	const messageBox = document.getElementById('message');
+  function hookMessageBox(messageBox) {
+    const maxLength = messageBox.getAttribute('maxlength')
+    const minLength = messageBox.getAttribute('minlength')
+    let currentLength = messageBox.value.length
 
-	if (messageBox) {
-		const messageBoxLabel = messageBox.previousSibling;
-		const maxLength = messageBox.getAttribute('maxlength');
-		const minLength = messageBox.getAttribute('minlength');
-		let currentLength = messageBox.value.length;
-		const counter = document.createElement('small');
-		messageBoxLabel.appendChild(counter);
+    //const messageBoxLabel = messageBox.previousSibling
+    //const counter = document.createElement('small')
+    //messageBoxLabel.appendChild(counter)
 
-		const updateCounter = () => {
-			counter.innerText = `(${currentLength}/${maxLength})`;
-			if (currentLength >= maxLength || currentLength < minLength) {
-				counter.style.color = 'red';
-			} else {
-				counter.removeAttribute('style');
-			}
-		};
+    const updateCounter = (counter) => {
+      counter.innerText = `(${currentLength}/${maxLength})`
+      if (currentLength >= maxLength || currentLength < minLength) {
+        counter.style.color = 'red'
+      } else {
+        counter.removeAttribute('style')
+      }
+    }
 
-		const updateLength = function(e) {
-			if (messageBox.value.length > maxLength) {
-				messageBox.value = messageBox.value.substring(0,maxLength);
-			}
-			currentLength = messageBox.value.length;
-			updateCounter();
-		};
+    const updateCounters = () => {
+      const counters = document.getElementsByClassName('messageCounter')
+      for(var i in counters) {
+        if (counters.hasOwnProperty(i)) {
+          updateCounter(counters[i])
+        }
+      }
+    }
 
-		updateCounter();
+    const updateState = function(e) {
+      // normalize/validate input
+      if (messageBox.value.length > maxLength) {
+        messageBox.value = messageBox.value.substring(0, maxLength)
+      }
+      currentLength = messageBox.value.length
 
-		messageBox.addEventListener('input', updateLength);
+      const messageBoxes = document.querySelectorAll('textarea[name=message]')
+      for(var i in messageBoxes) {
+        if (messageBoxes.hasOwnProperty(i)) {
+          // sync value
+          messageBoxes[i].value = messageBox.value
+        }
+      }
 
-	}
+      updateCounters()
+    }
 
-});
+    if (maxLength || minLength) {
+      updateCounters()
+      messageBox.addEventListener('input', updateState)
+    } else {
+      //console.log('counter.js - no message length limits set')
+    }
+  }
+
+  const messageBoxes = document.querySelectorAll('textarea[name=message]')
+  //if (!messageBoxes.length) console.warn('counter.js - no message boxes')
+  for(var i in messageBoxes) {
+    if (messageBoxes.hasOwnProperty(i)) {
+      hookMessageBox(messageBoxes[i])
+    }
+  }
+
+})
