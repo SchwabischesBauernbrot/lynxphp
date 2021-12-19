a5dcedb774a702617e3449ea08c3024bddf2c4c0
Odilitime <janesmith@airmail.cc>
2021-12-19 08:49:09 +0000

    maintain structure of btLookups, so on an empty result, we don't break expectations

--- common/modules/post/text/fe/modules/postpreformat.php
+++ common/modules/post/text/fe/modules/postpreformat.php
@@ -6,8 +6,24 @@ global $btLookups;
 if (!(isset($btLookups) && is_array($btLookups) && count($btLookups))) {
   return;
 }
+
+//echo "<pre>btLookups in[", print_r($btLookups, 1), "]</pre>\n";
+$inbtLookups = $btLookups; // copies
+
 $btLookups = $pkg->useResource('boardthreadlookup', $btLookups);
 
-//echo "<pre>[", print_r($btLookups, 1), "]</pre>\n";
+// ensure the structure is set
+foreach($inbtLookups as $board => $posts) {
+  //echo "looking [$board]<br>\n";
+  if (!isset($btLookups[$board])) {
+    $btLookups[$board] = array();
+  }
+  foreach($posts as $p => $t) {
+    if (!isset($btLookups[$board][$p])) {
+      $btLookups[$board][$p] = false;
+    }
+  }
+}
+//echo "<pre>btLookups out[", print_r($btLookups, 1), "]</pre>\n";
 
 ?>
