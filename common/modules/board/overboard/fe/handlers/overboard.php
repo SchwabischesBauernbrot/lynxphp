60620bcc896faac298c4bb9e5e598db9ab72b4b1
odilitime <janesmith@airmail.cc>
2021-12-24 03:29:57 +0000

    guards

--- common/modules/board/overboard/fe/handlers/overboard.php
+++ common/modules/board/overboard/fe/handlers/overboard.php
@@ -13,14 +13,17 @@ $thread_template = $templates['loop5'];
 
 $boards_html = '';
 $overboardData = $pkg->useResource('overboard');
+//echo "<pre>overboardData[", print_r($overboardData, 1), "]</pre>\n";
 
 $nPosts = array();
-foreach($overboardData['threads'] as $i => $t) {
-  if (!isset($t['posts'])) continue;
-  foreach($t['posts'] as $j => $post) {
-    //echo "<pre>post[", print_r($post, 1), "]</pre>\n";
-    preprocessPost($overboardData['threads'][$i]['posts'][$j]);
-    $nPosts[] = $post;
+if (isset($overboardData['threads'])) {
+  foreach($overboardData['threads'] as $i => $t) {
+    if (!isset($t['posts'])) continue;
+    foreach($t['posts'] as $j => $post) {
+      //echo "<pre>post[", print_r($post, 1), "]</pre>\n";
+      preprocessPost($overboardData['threads'][$i]['posts'][$j]);
+      $nPosts[] = $post;
+    }
   }
 }
 
@@ -50,21 +53,22 @@ $tags = array(
 $content = replace_tags($templates['header'], $tags);
 
 $threads_html = '';
-foreach($overboardData['threads'] as $thread) {
-  if (!isset($thread['posts'])) continue;
-  //echo "<pre>thread[", print_r($thread, 1), "]</pre>\n";
-  $posts = $thread['posts'];
-  //echo "count[", count($posts), "]<br>\n";
-  $bUri = $thread['boardUri'];
-  // we use base tag I believe...
-  $threads_html .= '<h2><a href="/' . $bUri . '/">&gt;&gt;&gt;/' . $bUri . '/</a></h2>' . $threadhdr_template;
-  foreach($posts as $i => $post) {
-    $threads_html .= renderPost($bUri, $post, array(
-      'checkable' => true, 'postCount' => $thread['thread_reply_count'],
-      'inMixedBoards' => true
-    ));
+if (isset($overboardData['threads'])) {
+  foreach($overboardData['threads'] as $thread) {
+    if (!isset($thread['posts'])) continue;
+    //echo "<pre>thread[", print_r($thread, 1), "]</pre>\n";
+    $posts = $thread['posts'];
+    //echo "count[", count($posts), "]<br>\n";
+    $bUri = $thread['boardUri'];
+    // we use base tag I believe...
+    $threads_html .= '<h2><a href="/' . $bUri . '/">&gt;&gt;&gt;/' . $bUri . '/</a></h2>' . $threadhdr_template;
+    foreach($posts as $i => $post) {
+      $threads_html .= renderPost($bUri, $post, array(
+        'checkable' => true, 'postCount' => $thread['thread_reply_count'],
+        'inMixedBoards' => true
+      ));
+    }
+    $threads_html .= $threadftr_template;
   }
-  $threads_html .= $threadftr_template;
 }
-
 wrapContent($threads_html);
