397d9e305ccfde71dbf851d2ecb3e697289ffaad
Odilitime <janesmith@airmail.cc>
2022-02-28 06:59:08 +0000

    rel tag in report links

--- frontend_lib/lib/lib.http.php
+++ frontend_lib/lib/lib.http.php
@@ -174,7 +174,7 @@ function curl_log_report() {
 
     $clickUrl = str_replace(BACKEND_BASE_URL, BACKEND_PUBLIC_URL, $l['url']);
 
-    echo '<li>' . $m . ' <a target=_blank href="' . $clickUrl . $joinChar[$hasQ] . 'prettyPrint=1">' . $l['url'] . '</a> took ' . $l['took'] . 'ms';
+    echo '<li>' . $m . ' <a target=_blank rel=noopener href="' . $clickUrl . $joinChar[$hasQ] . 'prettyPrint=1">' . $l['url'] . '</a> took ' . $l['took'] . 'ms';
     if ($m === 'POST' && isset($l['postData'])) {
       if (is_array($l['postData'])) {
         echo ' [', print_r($l['postData'], 1), ']';
