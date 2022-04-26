6c4777af5ced182e7656c23a66e77273baab2e8b
odilitime <janesmith@airmail.cc>
2022-04-26 02:36:38 +0000

    new settings tag format, queueIt() refactor, use post_queue() refactor, fix boardData

--- common/modules/post/queuing/be/modules/newpost_process.php
+++ common/modules/post/queuing/be/modules/newpost_process.php
@@ -7,33 +7,56 @@ $boardData = getBoard($boardUri, array('jsonFields' => 'settings'));
 
 // FIXME: how do we handle reloads? i.e. multiple posts of the same thing
 
-if (!isset($boardData['json']['settings']['queueing_mode'])) {
-  $boardData['json']['settings']['queueing_mode'] = '';
+if (!isset($boardData['settings']['queueing_mode'])) {
+  $boardData['settings']['queueing_mode'] = '';
 }
 
-if ($boardData['settings']['queueing_mode'] === 'community') {
+$tags = $io['p']['tags'];
+
+// FIXME: move into newpost_tag pipeline
+foreach($boardData['settings']['post_queueing'] as $t => $mode) {
+  if ($mode && in_array($t, $tags)) {
+    if ($mode === 'com') {
+      // add this tag
+      $io['p']['tags']['queue_com'] = true;
+    } else
+    if ($mode === 'mod') {
+      // add this tag
+      $io['p']['tags']['queue_mod'] = true;
+    } else {
+      // remove all these tags
+      $io['p']['tags']['queue_com'] = false;
+      $io['p']['tags']['queue_mod'] = false;
+    }
+  }
+}
+
+function queueIt($boardUri, $io, $type) {
+  $threadid = $io['p']['threadid'];
+  $ip = $io['priv']['ip'];
+  $data = array(
+    'post'  => $io['p'],
+    'files' => json_decode($io['files'], true), // don't need to double wrap this
+    'priv'  => $io['priv'],
+  );
+  $id = post_queue($boardUri, $ip, $threadid, $data, $type);
+  return $id;
+}
+
+// FIXME: maybe explain what tags triggered the queueing?
+if ($io['p']['tags']['queue_com']) {
   $io['addToPostsDB'] = false;
-  global $db, $models;
-  $id = $db->insert($models['post_queue'], array(array(
-    'board_uri' => $boardUri,
-    'post'  => json_encode($io['p']),
-    'files' => $io['files'],
-    'type'  => 'com',
-  )));
+  // queue is defaulting com...
+  $id = queueIt($boardUri, $io, 'com');
   $io['returnId'] = array(
     'status' => 'queued',
     'as' => $id,
   );
 } else
-if ($boardData['settings']['queueing_mode'] === 'moderator') {
+if ($io['p']['tags']['queue_mod']) {
   $io['addToPostsDB'] = false;
-  global $db, $models;
-  $id = $db->insert($models['post_queue'], array(array(
-    'board_uri' => $boardUri,
-    'post'  => json_encode($io['p']),
-    'files' => $io['files'],
-    'type' => 'mod',
-  )));
+  // queue is defaulting com...
+  $id = queueIt($boardUri, $io, 'mod');
   $io['returnId'] = array(
     'status' => 'queued',
     'as' => $id,
