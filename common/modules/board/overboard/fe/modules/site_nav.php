1574108105fcd4dd5570095d428e787780177155
odilitime <janesmith@airmail.cc>
2022-04-26 09:47:47 +0000

    getNav2 format, locate after bords/blank

--- common/modules/board/overboard/fe/modules/site_nav.php
+++ common/modules/board/overboard/fe/modules/site_nav.php
@@ -2,15 +2,21 @@
 
 $params = $getModule();
 
-/*
-if (0) {
+$navItem = array('label' => 'Overboard', 'destinations' => 'overboard.html');
+
+if (1) {
+  // FIXME: something better, maybe more like WP-menus
   $n = array();
-  foreach($io['navItems'] as $l => $us) {
-    if ($l === 'Boards') $n['Overboard'] = 'overboard.html';
-    $n[$l] = $us;
+  $added = false;
+  foreach($io['navItems'] as $i) {
+    $n[] = $i;
+    // put after boards
+    if (!$added && (!isset($i['label']) || $i['label'] === 'Boards')) {
+      $n[] = $navItem;
+      $added = true;
+    }
   }
   $io['navItems'] = $n;
 } else {
-*/
-$io['navItems']['Overboard'] = 'overboard.html';
-//}
+  $io['navItems'][] = $navItem;
+}
