940079ad4c49b7b51c5d7e5ae5eeebe8117afc2c
odilitime <janesmith@airmail.cc>
2022-04-26 21:33:07 +0000

    boardOwnerMiddleware() allow admins or globals

--- frontend_lib/lib/middlewares.php
+++ frontend_lib/lib/middlewares.php
@@ -8,6 +8,14 @@ function boardOwnerMiddleware($request) {
   // do we own this board?
   $account = backendLynxAccount();
   $ok = false;
+
+  // are we an admin or global?
+  if (isset($account['groups'])) {
+    if (in_array('admin', $account['groups']) || in_array('global', $account['groups'])) {
+      $ok = true;
+    }
+  }
+
   if (isset($account['ownedBoards']) && is_array($account['ownedBoards'])) {
     foreach($account['ownedBoards'] as $board) {
       if ($board === $boardUri) {
@@ -16,6 +24,7 @@ function boardOwnerMiddleware($request) {
       }
     }
   }
+  // FIXME: board vols...
   if (!$ok) {
     wrapContent('You do not own this board');
     return;
