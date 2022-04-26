3732be9f3e5b9e34af3e56f194d8a54b985eb2c9
odilitime <janesmith@airmail.cc>
2022-04-26 09:49:19 +0000

    makeIframeContents(), implement iframe mode

--- frontend_lib/lib/expand.php
+++ frontend_lib/lib/expand.php
@@ -1,5 +1,11 @@
 <?php
 
+function makeIframeContents($aHref, $aLabel) {
+  // no single quotes allowed (because we're quoting with single-quotes)
+  $html = '<a class="nojsonly-block" style="line-height: 100vh; text-align: center; width: 100vw; height: 100vh;" target="boardFrame" href="' . $aHref .'">' . $aLabel . '</a>';
+  return $html;
+}
+
 /*
 <details id="boardsNav">
   <summary class="nav-item">Boards</a></summary>
@@ -20,6 +26,7 @@
 */
 // iframeId*
 // label is basically thumbnail tag (getThumbnail)
+// too maybe ids and targets, should have one key drive them all
 function getExpander($label, $content, $options = array()) {
   extract(ensureOptions(array(
     'type' => 'media', // media/iframe
@@ -30,42 +37,47 @@ function getExpander($label, $content, $options = array()) {
     // use proper css...
     //'divStyle' => false,
     'iframeId' => false,
+    'iframeName' => '', // js? targeting?
     'iframeTitle' => '', // accessibility score
-    'iframeBorder' => true,
+    // probably could leave this off and let css style it
+    'iframeBorder' => true, // maybe 0 or 1?
     // I think this can be always hard coded
     //'aId' = false,
+    // when would this not be iframeName?
     'target' => false, // has to be unique per page...
+    // these two could be moved into $content tbh
     'aLabel' => 'Please click to load all the full html for this section',
     'aHref'  => '/', // almost required
+    // sizes are requied to position / size correctly
     'tn_sz' => array(),
     'sz'    => array(),
 
     'classes' => array(),
-    'labelId' => false,
+    'labelId' => false, // similar to detailsId
       'styleContentUrl' => false,
   ), $options));
 
   $style = '';
   if ($labelId && $styleContentUrl) {
-/*
-    $style = '<style>
-details[open].img#' . $labelId . ' > summary::after {
-  content: url(' . $styleContentUrl . ');
-}
-</style>';
-*/
-$r = 1;
-if ($sz[1]) {
-  if ($sz[1] < $sz[0]) {
-    $r = ($sz[1] / $sz[0]) * 100;
-  } else {
-    $r = ($sz[0] / $sz[1]) * 100;
-  }
-}
-// ' . $sz[1] . 'px
+    /*
+        $style = '<style>
+    details[open].img#' . $labelId . ' > summary::after {
+      content: url(' . $styleContentUrl . ');
+    }
+    </style>';
+    */
+    $r = 1;
+    if ($sz[1]) {
+      if ($sz[1] < $sz[0]) {
+        $r = ($sz[1] / $sz[0]) * 100;
+      } else {
+        $r = ($sz[0] / $sz[1]) * 100;
+      }
+    }
+    // ' . $sz[1] . 'px
 
-// only after click will collapse it
-// background-image: url(' . $styleContentUrl . ');
+    // only after click will collapse it
+    // background-image: url(' . $styleContentUrl . ');
     $style = '<style>
 details[open].img#' . $labelId . ' > summary::after {
   content: \'\';
@@ -95,20 +107,45 @@ details[open].img#' . $labelId . ' .contentarea {
   z-index: -1;
 */
   $id = $labelId !== false ? ' id="' . $labelId . '"' : '';
-  $classes[] = 'nojsonly-block';
-  $class = count($classes) ? ' class="' . join(' ', $classes) . '"' : '';
 
-  // nojs
-  $html = $style;
-  $html .= '<details' . $id . $class . '>' . "\n";
-  $html .= '<summary>' . $label . '</summary>' . "\n";
-  $html .= $content . "\n";
-  $html .= '<div class="contentarea"></div></details>' . "\n";
-  // for js
-  // we need to reserve the space to avoid relayout
-  // we can't just have JS insert these later...
-  // or manipulate the html above...
-  $html .= '<a class="jsonly" href="' . $styleContentUrl . '">' . $label . '</a>';
+  if ($type === 'iframe') {
+    // iframe type
+    /*
+    meta tag refresh loads the content onload, instead of waiting for it to be used
+    iframe title="boards list subframe" id="boardsSubpageFrame" frameborder=0 name="boardFrame" style="width: 100vw; height: 50vh;" src="/boards_cacheable.html"></iframe
+    what so bad if it's always loading from the cache?
+    #div is a js loading destination zone
+    #boardsSubpage 50% is weird, maybe 1/3 or just go full screen... ?
+    #boardsSubpageFrame class="nojsonly-block"  display: block;
+    */
+    // FIXME: base class
+    // FIXME: class for details/div/iframe (remove style)
+    $html = '
+    <details id="' . $detailsId . '" class="detailsClass" style="display: inline-block">
+      <summary class="' . $summaryClass . '">' . $label . '</summary>
+      <div id="' . $divId . '" style="position: absolute; top: 32px; left: 0; z-index: 1; padding: 5px; background-color: var(--background-rest);">
+        <iframe title="' . $iframeTitle . '" id="' . $iframeId . '" frameborder=0 name="' . $iframeName . '" style="width: 100vw; height: 50vh;" srcdoc=\'<a class="nojsonly-block" style="line-height: 100vh; text-align: center; width: 100vw; height: 100vh;" target="' . $target . '" href="' . $aHref .'">' . $aLabel . '</a>\'></iframe>
+      </div>
+    </details>
+    ';
+  } else {
+    // media
+
+    // nojs
+    $classes[] = 'nojsonly-block';
+    $class = count($classes) ? ' class="' . join(' ', $classes) . '"' : '';
+    $html = $style;
+    $html .= '<details' . $id . $class . '>' . "\n";
+    $html .= '<summary>' . $label . '</summary>' . "\n";
+    $html .= $content . "\n";
+    $html .= '<div class="contentarea"></div></details>' . "\n";
+
+    // for js
+    // we need to reserve the space to avoid relayout
+    // we can't just have JS insert these later...
+    // or manipulate the html above...
+    $html .= '<a class="jsonly" href="' . $styleContentUrl . '">' . $label . '</a>';
+  }
   return $html;
 }
 
