f1b487476bff291a7de671491595ba55392b28bd
odilitime <janesmith@airmail.cc>
2022-04-26 02:31:09 +0000

    getYourVotes(), getYourQueue(), getYourNextQueue(), post_queue(), post_dequeue(), track

--- common/modules/post/queuing/be/common.php
+++ common/modules/post/queuing/be/common.php
@@ -6,8 +6,142 @@ function getIdentity() {
   $userid = getUserID(); // are we logged in?
   if ($userid) return 'user_' .  $userid;
   $sid = getServerField('HTTP_SID');
-  // does it matter if it's valid or expired? no it doesn't
+  // it doesn't matter if it's valid or expired
+  // we just need something to track by
   return 'session_' . $sid;
 }
+
+function getYourVotes() {
+  // generate identity
+  $id = getIdentity();
+  //$ip = getip();
+
+  // get a list of our votes
+  global $db, $models;
+  $res = $db->find($models['post_queue_vote'], array(
+    'criteria' => array(
+      //'ip' => $ip,
+      'id' => $id,
+    ),
+  ), 'queueid');
+  $votes = $db->toArray($res);
+  $nqis = array();
+  foreach($votes as $v) {
+    $nqis[] = $v['queueid'];
+  }
+  return $nqis;
+}
+
+function getYourQueue($boardUri, $votes = false) {
+  // then cancel out queueid
+  //print_r($votes);
+  global $db, $models;
+
+  if (!$votes) {
+    $votes = getYourVotes();
+  }
+
+  $ip = getip();
+  $crit = array('board_uri' => $boardUri,
+// disable for development
+//    array('ip', '!=', $ip,),
+  );
+  if (count($votes)) {
+    // remove any we've already voted on
+    $crit[] = array('queueid', 'not in', $votes);
+  }
+  //print_r($crit);
+  // find a random post
+  $res = $db->find($models['post_queue'], array(
+    'criteria' => $crit,
+    'order' => 'random()',
+    'limit' => 1,
+  ));
+  $qps = $db->toArray($res);
+  return $qps;
+}
+
+function getYourNextQueue($boardUri, $votes = false) {
+  $qps = getYourQueue($boardUri, $votes);
+  if (!count($qps)) {
+    return false;
+  }
+  return $qps[0];
+}
+
+// post -> queue
+// why notthread_id here too?
+function post_queue($boardUri, $ip, $thread_id, $data, $type) {
+  global $db, $models;
+  $id = $db->insert($models['post_queue'], array(array(
+    'board_uri' => $boardUri,
+    'thread_id' => $thread_id,
+    'ip' => $ip,
+    'type' => $type,
+    'data'  => json_encode($data),
+  )));
+  return $id;
+}
+
+// queue -> post
+function post_dequeue($queueid) {
+  global $db, $models;
+  // get post/files data
+  $qp = $db->findById($models['post_queue'], $queueid);
+  // queueid, post, json, created_at, updated_at, type, board_uri,
+  // files, thread_id
+  //echo "<pre>qp[", print_r($qp, 1), "]</pre>\n";
+
+  // insert into board posts
+  $boardUri = $qp['board_uri']; // just incase we voted across boards
+  $data = json_decode($qp['data'], true);
+
+  $posts_model = getPostsModel($boardUri);
+  $id = 0;
+  $okToNuke = true;
+  if ($posts_model && $data) {
+    // FIXME: pipelines?
+
+    $id = createPost($boardUri, $data['post'], json_encode($data['files']), $data['priv']);
+    // $id maybe an array with file issues...
+
+    /*
+    $id = $db->insert($posts_model, array($data['post']));
+
+    $posts_priv_model = getPrivatePostsModel($boardUri);
+    $privPost = $data['priv'];
+    $privPost['postid'] = $id; // update postid
+    $db->insert($posts_priv_model, array($privPost));
+
+    // handle files
+    processFiles($boardUri, $data['files'], $qp['thread_id'] ? $qp['thread_id'] : $id, $id);
+
+    // bump board
+    global $now;
+    $inow = (int)$now;
+    $urow = array('last_thread' => $inow, 'last_post' => $inow);
+    //echo "bumping[$boardUri] [", print_r($urow, 1), "]<br>\n";
+    $db->update($models['board'], $urow, array('criteria' => array(
+      'uri' => $boardUri,
+    )));
+
+    // do we need to bump thread?
+    if ($qp['thread_id']) {
+      // bump thread
+      $urow = array();
+      //echo "bumping thread[", $qp['thread_id'] ,"] [", print_r($urow, 1), "]<br>\n";
+      $db->update($posts_model, $urow, array('criteria'=>array(
+        'postid' => $qp['thread_id'],
+      )));
+    }
+    */
+  } else {
+    // an honest attempt hasn't been made yet...
+    $okToNuke = false;
+  }
+  return $okToNuke;
+}
+
+return true;
 
 ?>
