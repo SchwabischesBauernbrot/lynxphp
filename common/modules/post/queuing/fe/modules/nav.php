dca5f36c5ef7a8d2ef64d66d879addde533cff5b
odilitime <janesmith@airmail.cc>
2022-04-26 02:39:40 +0000

    new tag system, try to get count from portalData

--- common/modules/post/queuing/fe/modules/nav.php
+++ common/modules/post/queuing/fe/modules/nav.php
@@ -2,13 +2,42 @@
 
 $params = $getModule();
 
-//print_r($io['boardSettings']);
+$show = false;
+
+// see if any of $io['boardSettings']['post_queueing'] are set to com
+foreach($io['boardSettings']['post_queueing'] as $t => $action) {
+  if ($action === 'com' || $action === 'mod') {
+    if ($action === 'com') {
+      $show = true;
+    } else {
+      // mod
+      // or if you're logged in as a mod...
+      $show = true;
+    }
+    break;
+  }
+}
+
+// only show there's content in the queue
+//print_r($io['boardSettings']['post_queueing']);
 
 // only show it if we need to
-if (isset($io['boardSettings']['queueing_mode']) &&
-    $io['boardSettings']['queueing_mode'] === 'community') {
+if ($show) {
+  // we'd have to make a backend call to check on every board page
+  // maybe using JS to solve this is better?
+  global $portalData;
+  $count_html = '';
+  if (isset($portalData['board']['post_queueing']['count'])) {
+    if ($portalData['board']['post_queueing']['count']) {
+      $count_html = ' (' . $portalData['board']['post_queueing']['count'] . ')';
+    } else {
+      // skip including it because there's nothing to moderate (at a community level?)
+      return;
+    }
+  }
+  // just always show it for now
   $boardUri = $io['boardUri'];
-  $io['navItems']['[Moderate]'] = $boardUri . '/moderate.html';
+  $io['navItems']['Moderate' . $count_html] = $boardUri . '/moderate.html';
 }
 
 ?>
