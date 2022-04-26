e09e4d0b790bdb84c4d14cf595e616c1cfa5cb48
odilitime <janesmith@airmail.cc>
2022-04-26 02:30:02 +0000

    no preview if post no is not set

--- common/modules/post/preview/fe/modules/post_link.php
+++ common/modules/post/preview/fe/modules/post_link.php
@@ -13,7 +13,10 @@ $params = $getModule();
 // PATH_TRANSLATED
 // PHPS_ELF
 $on_preview_page = strpos($_SERVER['REQUEST_URI'], '/preview/') !== false;
-if (!$on_preview_page) {
+// incase it's a queue'd preview
+$has_post_num = $io['p']['no'] ? true : false;
+
+if (!$on_preview_page && $has_post_num) {
   $io['links'][] = array('label' => '[Preview]', 'link' => '/' . $io['boardUri'] . '/preview/' . $io['p']['no'] . '.html');
 }
 // else we already have No and number on the page...
