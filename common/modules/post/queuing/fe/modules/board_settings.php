1f81425bb91bce106936c8bb0017e8dd3fb6c113
odilitime <janesmith@airmail.cc>
2022-04-26 02:38:51 +0000

    don't copy over any settings

--- common/modules/post/queuing/fe/modules/board_settings.php
+++ common/modules/post/queuing/fe/modules/board_settings.php
@@ -3,6 +3,8 @@
 $params = $getModule();
 
 // $io is fields
-$io['settings_queueing_mode'] = $shared['board_settings_fields']['queueing_mode'];
+//$io['settings_queueing_mode'] = $shared['board_settings_fields']['queueing_mode'];
+//$io['settings_queueing_mode_hs'] = $shared['board_settings_fields']['queueing_mode_hs'];
+//$io['settings_queueing_mode_link'] = $shared['board_settings_fields']['queueing_mode_link'];
 
 ?>
