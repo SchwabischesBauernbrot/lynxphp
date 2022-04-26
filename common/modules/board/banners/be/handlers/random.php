251b895837ab2d0ead84c4cf171cbbbc0a88ea01
odilitime <janesmith@airmail.cc>
2022-04-26 02:28:03 +0000

    overboard support

--- common/modules/board/banners/be/handlers/random.php
+++ common/modules/board/banners/be/handlers/random.php
@@ -1,15 +1,20 @@
 <?php
 $params = $get();
 
+$boardUri = getQueryField('boardUri');
 $boardData = boardMiddleware($request);
-if (!$boardData) {
+if (!$boardData && $boardUri !== 'overboard') {
   return; // middleware sends response
 }
 
 global $db, $models;
-$res = $db->find($models['board_banner'], array('criteria' => array(
-  array('board_id', '=', $boardData['boardid']),
-)));
+$crit = array();
+
+if ($boardUri !== 'overboard') {
+  $crit[] = array('board_id', '=', $boardData['boardid']);
+}
+
+$res = $db->find($models['board_banner'], array('criteria' => $crit));
 $banners = $db->toArray($res);
 if (!count($banners)) {
   return sendResponse(array());
