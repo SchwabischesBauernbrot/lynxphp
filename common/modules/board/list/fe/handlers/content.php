5a25d7a7b8f4053269451ea29be224177c0300f6
Odilitime <janesmith@airmail.cc>
2022-05-03 23:26:08 +0000

    pass params to BE

--- common/modules/board/list/fe/handlers/content.php
+++ common/modules/board/list/fe/handlers/content.php
@@ -2,11 +2,14 @@
 
 $params = $getHandler();
 
-$data = $pkg->useResource('list');
+$boardsOptions = getBoardsParams();
+//print_r($boardsOptions);
+
+$data = $pkg->useResource('list', $boardsOptions);
 
 // print_r($data);
 
-$res = getBoardsHandlerEngine($data);
+$res = getBoardsHandlerEngine($data, $boardsOptions);
 // print_r($res);
 
 wrapContent($res['content'], array('settings' => $res['settings']));
