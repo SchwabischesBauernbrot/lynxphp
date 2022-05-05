557860b81ec1fd3697843516676f790a93293ec2
odilitime <janesmith@airmail.cc>
2022-05-05 02:57:26 +0000

    getBoardsHandlerEngine => renderBoardsTemplate and new params

--- common/modules/board/list/fe/handlers/content.php
+++ common/modules/board/list/fe/handlers/content.php
@@ -8,8 +8,9 @@ $boardsOptions = getBoardsParams();
 $data = $pkg->useResource('list', $boardsOptions);
 
 // print_r($data);
+$templates = moduleLoadTemplates('board_listing', __DIR__);
 
-$res = getBoardsHandlerEngine($data, $boardsOptions);
+$res = renderBoardsTemplate($data, $templates, $boardsOptions);
 // print_r($res);
 
 wrapContent($res['content'], array('settings' => $res['settings']));
