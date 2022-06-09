11ce067f31bf06cff0150c52d0ba1f4a66537a0c
odilitime <janesmith@airmail.cc>
2022-06-09 01:22:31 +0000

    guard, note

--- common/modules/post/strings/fe/handlers/admin_strings.php
+++ common/modules/post/strings/fe/handlers/admin_strings.php
@@ -4,16 +4,20 @@ $params = $getHandler();
 
 $data = $pkg->useResource('string_list');
 
-$lookup = $shared['admin_fields']['action']['options'];
-
 $str = '';
-$str .= '<a href="admin/strings/add.html">Add strings</a><br>';
-$str .= '<table>';
-$str .= '<tr><th>string<th>action';
-foreach($data['strings'] as $s) {
-  $str .= '<tr><td>' . $s['string'] . '<td><nobr>' . $lookup[$s['action']] . '</nobr>' . "\n";
+
+if ($data) {
+  $lookup = $shared['admin_fields']['action']['options'];
+
+  $str .= '<a href="admin/strings/add.html">Add strings</a><br>';
+  // FIXME: add test post text function
+  $str .= '<table>';
+  $str .= '<tr><th>string<th>action';
+  foreach($data['strings'] as $s) {
+    $str .= '<tr><td>' . $s['string'] . '<td><nobr>' . $lookup[$s['action']] . '</nobr>' . "\n";
+  }
+  $str .= '</table>';
 }
-$str .= '</table>';
 
 wrapContent(renderAdminPortal() . $str);
 
