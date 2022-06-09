7c6e6d977a6757145dd5a9dc5f007db62380c9a8
odilitime <janesmith@airmail.cc>
2022-06-09 01:26:14 +0000

    votes, PIPELINE_BE_ADMIN_QUEUE_DATA

--- common/modules/post/queuing/be/handlers/queue_list.php
+++ common/modules/post/queuing/be/handlers/queue_list.php
@@ -1,21 +1,37 @@
 <?php
 $params = $get();
 
-global $db, $models;
-$res = $db->find($models['post_queue']);
+global $db, $models, $pipelines;
+$adjustedModel = $models['post_queue'];
+$adjustedModel['children'] = array(
+  array(
+    'type' => 'left',
+    'model' => $models['post_queue_vote'],
+    'pluck' => array('count(ALIAS.voteid) as votes'),
+    'on' => array(
+      // LEFT: not post_queueid but queueid
+      array('queueid', '=', $db->make_direct('post_queues.queueid'))
+    ),
+    'groupby' => array('post_queues.queueid'),
+  ),
+);
+$res = $db->find($adjustedModel);
 $qposts = $db->toArray($res);
 
 $post_files = array();
 // FIXME: simulate thumbnail sizes...
 
 foreach($qposts as $i => $qp) {
+  // row from qposts passes through
   $data = json_decode($qp['data'], true);
   $post = $data['post'];
   postDBtoAPI($post);
   $qposts[$i]['post'] = $post;
   $qposts[$i]['post']['created_at'] = $qp['created_at'];
   $qposts[$i]['post']['files'] = $post_files;
-
+  $io = $qposts[$i];
+  $pipelines[PIPELINE_BE_ADMIN_QUEUE_DATA]->execute($io);
+  $qposts[$i] = $io;
 }
 
 // would be nice to extract all the links from the posts
