905c3f43f99bd434ecf726b79426557760f07de5
odilitime <janesmith@airmail.cc>
2022-06-09 01:23:07 +0000

    PIPELINE_BE_ADMIN_QUEUE_DATA

--- common/modules/post/strings/be/data.php
+++ common/modules/post/strings/be/data.php
@@ -17,6 +17,10 @@ return array(
       // handle queuing process based on strings setting
       // FIXME: there's an ordering issue here...
       array('pipeline' => PIPELINE_NEWPOST_PROCESS, 'module' => 'newpost_process'),
+      // this optional pipeline, because it's a dependency
+      // it'll be included/defined
+      // (unless they disable the module...)
+      array('pipeline' => PIPELINE_BE_ADMIN_QUEUE_DATA, 'module' => 'admin_queue')
     ),
   ),
 );
