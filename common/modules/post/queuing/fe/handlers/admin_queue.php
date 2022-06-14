85b42dbe5b2280495790bae2e4e596963b96a1fc
odilitime <janesmith@airmail.cc>
2022-06-14 00:15:42 +0000

    post_queue_display refactor

--- common/modules/post/queuing/fe/handlers/admin_queue.php
+++ common/modules/post/queuing/fe/handlers/admin_queue.php
@@ -18,51 +18,9 @@ $str = '';
 $str .= 'posts: ' . count($data['queue_posts']) . "<br>\n";
 
 // FIXME: add a link to clear all with (updated) strings?
+$str .= '<a href="admin/queue/strings.html">delete all with refused strings</a>';
 
-global $pipelines;
-
-$io = array(
-  'addFields' => array()
-);
-$pipelines[PIPELINE_FE_ADMIN_QUEUE_ROW]->execute($io);
-$addFields = $io['addFields'];
-
-$str .= '<table width=100%>';
-// could but a check all here...
-$fields = array(
-  '', 'uri', 'id', 'thread', 'type', 'ip', 'post', 'votes',
-);
-$str .= '<tr><th><nobr>' . join('</nobr><th><nobr>', $fields) . '</nobr>';
-foreach($addFields as $f) {
-  $str .= '<th>' . $f['label'];
-}
-$str .= '<th>Actions';
-foreach($data['queue_posts'] as $s) {
-  $uri = $s['board_uri'];
-  $d = json_decode($s['data'], true);
-  $str .= '<tr>';
-  $str .= '<td><input type=checkbox name="list[]" value="' . $s['queueid'] . '">';
-  $str .= '<th><a href="/' . $uri . '" target=_blank>' . $uri;
-  $str .= '<td>' . $s['queueid'];
-  $str .= '<td>' . (!$s['thread_id'] ? 'new' : ('<a href="' . $uri . '/thread/' . $s['thread_id'] . '.html" target=_blank>' . $s['thread_id'] . '</a>'));
-  $str .= '<td>' . $s['type'];
-  $str .= '<td>' . $s['ip'] . '<td>' . renderPost($uri, $s['post']);
-  $str .= '<td>' . $s['votes'];
-  foreach($addFields as $f) {
-    $str .= '<td>' . $s[$f['field']];
-  }
-  // then actions?
-  $dataActions = array(
-    array('link' => 'admin/queue/' . $s['queueid'] . '/delete.html', 'label' => 'Delete'),
-  );
-  $htmlLinks = array();
-  foreach($dataActions as $a) {
-    $htmlLinks[] = action_getLinkHTML($a, array('where' => false));
-  }
-  $str .= '<td>' . join('<br>' > "\n", $htmlLinks);
-  //$str .= '<td><pre>' . print_r($s, 1) . "</pre>\n";
-}
-$str .= '</table>';
+$str .= post_queue_display($data['queue_posts']);
 
 $str .= "<br><hr><br>\n";
 
