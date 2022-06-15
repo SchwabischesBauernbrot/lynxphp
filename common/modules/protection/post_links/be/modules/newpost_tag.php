fc6efcdc44f9727a86deffd5d4deb640cf7376b6
odilitime <janesmith@airmail.cc>
2022-06-15 02:51:21 +0000

    refactor to common

--- common/modules/protection/post_links/be/modules/newpost_tag.php
+++ common/modules/protection/post_links/be/modules/newpost_tag.php
@@ -6,15 +6,7 @@ $params = $getModule();
 $msg = $io['p']['com'];
 //echo "looking at [$msg]<br>\n";
 
-$regex = "((https?|ftp)\:\/\/)?"; // SCHEME
-$regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
-$regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
-$regex .= "(\:[0-9]{2,5})?"; // Port
-$regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
-$regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
-$regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
-
-if (preg_match('/' . $regex . '/', $msg)) {
+if (post_links_has($msg)) {
   // add tag
   //echo "has link<br>\n";
   $io['tags']['has_links'] = true;
