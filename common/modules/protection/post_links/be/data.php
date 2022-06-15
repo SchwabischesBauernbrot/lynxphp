a8bcaea5d8958edcdbeaefdc31ab05eed3dd960a
odilitime <janesmith@airmail.cc>
2022-06-15 02:50:55 +0000

    PIPELINE_BE_ADMIN_QUEUE_DATA

--- common/modules/protection/post_links/be/data.php
+++ common/modules/protection/post_links/be/data.php
@@ -1,6 +1,8 @@
 <?php
 
-$bePkgs = array(
+// post_links/be
+
+return array(
   array(
     'models' => array(
       /*
@@ -17,9 +19,12 @@ $bePkgs = array(
       array('pipeline' => PIPELINE_POSTTAG_REGISTER, 'module' => 'register'),
       // determine if post needs a tag
       array('pipeline' => PIPELINE_NEWPOST_TAG, 'module' => 'newpost_tag'),
+      // this optional pipeline, because it's a dependency
+      // it'll be included/defined
+      // (unless they disable the module...)
+      array('pipeline' => PIPELINE_BE_ADMIN_QUEUE_DATA, 'module' => 'admin_queue')
     ),
   ),
 );
-return $bePkgs;
 
 ?>
