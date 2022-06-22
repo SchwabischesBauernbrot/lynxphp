be26e78289ca2c51bd8906720a20408124dd27c3
odilitime <janesmith@airmail.cc>
2022-06-22 04:34:44 +0000

    set a cookie if there isnt one

--- common/modules/post/reacts/be/handlers/list.php
+++ common/modules/post/reacts/be/handlers/list.php
@@ -10,6 +10,26 @@ if (!$boardData) {
 }
 */
 
+// ensure browse will have a session
+$setCookie = NULL;
+$userid = getUserID(); // are we logged in?
+$sesRow = ensureSession($userid); // sends a respsone on 500
+if (!$sesRow) {
+  return; // 500
+}
+// did we just make it?
+global $now;
+if (isset($sesRow['created']) && (int)$sesRow['created'] === (int)$now) {
+  // not going to have a username to send
+  $setCookie = array(
+    'name'  => 'session',
+    'value' => $sesRow['session'],
+    'ttl'   => $sesRow['expires'],
+  );
+}
+
+// FIXME: check board settings...
+
 global $db, $models, $tpp;
 
 $res = $db->find($models['board_react'], array('criteria' => array(
@@ -17,7 +37,11 @@ $res = $db->find($models['board_react'], array('criteria' => array(
 )));
 $reacts = $db->toArray($res);
 
+// FIXME: include board settings...
+
 //sendResponse($banners, 200, '', array('board' => $boardData));
-sendResponse2($reacts);
+sendResponse2($reacts, array(
+  'meta' => array('setCookie' => $setCookie,)
+));
 
 ?>
