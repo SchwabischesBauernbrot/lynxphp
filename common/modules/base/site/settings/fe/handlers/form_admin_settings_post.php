9961732880b56772ba6aaa8d458364cb24f778a1
odilitime <janesmith@airmail.cc>
2022-07-23 08:01:52 +0000

    update wrap options, handle checkmarks

--- common/modules/base/site/settings/fe/handlers/form_admin_settings_post.php
+++ common/modules/base/site/settings/fe/handlers/form_admin_settings_post.php
@@ -14,11 +14,20 @@ $pipelines[PIPELINE_ADMIN_SETTING_GENERAL]->execute($fields);
 
 // FIXME: get from formdata...
 $res = processFiles();
-wrapContent('Please wait...');
+$row = wrapContentData();
+wrapContentHeader($row);
+echo 'Please wait...';
 
 $files = array();
 if (!empty($res['handles']['logo'])) {
   $files = $res['handles']['logo'][0];
+}
+$fields = $common['fields']; // imported from fe/common.php
+foreach($fields as $field => $d) {
+  //if (substr($field, 0, 4) === 'show') {
+  if ($d['type'] === 'checkbox') { // smarter
+    $_POST[$field] = empty($_POST[$field]) ? false : true;
+  }
 }
 //echo '<pre>files: ', print_r($files, 1), "</pre>\n";
 
@@ -29,9 +38,9 @@ $res = $pkg->useResource('save_settings', array('logo' => json_encode($files)),
 if ($res['success']) {
   // maybe a js alert?
   echo "Success<br>\n";
-  redirectTo('/admin/settings.html');
+  redirectTo('/admin/settings.html', array('header' => false));
 } else {
-  wrapContent('Something went wrong...' . print_r($res, 1));
+  wrapContent('Something went wrong...' . print_r($res, 1), array('header' => false));
 }
 
 ?>
