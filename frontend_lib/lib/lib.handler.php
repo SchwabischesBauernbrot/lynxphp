f1845645ffb456d960b05ff251f87ffbe95d6609
odilitime <janesmith@airmail.cc>
2022-07-23 07:29:22 +0000

    mobile detection => wrapContentGetHeadHTML => footer tag, re-enable js/jschan/forms.js, number_format

--- frontend_lib/lib/lib.handler.php
+++ frontend_lib/lib/lib.handler.php
@@ -18,6 +18,35 @@ function redirectTo($url, $options = false) {
   */
 }
 
+//http://www.mlynn.org/2010/06/mobile-device-detection-and-redirection-with-php/ ?
+function checkmobile() {
+  if (isset($_SERVER['HTTP_X_WAP_PROFILE'])) return 1;
+  if (preg_match('/wap\.|\.wap/i',$_SERVER['HTTP_ACCEPT'])) return 1;
+  if (isset($_SERVER['HTTP_X_SKYFIRE_PHONE'])) return 1;
+  if (isset($_SERVER['HTTP_USER_AGENT'])) {
+    // Quick Array to kill out matches in the user agent
+    // that might cause false positives
+    $badmatches = array('OfficeLiveConnector',"MSIE\ 8\.0",'OptimizedIE8',"MSN\ Optimized",
+      "Creative\ AutoUpdate",'Swapper');
+    foreach($badmatches as $badstring) {
+      if(preg_match('/'.$badstring.'/i',$_SERVER['HTTP_USER_AGENT'])) return 0;
+    }
+    // Now we'll go for positive matches
+    $uamatches = array('midp','j2me','avantg','docomo','novarra','palmos','palmsource',
+      '240x320','opwv','chtml','pda',"windows\ ce","mmp\/",'blackberry','mib\/','symbian',
+      'wireless','nokia','hand','mobi','phone','cdm',"up\.b",'audio', "SIE\-", "SEC\-",
+      'samsung','HTC',"mot\-",'mitsu','sagem','sony','alcatel','lg','erics','vx','NEC',
+      'philips','mmm','xx','panasonic','sharp','wap','sch','rover','pocket','benq','java',
+      'pt','pg','vox','amoi','bird','compal','kg','voda','sany','kdd','dbt','sendo','sgh',
+      'gradi','jb',"\d\d\di",'moto','webos','kindle');
+    foreach($uamatches as $uastring) {
+      if(preg_match('/'.$uastring.'/i',$_SERVER['HTTP_USER_AGENT'])) return 1;
+    }
+  }
+  return 0;
+} // end func
+
+
 // or maybe don't have a static div...
 // use js to change it if X condition are met?
 
@@ -198,10 +227,17 @@ function wrapContentGetHeadHTML($row, $fullHead = false) {
 
   if ($fullHead) {
     $templates = loadTemplates('head');
+
+    $mobilecss = '';
+    if (checkmobile()) {
+      $mobilecss = '<link rel="stylesheet" href="css/mobile.css">';
+    }
+
     global $BASE_HREF;
     $tags = array(
       'backend_url' => BACKEND_PUBLIC_URL,
       'basehref' => $BASE_HREF,
+      'footer' => $mobilecss,
     );
     $head_html .= '<head>' . $term;
     $head_html .= replace_tags($templates['header'], $tags) . $term;
@@ -404,7 +440,7 @@ function wrapContentFooter($row) {
       'js/jschan/yous.js',
       // broken because it doesn't handle the output of new posts
       // queues, refused, success, error...
-      //'js/jschan/forms.js',
+      'js/jschan/forms.js',
       // upload item template
       'js/uploaditem.js',
       // lynxphp
@@ -501,7 +537,7 @@ function wrapContentFooter($row) {
   if (DEV_MODE) {
     global $now;
     $diff = (microtime(true) - $now) * 1000;
-    echo "took $diff ms<br>\n";
+    echo "took ", number_format($diff), " ms<br>\n";
     curl_log_report();
     curl_log_clear();
   }
