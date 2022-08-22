85c3b85cbda042c4c8a98e5f49efde8663350308
odilitime <janesmith@airmail.cc>
2022-08-22 07:20:23 +0000

    captcha_generateImage(), captcha_register(), captcha_generateCode()

--- common/modules/protection/captcha_lynxim/fe/common.php
+++ common/modules/protection/captcha_lynxim/fe/common.php
@@ -48,4 +48,67 @@ function distortImage($w, $h) {
   return $command . '\' ';
 }
 
+function captcha_generateCode() {
+  $text = '';
+  for($i = 0; $i < 6; $i++) {
+    $text .= chr(rand(0, 100) > 50 ? rand(97, 97 + 6) : rand(48, 57));
+  }
+  return $text;
+}
+
+function captcha_register($text) {
+  global $scratch, $now;
+  $captcha_id = md5(uniqid());
+  $captchas = $scratch->get('captchas');
+  if (!is_array($captchas)) $captchas = array();
+  $captchas[$captcha_id] = array(
+    // probably don't need to restore id, since we're keying by it
+    //'id' => $captcha_id,
+    'value' => $text,
+    'session' => isset($_COOKIE['session']) ? $_COOKIE['session'] : '',
+    'ip' => getip(),
+    // but needs to be validate for an entire page load...
+    // for no js...
+    // good for 24 hours => 5 minutes
+    'expires' => (int)($now) + (5 * 60),
+  );
+  $scratch->set('captchas', $captchas);
+  return array(
+    'id'      => $captcha_id,
+    'expires' => $captchas[$captcha_id]['expires'],
+  );
+}
+
+function captcha_generateImage($text) {
+  $level = 1;
+  $width = 286;
+  $height = 100;
+  $imageFont = 'AvantGarde-Book';  // convert -list font
+
+  $lineOffset = rand(-20, 20) / $level;
+  $command = '';
+  for($i = 0; $i < 5 * $level; $i++) {
+    $lineWidth = rand(10, 20) / $level;
+    if ($i) {
+      $command .= ' ';
+    }
+    $command .= 'rectangle 0,' . $lineOffset . ' ' . $width . ',';
+    $command .= $lineWidth + $lineOffset;
+    $lineOffset += rand(20, 30) / $level;
+    $lineOffset += $lineWidth;
+  }
+  //$command .= '\" -write mpr:mask +delete';
+
+
+  $cli = '/usr/bin/convert -size ' . $width . 'x' . $height . ' xc: -draw "' . $command . '" -write mpr:mask +delete';
+  $cli .= ' xc: -pointsize 70 -gravity center -font ' . $imageFont;
+  $cli .= ' -draw "text 0,0 \'' . $text . '\'" -write mpr:original +delete';
+  $cli .= ' mpr:original -negate -write mpr:negated +delete';
+  $cli .= ' mpr:negated mpr:original mpr:mask -composite';
+  $cli .= ' ' . distortImage($width, $height) . '-blur 0x1 jpg:-';
+
+  $data = `$cli`;
+  return $data;
+}
+
 ?>
