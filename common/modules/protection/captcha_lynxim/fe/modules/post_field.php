116f43a82ee5be0906ce3b708b235897c56c3886
odilitime <janesmith@airmail.cc>
2022-08-22 07:21:57 +0000

    only add the field if asked nicely

--- common/modules/protection/captcha_lynxim/fe/modules/post_field.php
+++ common/modules/protection/captcha_lynxim/fe/modules/post_field.php
@@ -2,35 +2,37 @@
 
 $params = $getModule();
 
-// io is formFields
+// io['formFields'] is formFields
+if (!empty($io['pipelineOptions']['showCAPTCHA'])) {
 
-// should this be enabled?
-global $board_settings;
-//echo "Settings [", gettype($board_settings), print_r($board_settings, 1),"]<br>\n";
-if ($board_settings === false) {
-  // get board settings...
-  $boardData = getBoard($io['boardUri']);
-  // don't commit with this
-  if (isset($boardData['settings'])) {
-    $board_settings = $boardData['settings'];
-  }
-}
-
-if (isset($board_settings['captcha_mode'])) {
-  $mode = $board_settings['captcha_mode']; // posts / thread / no
-  $enable = false;
-  if ($io['type'] === 'Thread') {
-    if ($mode !== 'no') {
-      $enable = true;
-    }
-  } else {
-    // Reply
-    if ($mode === 'posts') {
-      $enable = true;
+  // should this be enabled?
+  global $board_settings;
+  //echo "Settings [", gettype($board_settings), print_r($board_settings, 1),"]<br>\n";
+  if ($board_settings === false) {
+    // get board settings...
+    $boardData = getBoard($io['boardUri']);
+    // don't commit with this
+    if (isset($boardData['settings'])) {
+      $board_settings = $boardData['settings'];
     }
   }
-  if ($enable) {
-    $io['formfields']['captcha'] = array( 'type' => 'captcha', 'label' => 'Captcha');
+
+  if (isset($board_settings['captcha_mode'])) {
+    $mode = $board_settings['captcha_mode']; // posts / thread / no
+    $enable = false;
+    if ($io['type'] === 'Thread') {
+      if ($mode !== 'no') {
+        $enable = true;
+      }
+    } else {
+      // Reply
+      if ($mode === 'posts') {
+        $enable = true;
+      }
+    }
+    if ($enable) {
+      $io['formfields']['captcha'] = array( 'type' => 'captcha', 'label' => 'Captcha');
+    }
   }
 }
 
