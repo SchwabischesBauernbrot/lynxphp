be5f6043ed2a2da82548696b2632d586872ac811
odilitime <janesmith@airmail.cc>
2022-09-21 19:11:29 +0000

    move in boards.php code

--- common/modules/base/board/view/fe/handlers/view.php
+++ common/modules/base/board/view/fe/handlers/view.php
@@ -1,3 +1,11 @@
 <?php
 
+include '../frontend_lib/handlers/boards.php'; // request2QueryThread
+
+$boardUri = $request['params']['uri'];
+// transform req => q
+$q = request2QueryThread($request);
+//echo "<pre>", print_r($request['portals'], 1), "</pre>\n";
+getBoardThreadListing($q, $boardUri);
+
 ?>
