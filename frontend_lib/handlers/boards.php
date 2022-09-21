abfc066d4ac10319639e48998d1c9833e31e5d2a
odilitime <janesmith@airmail.cc>
2022-09-21 19:27:38 +0000

    move out getBoardThreadListing/getBoardThreadListingHandler/getBoardThreadListingRender, portal guard, files debug

--- frontend_lib/handlers/boards.php
+++ frontend_lib/handlers/boards.php
@@ -11,9 +11,13 @@ class query_thread {
 // could just be an array...
 */
 function request2QueryThread($request) {
-  return array(
-    'portals' => $request['portals'],
+  $obj = array(
+    'portals' => array(),
   );
+  if (!empty($request['portals'])) {
+    $obj['portals'] = $request['portals'];
+  }
+  return $obj;
 }
 // additional functions to automatically handle the header/footer of portals
 // probably hooked from the results
@@ -45,127 +49,6 @@ EOB;
   */
 }
 
-// refactored out so theme demo can use this
-function getBoardThreadListingRender($boardUri, $boardThreads, $pagenum, $wrapOptions = '') {
-  $pageData = $boardThreads['page1'];
-  $pages = $boardThreads['pageCount'];
-  $boardData = $boardThreads['board'];
-
-  $templates = loadTemplates('thread_listing');
-  //echo join(',', array_keys($templates));
-
-  // header is used
-  // see board_portal
-  $page_tmpl = $templates['loop0']; // not used
-  $boardnav_html = $templates['loop1']; // stomp (replacement is used in header)
-  $file_template = $templates['loop2']; // not used
-  $threadHdr_tmpl = $templates['loop3']; // used
-  $threadFtr_tmpl = $templates['loop4']; // used
-  $thread_tmpl = $templates['loop5']; // not used
-
-  extract(ensureOptions(array(
-    'noBoardHeaderTmpl' => false,
-  ), $wrapOptions));
-
-  //echo "test[", htmlspecialchars(print_r($templates, 1)),"]<br>\n";
-
-  // FIXME: register/push a portal with wrapContent
-  // so it can fast out efficiently
-  // also should wrapContent be split into header/footer for efficiency? yes
-  // and we need keying too, something like ESI
-
-  // need to set boardSettings here for DEMO
-  // but how do we normally get this? boardData['settings']
-  // getBoardPortal promotes it internally
-
-  $boardData['pageCount'] = $boardThreads['pageCount'];
-  $boardPortal = getBoardPortal($boardUri, $boardData, array(
-    'pagenum' => $pagenum, 'noBoardHeaderTmpl' => $noBoardHeaderTmpl));
-  $boardnav_html = '';
-
-  // used to look at text, so we can queue up another backend query if needed
-  // FIXME: check count of PIPELINE_POST_PREPROCESS
-  $nPosts = array();
-  foreach($pageData as $i => $thread) {
-    if (!isset($thread['posts'])) continue;
-    $posts = $thread['posts'];
-    foreach($posts as $j => $post) {
-      preprocessPost($pageData[$i]['posts'][$j]);
-      $nPosts[] = $post;
-    }
-  }
-  global $pipelines;
-  $data = array(
-    'posts' => $nPosts,
-    'boardThreads' => $boardThreads,
-    'pagenum' => $pagenum
-  );
-  $pipelines[PIPELINE_POST_POSTPREPROCESS]->execute($data);
-  unset($nPosts);
-
-  $threads_html = '';
-  foreach($pageData as $thread) {
-    //echo "<pre>", print_r($thread, 1), "</pre>\n";
-    //echo "[", $thread['posts'][0]['no'], "] replies[", $thread['thread_reply_count'], "]<br>\n";
-    if (!isset($thread['posts'])) continue;
-    $posts = $thread['posts'];
-    $threadId = $posts[0]['no'];
-    //echo "count[", count($posts), "]<br>\n";
-    $threads_html .= $threadHdr_tmpl;
-    // we only include 6...
-    //$cnt = count($posts);
-    foreach($posts as $i => $post) {
-      //if ($i === 0) $threads_html .= $threadHdr_tmpl;
-      $topReply = isset($posts[1]) ? $posts[1]['no'] : false;
-      $threads_html .= renderPost($boardUri, $post, array(
-        'checkable' => true, 'postCount' => empty($thread['thread_reply_count']) ? -1 : $thread['thread_reply_count'],
-        'topReply' => $topReply, 'where' => $boardUri . '/', 'boardSettings' => $boardData['settings'],
-      ));
-      //if ($i === count($posts) - 1) $threads_html .= $threadFtr_tmpl;
-    }
-    $expander_html = '';
-    /*
-    // rpp
-    if ($thread['thread_reply_count'] > 5) {
-      //$lastPost = $posts[count($posts) - 1];
-      $threadUrl = '/' . $boardUri . '/thread/' . $threadId . '.html';
-      if (isset($posts[1])) {
-        $secondPost = $posts[1];
-        $threadUrl .= '#' . $secondPost['no'];
-      }
-      $expander_html = '<a class="expand2Link" target="thread' . $threadId . 'View" href="'. $threadUrl . '" tabindex="0">Expand Thread</a>';
-    }
-    */
-    $threadFtr_tags = array(
-      'threadNum' => $threadId,
-      // iframe could use loading=lazy but FF doesn't yet support it
-      // consider it when it's added
-      'expander'  => $expander_html,
-    );
-    $threads_html .= replace_tags($threadFtr_tmpl, $threadFtr_tags);
-  }
-
-  $p = array(
-    'boardUri' => $boardUri,
-    'tags' => array(
-      // need this for form actions
-      'uri' => $boardUri,
-      // title, description?
-      //'title' => htmlspecialchars($boardData['title']),
-      //'description' => htmlspecialchars($boardData['description']),
-      'threads' => $threads_html,
-      'boardNav' => $boardnav_html,
-      'pagenum' => $pagenum,
-      // mixin
-      //'postform' => renderPostForm($boardUri, $boardUri . '/catalog'),
-      'postactions' => renderPostActions($boardUri),
-    ),
-  );
-  $pipelines[PIPELINE_BOARD_DETAILS_TMPL]->execute($p);
-  $tmpl = replace_tags($templates['header'], $p['tags']);
-  wrapContent($boardPortal['header'] . $tmpl . $boardPortal['footer'], $wrapOptions);
-}
-
 // /:uri
 function getBoardFileRedirect($request) {
   $boardUri = $request['params']['uri'];
@@ -178,6 +61,8 @@ function getBoardFileRedirect($request) {
   //echo "Would redirect to [$boardUri]\n";
 }
 
+// moved out to base/board/view
+/*
 function getBoardThreadListingHandler($request) {
   $boardUri = $request['params']['uri'];
   // transform req => q
@@ -192,6 +77,7 @@ function getBoardThreadListingPageHandler($request) {
   $q = request2QueryThread($request);
   getBoardThreadListing($q, $boardUri, $page);
 }
+*/
 
 function getBoardCatalogHandler($request) {
   $boardUri = $request['params']['uri'];
@@ -384,6 +270,10 @@ function makePostHandlerEngine($request) {
     'boardUri' => $boardUri,
     'result'   => $result,
     'json'     => $json,
+    // we could get the hashes from $res (handles.files[].hash)
+    // nah it's a refresh issue...
+    //'filesDebugs' => $res,
+    //'_FILES' => $_FILES,
   );
 }
 
@@ -488,26 +378,6 @@ function makePostHandlerJson($request) {
     $arr['POST'] = $_POST;
   }
   sendJson($arr, array('code' => $code));
-}
-
-// /:uri/
-function getBoardThreadListing($q, $boardUri, $pagenum = 1) {
-  //echo "pagenum[$pagenum]<br>\n";
-
-  // this does the backend call
-  // if we know what portal we're using
-  // well ofc we fucking do...
-  // but yea, we could pass an option to do something?
-  // pass a parameter to the backend that says baord portal
-  // and then things can hook on it
-  $boardThreads = backendGetBoardThreadListing($q, $boardUri, $pagenum);
-  if (!$boardThreads) {
-    wrapContent("There is a problem with the backend [$boardUri]");
-    return;
-  }
-  //echo "<pre>", print_r($boardThreads, 1), "</pre>\n";
-
-  getBoardThreadListingRender($boardUri, $boardThreads, $pagenum);
 }
 
 function renderBoardCatalog($boardUri) {
