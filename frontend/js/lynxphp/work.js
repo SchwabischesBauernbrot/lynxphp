b8df235e16b9d79ae222bc0f27862c89a316bac8
odilitime <janesmith@airmail.cc>
2022-09-21 18:59:59 +0000

    new files

---
+++ frontend/js/lynxphp/work.js
@@ -0,0 +1,11 @@
+
+function doWork() {
+  fetch('backend/opt/work').then(res => { statusCode = res.status; return res.text() } ).then(html => {
+    console.log('work', html)
+  })
+}
+
+doWork()
+
+// we need a proper backoff to deploy something like this..
+//setInterval(doWork, 60 * 1000)
