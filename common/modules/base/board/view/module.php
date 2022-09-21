0f12911e659762ed4b86e5caa37b091df85be51a
odilitime <janesmith@airmail.cc>
2022-09-21 19:13:28 +0000

    move opt/board_page

--- common/modules/base/board/view/module.php
+++ common/modules/base/board/view/module.php
@@ -3,17 +3,23 @@ return array(
   'name' => 'board_view',
   'version' => 1,
   'resources' => array(
-    /*
     array(
-      'name' => 'random',
+      'name' => 'board_page',
       'params' => array(
-        'endpoint' => 'lynx/randomBanner',
+        'endpoint' => 'opt/boards/:uri/:page',
         'unwrapData' => true,
-        'requires' => array('boardUri'),
-        'params' => 'querystring',
+        'sendSession' => true,
+        'requires' => array('uri', 'page'),
+        'params' => array(
+          'querystring' => 'portals',
+          'params' => array('uri', 'page'),
+        ),
+        'cacheSettings' => array(
+          'databaseTables' => array('user_sessions', 'board_{{uri}}_public_posts',
+            'board_{{uri}}_public_post_files', 'boards'),
+        ),
       ),
     ),
-    */
   ),
 );
 ?>
