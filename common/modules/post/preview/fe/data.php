b1e6e7f5e11684eee9ebf72e937817efeb0c2d60
odilitime <janesmith@airmail.cc>
2022-09-21 22:20:19 +0000

    clean up, suggest some better REST style urls

--- common/modules/post/preview/fe/data.php
+++ common/modules/post/preview/fe/data.php
@@ -3,15 +3,17 @@ return array(
   array(
     'handlers' => array(
       array(
-        'method'  => 'GET',
         'route'   => '/:board/preview/:id',
         'handler' => 'preview',
       ),
       /*
       array(
-        'method'  => 'GET',
-        'route'   => '/:uri/settings/banners',
-        'handler' => 'settings_list',
+        'route'   => '/:uri/threads/:threadId/posts/:postid/preview.html',
+        'handler' => 'preview',
+      ),
+      array(
+        'route'   => '/:uri/posts/:postid/preview.html',
+        'handler' => 'preview',
       ),
       */
     ),
@@ -29,21 +31,6 @@ return array(
         'pipeline' => 'PIPELINE_POST_LINKS',
         'module' => 'post_link',
       ),
-      /*
-      // format text
-      array(
-        'pipeline' => 'PIPELINE_POST_TEXT_FORMATTING',
-        'module' => 'format',
-      ),
-      array(
-        'pipeline' => 'PIPELINE_POST_PREPROCESS',
-        'module' => 'preformat',
-      ),
-      array(
-        'pipeline' => 'PIPELINE_POST_POSTPREPROCESS',
-        'module' => 'postpreformat',
-      ),
-      */
     ),
   ),
 );
