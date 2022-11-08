ee881e047024ab6d6e0c04d3c8b620e1b0d00815
odilitime <janesmith@airmail.cc>
2022-11-08 18:10:13 +0000

    new quote hash format handling

--- frontend/js/jschan/quote.js
+++ frontend/js/jschan/quote.js
@@ -1,122 +1,148 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  const postForm = document.querySelector('#postform');
-  //const newPostButton = document.querySelector('input[type="submit"]');
-  const newPostButton = document.querySelector('.openPostForm');
+  // DOM elements
+  // .post-quoters too
+  const postForm = document.querySelector('#postform')
+  //const newPostButton = document.querySelector('input[type="submit"]')
+  const newPostButton = document.querySelector('.openPostForm')
   //console.log('newPostButton', newPostButton)
 
   const openPostForm = (e) => {
     if (e) {
-      e.preventDefault();
+      e.preventDefault()
     }
-    //history.replaceState({}, '', '#postform');
+    // just remove the hash
+    // might also be #XX_postform
+    //history.replaceState({}, '', '#postform')
+    //location.hash = '' // just makes it #
+    history.replaceState({}, '', location.pathname) // remove all hash
+
+    // what about multiple post forms
+    // I think we just want the top floating one
+
     // disabled on locked/closed threads
     if (postForm) {
-      postForm.style.display = 'flex';
+      // show popup
+      postForm.style.display = 'flex'
     }
     if (newPostButton) {
-      newPostButton.style.visibility = 'hidden';
+      // this hides the in-page link
+      newPostButton.style.visibility = 'hidden'
     }
     if (postForm) {
-      postForm.dispatchEvent(new Event('opened'));
+      postForm.dispatchEvent(new Event('opened'))
     }
-  };
-
+  }
 
   const closePostForm = (e) => {
-    e.preventDefault();
-    history.replaceState({}, '', location.pathname);
+    e.preventDefault()
+    history.replaceState({}, '', location.pathname)
     if (postForm) {
-      postForm.style.display = 'none';
+      // hide popup
+      postForm.style.display = 'none'
     }
     if (newPostButton) {
-      newPostButton.style.visibility = 'visible';
+      // add back the in-page link to form
+      newPostButton.style.visibility = 'visible'
     }
-  };
+  }
 
   // if we have a postform
   if (postForm) {
     if (newPostButton) {
-      newPostButton.addEventListener('click', openPostForm, false);
+      newPostButton.addEventListener('click', openPostForm, false)
     }
-    const closeButton = postForm ? postForm.querySelector('.close') : null;
-    closeButton.addEventListener('click', closePostForm, false);
+    const closeButton = postForm ? postForm.querySelector('.close') : null
+    closeButton.addEventListener('click', closePostForm, false)
   }
 
-  //const messageBox = document.getElementById('message');
-  const messageBox = document.querySelector('#postform textarea[name="message"]');
+  //const messageBox = document.getElementById('message')
+  const messageBox = document.querySelector('#postform textarea[name="message"]')
 
   const addToMessageBox = (str) => {
-    const index = messageBox.selectionStart;
-    messageBox.value = `${messageBox.value.substr(0,index)}${str}${messageBox.value.substr(index)}`;
-    messageBox.setSelectionRange(index+str.length, index+str.length); //this scroll anyway, no need to set scrolltop
+    const index = messageBox.selectionStart
+    messageBox.value = `${messageBox.value.substr(0,index)}${str}${messageBox.value.substr(index)}`
+    messageBox.setSelectionRange(index+str.length, index+str.length) //this scroll anyway, no need to set scrolltop
   }
 
   const addQuote = function(number) {
-    openPostForm();
-    let quoteText = `>>${number}\n`;
-    let selection;
+    openPostForm()
+    let quoteText = `>>${number}\n`
+    let selection
     if (window.getSelection) {
-      selection = window.getSelection().toString();
+      selection = window.getSelection().toString()
     } else if (document.getSelection) {
-      selection = document.getSelection().toString();
+      selection = document.getSelection().toString()
     } else if (document.selection) {
-      selection = document.selection.createRange().text;
+      selection = document.selection.createRange().text
     }
     if (selection && selection.length > 0) {
       const quotedSelection = selection.split(/\r?\n/) //split by lines
         .map(line => line.trim().length > 0 ? `>${line}` : line) //make non empty lines greentext
-        .join('\n'); //join it back together and newline
-      quoteText += `${quotedSelection}\n`;
+        .join('\n') //join it back together and newline
+      quoteText += `${quotedSelection}\n`
     }
-    addToMessageBox(quoteText);
-    messageBox.focus();
-    messageBox.dispatchEvent(new Event('input')); // for counter.js
+    addToMessageBox(quoteText)
+    messageBox.focus()
+    messageBox.dispatchEvent(new Event('input')) // for counter.js
   }
 
   const quote = function(e) {
-    const quoteNum = this.textContent.trim();
+    const quoteNum = this.textContent.trim()
+    console.debug('quote.js - quote', quoteNum, 'isThread', isThread, 'ctrl', e.ctrlKey)
     if (isThread && !e.ctrlKey) {
-      addQuote(quoteNum);
+      addQuote(quoteNum)
     } else {
-      setLocalStorage('clickedQuote', quoteNum);
+      setLocalStorage('clickedQuote', quoteNum)
     }
-  };
+  }
 
   //on loading page open with js method if user has scripts
+  //console.log('quote.js - location.hash', location.hash)
+  if (location.hash.match(/_postform/)) {
+    // has to be before openPostForm
+    var replyTo = location.hash.replace(/^#/, '').replace('_postform', '')
+    openPostForm() // strips location.hash
+    //console.log('reply link clicked', replyTo)
+    if (replyTo) {
+      addQuote(replyTo)
+    }
+  } else // handle old style
   if (location.hash === '#postform') {
-    openPostForm();
+    openPostForm()
   }
+  console.debug('quote.js - isThread', isThread)
   if (isThread) {
     //add quote to postform if link clicked with quote
-    const quoteNum = localStorage.getItem('clickedQuote');
+    const quoteNum = localStorage.getItem('clickedQuote')
+    console.debug('quote.js - clickedQuote process', quoteNum)
     if (quoteNum != null) {
-      addQuote(quoteNum);
+      addQuote(quoteNum)
       //scroll to the post you quoted
-      const quotingPost = document.getElementById(quoteNum);
+      const quotingPost = document.getElementById(quoteNum)
       if (quotingPost) {
-        quotingPost.scrollIntoView();
+        quotingPost.scrollIntoView()
       }
     }
-    localStorage.removeItem('clickedQuote');
+    localStorage.removeItem('clickedQuote')
   }
 
   const addQuoteListeners = (l) => {
     for (let i = 0; i < l.length; i++) {
-      l[i].addEventListener('click', quote, false);
+      l[i].addEventListener('click', quote, false)
     }
-  };
+  }
 
-  const links = document.getElementsByClassName('post-quoters');
-  addQuoteListeners(links);
+  const links = document.getElementsByClassName('post-quoters')
+  addQuoteListeners(links)
 
   window.addEventListener('addPost', function(e) {
     if (e.detail.hover) {
-      return; //dont need to handle hovered posts for this
+      return //dont need to handle hovered posts for this
     }
-    const post = e.detail.post;
-    const newlinks = post.getElementsByClassName('post-quoters');
-    addQuoteListeners(newlinks);
-  });
+    const post = e.detail.post
+    const newlinks = post.getElementsByClassName('post-quoters')
+    addQuoteListeners(newlinks)
+  })
 
-});
+})
