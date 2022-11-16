d431ec4df30bd628cb0a3ce8d1e74a0cea8c000d
odilitime <janesmith@airmail.cc>
2022-11-16 17:58:56 +0000

    use getCompiledSettings to drive navItems => navItems2

--- frontend_lib/handlers/mixins/admin_portal.php
+++ frontend_lib/handlers/mixins/admin_portal.php
@@ -2,17 +2,41 @@
 
 // FIXME: split into header/footer functions
 function renderAdminPortal() {
+  $navItems = array(
+    'Settings' => 'admin/settings.html',
+    'FE Routes' => 'admin/fe_routes.php',
+    'BE Routes' => 'admin/be_routes.php',
+    'Modules' => 'admin/modules.php',
+    // should be iframe'd or more integrated...
+    'System' => 'admin/install.php',
+  );
+
+  $adminSettings = getCompiledSettings('admin');
+  $settingSubItems = array(
+    //array('label' => 'site', 'destinations' => 'admin/settings/site.html')
+  );
+  foreach($adminSettings as $nav => $ni) {
+    $settingSubItems[] = array(
+      'label' => $nav,
+      'destinations' => 'admin/settings/' . $nav . '.html',
+    );
+  }
+
+  $navItems2 = array(
+    array('label' => 'Settings', 'subItems' => $settingSubItems),
+    array('label' => 'FE Routes', 'destinations' => 'admin/fe_routes.php'),
+    array('label' => 'BE Routes', 'destinations' => 'admin/be_routes.php'),
+    array('label' => 'Modules', 'destinations' => 'admin/modules.php'),
+    // should be iframe'd or more integrated...
+    array('label' => 'System', 'destinations' => 'admin/install.php'),
+  );
+  $adminSettings = getCompiledSettings('admin');
+
   return renderPortalHeader('admin', array(
     'headerPipeline' => PIPELINE_ADMIN_HEADER_TMPL,
     'navPipeline'    => PIPELINE_ADMIN_NAV,
-    'navItems' => array(
-      'Settings' => 'admin/settings.html',
-      'FE Routes' => 'admin/fe_routes.php',
-      'BE Routes' => 'admin/be_routes.php',
-      'Modules' => 'admin/modules.php',
-      // should be iframe'd or more integrated...
-      'System' => 'admin/install.php',
-    ),
+    //'navItems'       => $navItems,
+    'navItems2'       => $navItems2,
   ));
 }
 
