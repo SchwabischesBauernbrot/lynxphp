44edb48840dba6e69a3ef5696fc76747c0bdb533
odilitime <janesmith@airmail.cc>
2022-11-16 18:08:43 +0000

    move in homepage settings

--- common/modules/site/homepage/module.php
+++ common/modules/site/homepage/module.php
@@ -2,6 +2,55 @@
 return array(
   'name' => 'site_homepage',
   'version' => 1,
+  'settings' => array(
+    array(
+      'level' => 'admin', // constant?
+      'location' => 'homepage', // /tab/group
+      'addFields' => array(
+        'showSiteName' => array(
+          'label' => 'Show Site Name',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'showWelcome' => array(
+          'label' => 'Show Welcome to SITE NAME',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'slogan' => array(
+          'label' => 'Site Slogan',
+          'type'  => 'text',
+        ),
+        'showSlogan' => array(
+          'label' => 'Show Slogan',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'showLogo' => array(
+          'label' => 'Show Logo',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'showShortlist' => array(
+          'label' => 'Show Board Short List',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        // SFW/NSFW?
+        'showRecentImages' => array(
+          'label' => 'Show Recent Images (All)',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        // threads/replies/all?
+        'showRecentPosts' => array(
+          'label' => 'Show Recent Posts',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+      )
+    ),
+  ),
   'resources' => array(
     array(
       'name' => 'homepage',
