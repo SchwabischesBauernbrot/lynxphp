4709dcf9aaf1e057d1614f72b83af25042d5b91b
odilitime <janesmith@airmail.cc>
2022-11-16 18:13:53 +0000

    captialize Banners

--- common/modules/board/banners/fe/modules/nav_settings.php
+++ common/modules/board/banners/fe/modules/nav_settings.php
@@ -1,9 +1,8 @@
 <?php
 
-// FIXME: we need access to package
 $params = $getModule();
 
 // io is navItems
-$io['navItems']['banners'] = '{{uri}}/settings/banners';
+$io['navItems']['Banners'] = '{{uri}}/settings/banners';
 
 ?>
