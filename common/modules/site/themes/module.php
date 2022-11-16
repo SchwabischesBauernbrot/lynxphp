708d3d3914c5a8f9315c68a7a4b2cc2923a86528
odilitime <janesmith@airmail.cc>
2022-11-16 18:08:13 +0000

    move in theme settings

--- common/modules/site/themes/module.php
+++ common/modules/site/themes/module.php
@@ -3,6 +3,20 @@
 return array(
   'name'    => 'site_themes',
   'version' => 1,
+  'settings' => array(
+    array(
+      'level' => 'admin', // constant?
+      'location' => 'site', // /tab/group
+      'addFields' => array(
+        'defaultTheme' => array(
+          'label' => 'Default Theme',
+          'type'  => 'select',
+          'options' => $shared['themes'],
+          //'optionsExec' => 'getThemes',
+        ),
+      )
+    ),
+  ),
 );
 
 ?>
