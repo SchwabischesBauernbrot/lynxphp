f14037fe02006edd16aaae5ab87c0bcf348327f3
odilitime <janesmith@airmail.cc>
2022-11-16 18:13:00 +0000

    use getCompiledSettings to drive sections

--- common/modules/base/site/settings/fe/handlers/form_admin_settings_get.php
+++ common/modules/base/site/settings/fe/handlers/form_admin_settings_get.php
@@ -7,14 +7,23 @@ $values = $data['site'];
 //echo "<pre>[", print_r($values, 1), "]</pre>\n";
 
 global $pipelines;
-$fields = $common['fields']; // imported from fe/common.php
+//echo "<pre>[", print_r($params, 1), "]</pre>\n";
+$section = $params['request']['params']['section'];
+
+$adminSettings = getCompiledSettings('admin');
+if (isset($adminSettings[$section])) {
+  $fields = $adminSettings[$section];
+} else {
+  $fields = $common['fields']; // imported from fe/common.php
+}
+
 // handle hooks for additionl settings
-$pipelines[PIPELINE_ADMIN_SETTING_GENERAL]->execute($fields);
+//$pipelines[PIPELINE_ADMIN_SETTING_GENERAL]->execute($fields);
 
 //echo "<pre>", print_r($fields, 1), "</pre>\n";
 
 $html = generateForm($params['action'], $fields, $values);
 
-wrapContent(renderAdminPortal() . 'Settings'. $html);
+wrapContent(renderAdminPortal() . ucfirst($section) . ' Settings'. $html);
 
 ?>
