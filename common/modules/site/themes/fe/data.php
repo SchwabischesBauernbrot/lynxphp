cee304509b9005c406a549fe134e37061ab33fe6
odilitime <janesmith@airmail.cc>
2022-12-13 17:03:13 +0000

    allow all css theme files

--- common/modules/site/themes/fe/data.php
+++ common/modules/site/themes/fe/data.php
@@ -5,6 +5,7 @@ $fePkgs = array(
     // what was this for?
     //'dependencies' => array('user/mgmt'),
     'handlers' => array(
+      // css passthru
       array(
         'method'  => 'GET',
         'route'   => '/user/settings/theme.php',
@@ -68,6 +69,40 @@ $fePkgs = array(
         'pipeline' => 'PIPELINE_ADMIN_SETTING_GENERAL',
         'module' => 'admin_settings',
       ),
+    ),
+    'css' => array(
+      // we just need to add them there to allow them all
+      // which theme matters on user setting
+      //array('file' => 'board_banner.css')
+      array('file' => 'amoled.css'),
+      array('file' => 'army-green.css'),
+      array('file' => 'cancer.css'),
+      array('file' => 'chaos.css'),
+      array('file' => 'choc.css'),
+      array('file' => 'clear.css'),
+      array('file' => 'darkblue.css'),
+      array('file' => 'gurochan.css'),
+      array('file' => 'kc.css'),
+      array('file' => 'lain.css'),
+      array('file' => 'miku.css'),
+      array('file' => 'mushroom.css'),
+      array('file' => 'navy.css'),
+      array('file' => 'pink.css'),
+      array('file' => 'rei-zero.css'),
+      array('file' => 'robot.css'),
+      array('file' => 'seafoam-dark.css'),
+      array('file' => 'seafoam-light.css'),
+      array('file' => 'snerx.css'),
+      array('file' => 'solarized-dark.css'),
+      array('file' => 'solarized-light.css'),
+      array('file' => 'sovl.css'),
+      array('file' => 'tempus-cozette.css'),
+      array('file' => 'tomorrow.css'),
+      array('file' => 'tomorrow2.css'),
+      array('file' => 'vapor.css'),
+      array('file' => 'win95.css'),
+      array('file' => 'yotsuba-b.css'),
+      array('file' => 'yotsuba.css'),
     ),
   ),
 );
