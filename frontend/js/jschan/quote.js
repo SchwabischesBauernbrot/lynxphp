5ccb4d4713f30a4ac40c112133e9079e0100c39f
Odilitime <janesmith@airmail.cc>
2023-01-02 19:04:45 +0000

    detect when hash changes after page load

--- frontend/js/jschan/quote.js
+++ frontend/js/jschan/quote.js
@@ -1,51 +1,110 @@
+let messageBox
+let postForm
+let newPostButton
+
+const openPostForm = (e) => {
+  if (e) {
+    e.preventDefault()
+  }
+  // just remove the hash
+  // might also be #XX_postform
+  //history.replaceState({}, '', '#postform')
+  //location.hash = '' // just makes it #
+  history.replaceState({}, '', location.pathname) // remove all hash
+
+  // what about multiple post forms
+  // I think we just want the top floating one
+
+  // disabled on locked/closed threads
+  if (postForm) {
+    // show popup
+    postForm.style.display = 'flex'
+  }
+  if (newPostButton) {
+    // this hides the in-page link
+    newPostButton.style.visibility = 'hidden'
+  }
+  if (postForm) {
+    postForm.dispatchEvent(new Event('opened'))
+  }
+}
+
+const closePostForm = (e) => {
+  e.preventDefault()
+  history.replaceState({}, '', location.pathname)
+  if (postForm) {
+    // hide popup
+    postForm.style.display = 'none'
+  }
+  if (newPostButton) {
+    // add back the in-page link to form
+    newPostButton.style.visibility = 'visible'
+  }
+}
+
+const addToMessageBox = (str) => {
+  // there won't be one, if a closed thread
+  if (messageBox) {
+    const index = messageBox.selectionStart
+    messageBox.value = `${messageBox.value.substr(0,index)}${str}${messageBox.value.substr(index)}`
+    messageBox.setSelectionRange(index+str.length, index+str.length) //this scroll anyway, no need to set scrolltop
+    messageBox.dispatchEvent(new Event('input')) // for counter.js
+  }
+}
+
+const addQuote = function(number) {
+  openPostForm()
+  let quoteText = `>>${number}\n`
+  let selection
+  if (window.getSelection) {
+    selection = window.getSelection().toString()
+  } else if (document.getSelection) {
+    selection = document.getSelection().toString()
+  } else if (document.selection) {
+    selection = document.selection.createRange().text
+  }
+  if (selection && selection.length > 0) {
+    const quotedSelection = selection.split(/\r?\n/) //split by lines
+      .map(line => line.trim().length > 0 ? `>${line}` : line) //make non empty lines greentext
+      .join('\n') //join it back together and newline
+    quoteText += `${quotedSelection}\n`
+  }
+  addToMessageBox(quoteText)
+  if (messageBox) {
+    messageBox.focus()
+  }
+}
+
+
+function quote_onHashChange() {
+  console.log('quote.js - location.hash', location.hash)
+  if (location.hash.match(/_postform/)) {
+    // has to be before openPostForm
+    var replyTo = location.hash.replace(/^#/, '').replace('_postform', '')
+    openPostForm() // strips location.hash
+    console.log('reply link clicked', replyTo)
+    if (replyTo) {
+      addQuote(replyTo)
+    }
+  } else // handle old style
+  if (location.hash === '#postform') {
+    openPostForm()
+  }
+}
+
+window.addEventListener('hashchange', function() {
+  console.log('new hash', window.location.hash)
+  quote_onHashChange()
+})
+
 window.addEventListener('DOMContentLoaded', (event) => {
 
   // DOM elements
   // .post-quoters too
-  const postForm = document.querySelector('#postform')
+  postForm = document.querySelector('#postform')
   //const newPostButton = document.querySelector('input[type="submit"]')
-  const newPostButton = document.querySelector('.openPostForm')
+  newPostButton = document.querySelector('.openPostForm')
   //console.log('newPostButton', newPostButton)
-
-  const openPostForm = (e) => {
-    if (e) {
-      e.preventDefault()
-    }
-    // just remove the hash
-    // might also be #XX_postform
-    //history.replaceState({}, '', '#postform')
-    //location.hash = '' // just makes it #
-    history.replaceState({}, '', location.pathname) // remove all hash
-
-    // what about multiple post forms
-    // I think we just want the top floating one
-
-    // disabled on locked/closed threads
-    if (postForm) {
-      // show popup
-      postForm.style.display = 'flex'
-    }
-    if (newPostButton) {
-      // this hides the in-page link
-      newPostButton.style.visibility = 'hidden'
-    }
-    if (postForm) {
-      postForm.dispatchEvent(new Event('opened'))
-    }
-  }
-
-  const closePostForm = (e) => {
-    e.preventDefault()
-    history.replaceState({}, '', location.pathname)
-    if (postForm) {
-      // hide popup
-      postForm.style.display = 'none'
-    }
-    if (newPostButton) {
-      // add back the in-page link to form
-      newPostButton.style.visibility = 'visible'
-    }
-  }
 
   // if we have a postform
   if (postForm) {
@@ -57,41 +116,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 
   //const messageBox = document.getElementById('message')
-  const messageBox = document.querySelector('#postform textarea[name="message"]')
+  messageBox = document.querySelector('#postform textarea[name="message"]')
+  quote_onHashChange()
 
-  const addToMessageBox = (str) => {
-    // there won't be one, if a closed thread
-    if (messageBox) {
-      const index = messageBox.selectionStart
-      messageBox.value = `${messageBox.value.substr(0,index)}${str}${messageBox.value.substr(index)}`
-      messageBox.setSelectionRange(index+str.length, index+str.length) //this scroll anyway, no need to set scrolltop
-      messageBox.dispatchEvent(new Event('input')) // for counter.js
-    }
-  }
-
-  const addQuote = function(number) {
-    openPostForm()
-    let quoteText = `>>${number}\n`
-    let selection
-    if (window.getSelection) {
-      selection = window.getSelection().toString()
-    } else if (document.getSelection) {
-      selection = document.getSelection().toString()
-    } else if (document.selection) {
-      selection = document.selection.createRange().text
-    }
-    if (selection && selection.length > 0) {
-      const quotedSelection = selection.split(/\r?\n/) //split by lines
-        .map(line => line.trim().length > 0 ? `>${line}` : line) //make non empty lines greentext
-        .join('\n') //join it back together and newline
-      quoteText += `${quotedSelection}\n`
-    }
-    addToMessageBox(quoteText)
-    if (messageBox) {
-      messageBox.focus()
-    }
-  }
-
+  // what calls this, addQuoteListeners below
   const quote = function(e) {
     const quoteNum = this.textContent.trim()
     console.debug('quote.js - quote', quoteNum, 'isThread', isThread, 'ctrl', e.ctrlKey)
@@ -103,20 +131,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
 
   //on loading page open with js method if user has scripts
-  //console.log('quote.js - location.hash', location.hash)
-  if (location.hash.match(/_postform/)) {
-    // has to be before openPostForm
-    var replyTo = location.hash.replace(/^#/, '').replace('_postform', '')
-    openPostForm() // strips location.hash
-    //console.log('reply link clicked', replyTo)
-    if (replyTo) {
-      addQuote(replyTo)
-    }
-  } else // handle old style
-  if (location.hash === '#postform') {
-    openPostForm()
-  }
-  console.debug('quote.js - isThread', isThread)
+
+  //console.debug('quote.js - isThread', isThread)
   if (isThread) {
     //add quote to postform if link clicked with quote
     const quoteNum = localStorage.getItem('clickedQuote')
