cab503af18a250967e268c96ed6c6b4ee8308ba4
odilitime <janesmith@airmail.cc>
2023-01-02 21:21:07 +0000

    add guard for 404

--- common/modules/base/thread/view/be/handlers/refresh.php
+++ common/modules/base/thread/view/be/handlers/refresh.php
@@ -27,16 +27,18 @@ $replies = getThread($boardUri, $threadNum, array(
 ));
 
 $maxtime = 0;
-foreach($replies as $i => $r) {
-  $maxtime = max($maxtime, $r['updated_at']);
-  // save some bandwidth
-  $replies[$i]['exposedFields'] = array_filter($r['exposedFields'], function($value) { return !is_null($value) && $value !== ''; });
-  $r['exposedFields'] = $replies[$i]['exposedFields'];
-  $replies[$i] = array_filter($r, function($value) {
-    $isNotEmpty = (!is_null($value) && $value !== '');
-    $isEmptyArray = is_array($value) && count($value) === 0;
-    return $isNotEmpty && !$isEmptyArray;
-  });
+if (is_array($replies) && count($replies)) {
+  foreach($replies as $i => $r) {
+    $maxtime = max($maxtime, $r['updated_at']);
+    // save some bandwidth
+    $replies[$i]['exposedFields'] = array_filter($r['exposedFields'], function($value) { return !is_null($value) && $value !== ''; });
+    $r['exposedFields'] = $replies[$i]['exposedFields'];
+    $replies[$i] = array_filter($r, function($value) {
+      $isNotEmpty = (!is_null($value) && $value !== '');
+      $isEmptyArray = is_array($value) && count($value) === 0;
+      return $isNotEmpty && !$isEmptyArray;
+    });
+  }
 }
 if (!$maxtime) {
   global $now;
