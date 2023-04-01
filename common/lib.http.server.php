0f1dbd5ddab0e1a1fd1870f7770ed0a71145ef4a
odilitime <janesmith@airmail.cc>
2023-04-01 22:56:07 +0000

    require units in a way it wont be loaded twice, fileParseGlobal()/processFilesVar()

--- common/lib.http.server.php
+++ common/lib.http.server.php
@@ -1,8 +1,8 @@
 <?php
+// for getting data from apache/nginx
+ldr_require('../common/lib.units.php');
 
-ldr_require('lib.units.php');
-
-// apache/nginx hack
+// apache polyfill for nginx
 if (!function_exists('getallheaders')) {
   function getallheaders() {
     $headers = array();
@@ -110,6 +110,84 @@ function hasGetVars($fields) {
     }
   }
   return true;
+}
+
+$phpFileUploadErrors = array(
+    0 => 'There is no error, the file uploaded with success',
+    1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+    2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+    3 => 'The uploaded file was only partially uploaded',
+    4 => 'No file was uploaded',
+    6 => 'Missing a temporary folder',
+    7 => 'Failed to write file to disk.',
+    8 => 'A PHP extension stopped the file upload.',
+);
+
+// handle non-4 errors & reporting, otherwise collect data/normalize
+function fileParseGlobal($f, $errorCode, $row) {
+  global $phpFileUploadErrors;
+  // handle error
+  if (!$f['tmp_name']) {
+    if (!empty($errorCode)) {
+      // decode error
+      return array(
+        'error' => $phpFileUploadErrors[$errorCode],
+        'debug' => $row,
+      );
+    } else {
+      // we don't have a path (and we should?)
+      return array(
+        'error' => 'empty file',
+        'debug' => $row,
+      );
+    }
+  }
+  // warnings?
+  return array(
+    'tmp_name' => $f['tmp_name'],
+    'type' => $f['type'],
+    'name' => $f['name'],
+  );
+}
+
+function processFilesVar($filter_fields = false) {
+  $fields = $filter_fields;
+  if ($fields === false) {
+    // just auto-detect them
+    $fields = array_keys($_FILES);
+  }
+  // normalized fields as an array
+  if (!is_array($fields)) $fields = array($filter_fields);
+
+  $files = array();
+  if (isset($_FILES)) {
+    // this could be a less crazy data structure...
+    //print_r($_FILES);
+
+    // for every field we're interested in
+    foreach($fields as $field) {
+      $files[$field] = array();
+      // each field could have multiple file support...
+      if (is_array($_FILES[$field]['tmp_name'])) {
+        foreach($_FILES[$field]['tmp_name'] as $i=>$path) {
+          $ec = $_FILES[$field]['error'][$i];
+          if ($ec !== 4) {
+            $files[$field][] = fileParseGlobal(array(
+              'tmp_name' => $path,
+              'type' => $_FILES[$field]['type'][$i],
+              'name' => $_FILES[$field]['name'][$i],
+            ), $ec, $_FILES[$field]);
+          }
+        }
+      } else {
+        $ec = $_FILES[$field]['error'];
+        if ($ec !== 4) {
+          $files[$field][] = fileParseGlobal($_FILES[$field], $ec, $_FILES[$field]);
+        }
+      }
+    }
+  }
+  return $files;
 }
 
 ?>
