c045a931028067c5edba47a1b20fda59a47015a0
odilitime <janesmith@airmail.cc>
2023-05-04 17:39:36 +0000

    getAdminFields() refactor

--- common/modules/base/site/settings/fe/common.php
+++ common/modules/base/site/settings/fe/common.php
@@ -4,46 +4,62 @@
 
 // function are automatically exported
 
+function getAdminFields($section) {
+  $fields = false;
+  $adminSettings = getCompiledSettings('admin');
+  //print_r($adminSettings);
+
+  if (isset($adminSettings[$section])) {
+    if (is_array($adminSettings[$section])) {
+      $fields = $adminSettings[$section];
+    } else {
+    }
+  } else {
+    $fields = array(
+      'fields' => array(
+        'siteName' => array(
+          'label' => 'Site Name',
+          'type'  => 'text',
+        ),
+        'logo' => array(
+          'label' => 'Site Logo',
+          'type'  => 'image',
+        ),
+        /*
+        'showSiteName' => array(
+          'label' => 'Show Site Name',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'showWelcome' => array(
+          'label' => 'Show Welcome to SITE NAME',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'slogan' => array(
+          'label' => 'Site Slogan',
+          'type'  => 'text',
+        ),
+        'showSlogan' => array(
+          'label' => 'Show Slogan',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        'showLogo' => array(
+          'label' => 'Show Logo',
+          'type'  => 'checkbox',
+          'default' => true,
+        ),
+        */
+      ),
+    );
+  }
+  return $fields;
+}
+
 // allow export of data as $common in your handlers and modules
 // now set in module.php s
 
-return array(
-  'fields' => array(
-    'siteName' => array(
-      'label' => 'Site Name',
-      'type'  => 'text',
-    ),
-    'logo' => array(
-      'label' => 'Site Logo',
-      'type'  => 'image',
-    ),
-    /*
-    'showSiteName' => array(
-      'label' => 'Show Site Name',
-      'type'  => 'checkbox',
-      'default' => true,
-    ),
-    'showWelcome' => array(
-      'label' => 'Show Welcome to SITE NAME',
-      'type'  => 'checkbox',
-      'default' => true,
-    ),
-    'slogan' => array(
-      'label' => 'Site Slogan',
-      'type'  => 'text',
-    ),
-    'showSlogan' => array(
-      'label' => 'Show Slogan',
-      'type'  => 'checkbox',
-      'default' => true,
-    ),
-    'showLogo' => array(
-      'label' => 'Show Logo',
-      'type'  => 'checkbox',
-      'default' => true,
-    ),
-    */
-  ),
-);
+return;
 
 ?>
