5e9d8e55d894d8e7081889084fa4c4ef4825b22d
odilitime <janesmith@airmail.cc>
2023-06-03 19:37:43 +0000

    style, note

--- backend/handlers/lynx/files.php
+++ backend/handlers/lynx/files.php
@@ -9,8 +9,9 @@ if (!isset($_FILES['files'])) {
   return sendResponse(array(), 400, 'no file upload set in files field');
 }
 $hash = hash_file('sha256', $_FILES['files']['tmp_name']);
+// would be nice if this wasn't in the webrooot
 move_uploaded_file($_FILES['files']['tmp_name'], 'storage/tmp/'.$hash);
-$data=array(
+$data = array(
   'type' => $_FILES['files']['type'],
   'name' => $_FILES['files']['name'],
   'size' => $_FILES['files']['size'],
