ba55dc9f30c2858def91f4b46665c2361e9dec21
odilitime <janesmith@airmail.cc>
2023-06-05 16:53:13 +0000

    define board and posts portals

--- common/modules/base/board/view/module.php
+++ common/modules/base/board/view/module.php
@@ -2,6 +2,17 @@
 return array(
   'name' => 'base_board_view',
   'version' => 1,
+  'portals' => array(
+    'board' => array(
+      'fePipelines' => array('PIPELINE_BOARD_HEADER_TMPL', 'PIPELINE_BOARD_FOOTER_TMPL'),
+      'requires' => array('boardUri'),
+      // cacheSettings for backend module
+    ),
+    'posts' => array(
+      'requires' => array('boardUri'),
+      // cacheSettings for backend module
+    ),
+  ),
   'resources' => array(
     array(
       'name' => 'board_page',
