0d2798a9ebc626ddd4d3380af75349676f001b0f
odilitime <janesmith@airmail.cc>
2023-06-05 16:56:10 +0000

    portal stubs, change template format, use getters, notes

--- common/modules/base/board/settings/fe/portals/board_settings.php
+++ common/modules/base/board/settings/fe/portals/board_settings.php
@@ -1,5 +1,51 @@
 <?php
 
+// code and data for portal
+
+// should the metadata be separate from the code
+// so we dont need the code if we want to peak at the metadata
+// well in the current system, there is no need for peaking
+// we either use a portal or don't
+// and no harm in iterating over all the portals and loading them
+
+// get data for this
+// a function call is a bit safer than returning from the include
+// should this be a global, so it's not a call but a global
+/*
+function getPortalBoardSettingsConfig() {
+  // could put params code here
+  // do we need SID?
+  return
+}
+*/
+
+global $portalsConfig;
+$portalsConfig['boardSettings'] = array(
+  // wouldn't this depend on the route itself... *sigh*
+  'params' => array('uri' => array('type' => 'params', 'name' => 'uri'))
+);
+
+// a handler(router) adapter
+function getPortalBoardSettings($opts, $request) {
+  //echo "getPortalBoardSettings<br>\n";
+  //echo "<pre>getPortalBoardSettings row[", print_r($row, 1), "]</pre>\n";
+  //echo "<pre>getPortalBoardSettings request[", print_r($request, 1), "]</pre>\n";
+
+  //$config = getPortalBoardSettingsConfig();
+  global $portalsConfig;
+  // meta coding
+  $params = portal_getParamsFromContext($portalsConfig['boardSettings']['params'], $request);
+  //echo "<pre>getPortalBoardSettings params[", print_r($params, 1), "]</pre>\n";
+  $boardSettings = getter_getBoardSettings($params['uri']);
+  $row = renderBoardSettingsPortalData($params['uri'], array(
+    'boardSettings' => $boardSettings,
+  ));
+  return array(
+    'uri' => $params['uri'],
+    'portalSettings' => $row,
+  );
+}
+
 // refactored out so we can share data between header/footer
 // without having to recalculate it
 function renderBoardSettingsPortalData($boardUri, $options = false) {
@@ -19,7 +65,6 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
 
   // needs to be moved to mixins now
   $templates = loadTemplates('board_settings');
-  $tmpl = $templates['header'];
 
   //$page_wrapper_tmpl = $templates['loop0'];
   //$pageLink_tmpl     = $templates['loop1'];
@@ -28,16 +73,15 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
   // would be nice to have the board settings by here
   // so we can pass it in to control/hint the nav
   // we need boardSettings for pipelines (mainly nav)
+  //echo "boardSettings[$boardSettings]<br>\n";
   if ($boardSettings === false) {
     if (DEV_MODE) {
       echo "No boardSettings passed to renderBoardSettingsPortalData<Br>\n";
     }
-    $boardData = getBoard($boardUri);
-    if (isset($boardData['settings'])) {
-      $boardSettings = $boardData['settings'];
-    }
+    $boardSettings = getter_getBoardSettings($boardUri);
     //print_r($boardSettings);
   }
+  // print_r($boardSettings);
   $nav_io = array(
     'boardUri' => $boardUri,
     // would be help to know what settings are used
@@ -46,10 +90,25 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
     'navItems' => array(
     ),
   );
+
+  $blocks = getCompiledSettings('bo');
+  //echo "<pre>", print_r($blocks, 1), "</pre>\n";
+  foreach($blocks as $section => $fs) {
+    if ($section === 'board') {
+      // special case for now
+      $io['navItems']['Board settings'] = $boardUri . '/settings/board.html';
+    } else {
+      $lbl = getCompiledSettingsSectionLabel('bo', $section);
+      // if (!$lbl) $lbl=$section
+      $nav_io['navItems'][$lbl] = $boardUri . '/settings/' . $section;
+    }
+  }
+
+  // FIXME: convert to getNav2
   $pipelines[PIPELINE_BOARD_SETTING_NAV]->execute($nav_io);
 
   $nav_html = getNav($nav_io['navItems'], array(
-    'list' => false,
+    'list' => true,
     'replaces' => array('uri' => $boardUri),
     // handle no pages...
     //'selected' => $pageCount ? $selected : NULL,
@@ -96,7 +155,7 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
   //echo "noBoardHeaderTmpl[$noBoardHeaderTmpl]<Br>\n";
   if (!$noBoardHeaderTmpl) {
     // banner is injected here:
-    $pipelines[PIPELINE_BOARD_HEADER_TMPL]->execute($p);
+    $pipelines[PIPELINE_BOARD_SETTING_HEADER_TMPL]->execute($p);
   }
 
   // if threadNum, is it locked?
@@ -105,8 +164,13 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
     'reply' => $threadNum, 'maxMessageLength' => $maxMessageLength,
   ));
 
+  //echo "returning<br>\n";
+
   return array(
-    'tmpl' => $tmpl,
+    'tmpl' => array(
+      'header' => $templates['header'],
+      'footer' => $templates['loop0'],
+    ),
     'tags' => $p['tags'],
     'isCatalog' => $isCatalog,
     'threadNum' => $threadNum,
@@ -118,6 +182,13 @@ function renderBoardSettingsPortalData($boardUri, $options = false) {
   );
 }
 
+function getPortalBoardSettingsHeader($data) {
+  global $boardData; // better than nothing
+  //echo "getPortalBoardSettingsHeader<br>\n";
+  echo renderBoardSettingsPortalHeaderEngine($data['portalSettings'], $data['uri'], $boardData);
+}
+
+// why is the engine separate?
 function renderBoardSettingsPortalHeaderEngine($row, $boardUri, $boardData) {
   global $pipelines;
   $isCatalog = $row['isCatalog'];
@@ -148,7 +219,7 @@ function renderBoardSettingsPortalHeaderEngine($row, $boardUri, $boardData) {
   if (!isset($boardData['title'])) $boardData['title'] = 'Communication problem';
   if (!isset($boardData['description'])) $boardData['description'] = 'try again in a bit';
 
-  return replace_tags($row['tmpl'], array_merge($row['tags'], array(
+  return replace_tags($row['tmpl']['header'], array_merge($row['tags'], array(
     'uri' => $boardUri,
     'url' => $_SERVER['REQUEST_URI'],
     'title' => $isCatalog ? '' : ' - ' . htmlspecialchars($boardData['title']),
@@ -163,18 +234,22 @@ function renderBoardSettingsPortalHeaderEngine($row, $boardUri, $boardData) {
   )));
 }
 
+function getPortalBoardSettingsFooter($data) {
+  global $boardData; // better than nothing
+  echo renderBoardSettingsPortalFooterEngine($data['portalSettings'], $data['uri'], $boardData);
+}
+
 function renderBoardSettingsPortalFooterEngine($row, $boardUri, $boardData) {
   global $pipelines;
-  return '';
+  $tmpl = $row['tmpl']['footer'];
+  return $tmpl;
 
   //$templates = loadTemplates('board_settings');
-  //$tmpl = $templates['header'];
   $tmpl = $row['tmpl'];
   $threadstats_tmpl = $templates['loop0'];
   $postForm_tmpl = $templates['loop1'];
 
   $threadstats_html = '';
-
   if (isset($boardData['posts']) && is_array($boardData['posts'])) {
     $files = 0;
     //echo "[", print_r($boardData['posts'], 1), "]<br>\n";
@@ -230,6 +305,7 @@ function getBoardSettingsPortal($boardUri, $boardData = false, $options = false)
 
 // portals? header/footer split?
 // is this function responsible for boardData? can't be if we're to be efficient
+// why is the engine separate?
 function renderBoardSettingsPortalHeader($boardUri, $boardData = false, $options = false) {
   if ($boardData === false) {
     // FIXME: look up board data on-demand
@@ -243,7 +319,7 @@ function renderBoardSettingsPortalFooter($boardUri, $boardData = false, $options
     // FIXME: look up board data on-demand
   }
   $row = renderBoardSettingsPortalData($boardUri, $options);
-  echo renderBoardSettingsPortalFooterEngine($row, $boardUri, $boardData);
+  return renderBoardSettingsPortalFooterEngine($row, $boardUri, $boardData);
 }
 
 ?>
