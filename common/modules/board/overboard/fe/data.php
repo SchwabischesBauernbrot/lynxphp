d14507d246e57e72b523a882420b5c71bee5aa73
odilitime <janesmith@airmail.cc>
2023-06-05 18:13:41 +0000

    use board portal

--- common/modules/board/overboard/fe/data.php
+++ common/modules/board/overboard/fe/data.php
@@ -1,4 +1,21 @@
 <?php
+// we need to be able to pass options
+// probably separate the layout options from the backend otpions
+$obPortals = array('board' => array(
+  // special overboard boardData
+  'pageCount' => 1,
+  'uri' => 'overboard',
+  'title' => 'All Boards',
+  'description' => 'posts across the site',
+  'settings' => array(),
+  'noBackendData' => true,
+  //'pagenum' => $pagenum,
+  'noBoardHeaderTmpl' => true, // controls banner
+  'isThread' => true, // turn off paging
+  //'isCatalog' => true, // prefix title
+  'threadClosed' => true, // turn off post form
+  'paramsCode' => array('page' => array('type' => 'params', 'name' => 'page'))
+));
 
 $fePkgs = array(
   array(
@@ -6,6 +23,7 @@ $fePkgs = array(
       array(
         'route'   => '/overboard/page/:page.html',
         'handler' => 'overboard',
+        'portals' => $obPortals,
         /*
         'cacheSettings' => array(
           'files' => array(
@@ -19,6 +37,7 @@ $fePkgs = array(
       array(
         'route'   => '/overboard.html',
         'handler' => 'overboard',
+        'portals' => $obPortals,
         /*
         'cacheSettings' => array(
           'files' => array(
