9b9afa82460c197e8ac9e2b83c0076b67534e77d
odilitime <janesmith@airmail.cc>
2023-06-05 18:18:58 +0000

    get count from portal data

--- common/modules/post/actions/fe/modules/nav_settings.php
+++ common/modules/post/actions/fe/modules/nav_settings.php
@@ -6,12 +6,20 @@ $params = $getModule();
 // io is navItems
 
 //print_r($params);
+global $portalData;
+//echo "<pre>", print_r($portalData, 1), "</pre>\n";
+$openReportCount = false;
+if (isset($portalData['boardSettings']['openReportCount'])) {
+  $openReportCount = $portalData['boardSettings']['openReportCount'];
+}
 
 // costly but polish
-$result = $pkg->useResource('open_reports', array('boardUri' => $io['boardUri']));
+if ($openReportCount === false) {
+  $result = $pkg->useResource('open_reports', array('boardUri' => $io['boardUri']));
+  if (!isset($result['reports']) || !is_array($result['reports'])) $result['reports'] = array();
+  $openReportCount = count($result['reports']);
+}
 
-if (!isset($result['reports']) || !is_array($result['reports'])) $result['reports'] = array();
-
-$io['navItems']['reports ('.count($result['reports']).')'] = '{{uri}}/settings/reports';
+$io['navItems']['reports (' . $openReportCount . ')'] = '{{uri}}/settings/reports';
 
 ?>
