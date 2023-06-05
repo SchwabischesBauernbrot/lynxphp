1243f7231bc6e33a1469abf828c212c2e068190e
odilitime <janesmith@airmail.cc>
2023-06-05 16:46:55 +0000

    add boardSettings portal

--- common/modules/base/board/settings/module.php
+++ common/modules/base/board/settings/module.php
@@ -2,6 +2,27 @@
 return array(
   'name' => 'base_board_settings',
   'version' => 1,
+  'portals' => array(
+    'boardSettings' => array(
+      // parameters?
+      //'be_pipelines' => array('PIPELINE_BE_BOARD_SETTING_PORTAL'),
+      // or we could put them in fe/data.php
+      // but the functions that execute this pipeline is here...
+      'fePipelines' => array('PIPELINE_BOARD_SETTING_HEADER_TMPL'),
+      'requires' => array('boardUri'),
+      // options for the future
+      //'resources' => array(
+        // we only need one, w/e it is
+        // don't need to pollute the route space
+        // endpoints
+        // we could have a generic one
+        // module?
+        // what structure data do we need?
+        // a guess a true works for now
+        // but we don't have a need for a false...
+      //),
+    ),
+  ),
   'resources' => array(
     array(
       'name' => 'list',
