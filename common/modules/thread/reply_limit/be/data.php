23eaa0f67d6741fc1ffd7823e8d66cc46b0e7d07
Odilitime <janesmith@airmail.cc>
2023-06-14 08:11:45 +0000

    reference correct file

--- common/modules/thread/reply_limit/be/data.php
+++ common/modules/thread/reply_limit/be/data.php
@@ -19,7 +19,7 @@ return array(
     'modules' => array(
       // handle incoming posts (though we only need to be on replies)
       // why not just reject the reply on PIPELINE_REPLY_ALLOWED
-      array('pipeline' => PIPELINE_REPLY_ALLOWED, 'module' => 'reply_allowed'),
+      array('pipeline' => PIPELINE_REPLY_ALLOWED, 'module' => 'allow_reply'),
       //array('pipeline' => PIPELINE_NEWPOST_PROCESS, 'module' => 'newpost_process'),
     ),
   ),
