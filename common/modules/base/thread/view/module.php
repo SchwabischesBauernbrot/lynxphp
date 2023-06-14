de3288312158ba135fbe4e4bf3103c7f86604880
odilitime <janesmith@airmail.cc>
2023-06-14 07:14:22 +0000

    move board_thread from opt in

--- common/modules/base/thread/view/module.php
+++ common/modules/base/thread/view/module.php
@@ -12,6 +12,27 @@ return array(
         'params' => 'querystring',
       ),
     ),
+    array(
+      'name' => 'board_thread',
+      'params' => array(
+        'endpoint' => 'opt/:uri/thread/:num',
+        'unwrapData' => true,
+        //'sendSession' => true,
+        'requires' => array('uri', 'num'),
+        'params' => array(
+          'params' => array('uri', 'num'),
+        ),
+        'cacheSettings' => array(
+          // PIPELINE_BOARD_QUERY_MODEL could modify boards
+          // posts/files
+          // well boards will get bumped when there's a bump...
+          // should be good enough for now
+          'databaseTables' => array('user_sessions', 'board_{{uri}}_public_posts',
+            'board_{{uri}}_public_post_files', 'boards'),
+        ),
+      ),
+    ),
+
   ),
 );
 ?>
