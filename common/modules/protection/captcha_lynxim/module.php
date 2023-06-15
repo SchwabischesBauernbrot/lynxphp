af01ba625c7e53561f9db25844f32a0a0bff54a1
odilitime <janesmith@airmail.cc>
2023-06-15 18:41:27 +0000

    move in setting

--- common/modules/protection/captcha_lynxim/module.php
+++ common/modules/protection/captcha_lynxim/module.php
@@ -2,6 +2,23 @@
 return array(
   'name' => 'protection_captcha_lynxim',
   'version' => 1,
+  'settings' => array(
+    array(
+      'level' => 'bo', // constant?
+      'location' => 'board', // /tab/group
+      'addFields' => array(
+        'captcha_mode' => array(
+          'label' => 'CAPTCHA mode',
+          'type'  => 'select',
+          'options' => array(
+            'no' => 'No captcha',
+            'threads' => 'Only for threads',
+            'posts' => 'For all posts',
+          ),
+        ),
+      )
+    ),
+  ),
   'resources' => array(
   ),
 );
