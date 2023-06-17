75cbaa64a436f04f7afc0f0302044f890b1bd89b
odilitime <janesmith@airmail.cc>
2023-06-17 04:56:34 +0000

    add admin portal

--- common/modules/base/site/settings/fe/data.php
+++ common/modules/base/site/settings/fe/data.php
@@ -8,14 +8,23 @@ return array(
       array(
         'route'   => '/admin/settings/:section',
         'handler' => 'admin_settings',
+        'portals' => array(
+          'admin' => array()
+        ),
       ),
       array(
         'route'   => '/admin/settings/:section/add',
         'handler' => 'admin_settings_add',
+        'portals' => array(
+          'admin' => array()
+        ),
       ),
       array(
         'route'   => '/admin/settings/:section/remove',
         'handler' => 'admin_settings_remove',
+        'portals' => array(
+          'admin' => array()
+        ),
       ),
     ),
     'modules' => array(
