b1319bb27d6c6a6def7eb699c69c0edff16f8dbe
odilitime <janesmith@airmail.cc>
2023-06-17 04:52:30 +0000

    remove renderGlobalPortal()

--- frontend_lib/handlers/global.php
+++ frontend_lib/handlers/global.php
@@ -1,32 +1,8 @@
 <?php
-
 function getGlobalPage() {
 
-  $portal = array(
-    'header'=>array(
-      'file' => '',
-      // tag => code/constant
-      'replaces' => array(),
-      'nav' => array(
-        'items' => array(
-        ),
-        'replaces' => array(),
-        'selected' => '',
-        'displayOpts' => array(
-          'list' => true
-        )
-      )
-    ),
-    'footer'=>array(
-      'file' => '',
-      'replaces' => array(),
-    ),
-  );
-
-  $boardnav_html = renderGlobalPortal();
-
-  $content = $boardnav_html;
-  $content .= <<< EOB
+  // portal header was put here
+  $content = <<< EOB
 You're an hotpocket, you do it for FREE!
 EOB;
   wrapContent($content);
