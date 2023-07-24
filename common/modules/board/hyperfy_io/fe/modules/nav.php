3d3dc3f786ccbca82c8b8fd2899d1b5fefed5adc
Odilitime <janesmith@airmail.cc>
2023-07-24 22:38:23 +0000

    fix hyperfy uri

--- common/modules/board/hyperfy_io/fe/modules/nav.php
+++ common/modules/board/hyperfy_io/fe/modules/nav.php
@@ -4,7 +4,7 @@ $params = $getModule();
 
 //echo "<pre>", print_r($io, 1), "</pre>\n";
 
-if (!empty($io['boardSettings']['hyperfy'])) {
+if (!empty($io['boardSettings']['hyperfy_uri'])) {
   //$hfuri = $io['boardSettings']['hyperfy_uri'];
   $label = empty($io['boardSettings']['hyperfy_label']) ? 'Multiplayer' : $io['boardSettings']['hyperfy_label'];
   $io['navItems'][] = array('label' => $label, 'destinations' => $io['boardUri'] . '/hyperfy.html');
