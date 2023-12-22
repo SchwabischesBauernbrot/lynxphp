dc9474cec3c08a1e5b528c9a2052c2cffaae539f
odilitime <janesmith@airmail.cc>
2023-12-22 08:06:16 +0000

    getUserSettings builds lib.perms account cache

--- frontend_lib/lib/lib.fe.settings.php
+++ frontend_lib/lib/lib.fe.settings.php
@@ -13,17 +13,49 @@ function settingsGetSections() {
 function getUserSettings() {
   global $userSettings;
   if ($userSettings) return $userSettings;
+  // make sure we have a cookie
+  // can't return false because that's like userSettings not returning any data and it will retry
+  if (!isLoggedIn()) return '';
 
   global $packages;
   if (!empty($packages['user_setting'])) {
+    // so we only need to talk to the backend if we have a session
+    // $user_id = loggedIn();
+
     // cacheable?
     $data = $packages['user_setting']->useResource('settings');
     //echo "<pre>", print_r($data, 1), "</pre>\n";
     // loggedIn (1/0)
+    if (isset($data['loggedIn'])) {
+      // communicate to lib.perms to reduce backend calls
+      global $loggedIn;
+      //echo "loggedin set";
+      $loggedIn = $data['loggedIn'] ? 'true' : 'false';
+    }
     // session ID string
     // settings: current_theme, name, postpass, volume, sitecustomcss, nsfw, hover, time
     if (isset($data['settings'])) {
+      // this should be the login is valid?
+      // /opt/session
       $userSettings = $data['settings'];
+    }
+    if (isset($data['ownedBoards']) || isset($data['groups'])) {
+      global $persist_scratch, $now;
+      $key = 'user_session' . $_COOKIE['session'];
+      $user = $persist_scratch->get($key);
+      // emulate full request
+      $user['account_ts'] = $now;
+      if (!isset($user['account']['meta'])) {
+        $user['account']['meta']['code'] = 200;
+      }
+      // update the important data
+      if (isset($data['ownedBoards'])) {
+        $user['account']['ownedBoards'] = $data['ownedBoards'];
+      }
+      if (isset($data['groups'])) {
+        $user['account']['groups'] = $data['groups'];
+      }
+      $persist_scratch->set($key, $user);
     }
   }
   return $userSettings;
