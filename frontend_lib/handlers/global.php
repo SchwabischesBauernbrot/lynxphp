4881154c4429366d439962659a3088fc42a89153
odilitime <janesmith@airmail.cc>
2023-12-22 05:50:49 +0000

    global_portal

--- frontend_lib/handlers/global.php
+++ frontend_lib/handlers/global.php
@@ -1,11 +1,14 @@
 <?php
+
+include "../frontend_lib/handlers/mixins/global_portal.php";
+
 function getGlobalPage() {
 
   // portal header was put here
   $content = <<< EOB
 You're an hotpocket, you do it for FREE!
 EOB;
-  wrapContent($content);
+  wrapContent(renderGlobalPortal() . $content);
 }
 
 ?>
