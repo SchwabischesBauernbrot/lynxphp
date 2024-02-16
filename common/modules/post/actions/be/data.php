dc4136927b54abed1752023de095bee8d51689e9
odilitime <janesmith@airmail.cc>
2024-02-16 09:48:38 +0000

    PIPELINE_BE_CONTENTACTIONS_DELETE

--- common/modules/post/actions/be/data.php
+++ common/modules/post/actions/be/data.php
@@ -7,6 +7,9 @@ $bePkgs = array(
     'modules' => array(
       array('pipeline' => PIPELINE_BE_BOARD_SETTINGS_PORTAL, 'module' => 'board_settings_portal_data'),
     ),
+    'pipelines' => array(
+      array('name' => 'PIPELINE_BE_CONTENTACTIONS_DELETE'),
+    ),
   ),
 );
 return $bePkgs;
