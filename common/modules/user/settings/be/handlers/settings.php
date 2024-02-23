96db519027870ca437d6f9c16c5d0067cc7ba7c7
odilitime <janesmith@airmail.cc>
2024-02-23 17:54:32 +0000

    refactor, only include account if logged in

--- common/modules/user/settings/be/handlers/settings.php
+++ common/modules/user/settings/be/handlers/settings.php
@@ -1,61 +1,62 @@
 <?php
 $params = $get();
 
-$userid = getUserID();
-$userRes = getAccount($userid);
-if (!$userRes) {
-  return sendJson(array('err' => 'user_id has been deleted'), array('code' => 400));
-}
-
-$settings = getUserSettings($userid);
-
-$ownedBoards = userBoards($user_id);
-$groups = getUserGroups($user_id);
-$isAdmin  = userInGroup($user_id, 'admin');
-$isGlobal = userInGroup($user_id, 'global');
-
-/*
-$account = array(
-  'noCaptchaBan' => false,
-  'login' => empty($userRes['username']) ? $userRes['publickey'] : $userRes['username'],
-  'email' => $userRes['email'],
-  'globalRole' => $isAdmin ? 1 : ($isGlobal ? 2 : 99),
-  //'disabledLatestPostings'
-  //'volunteeredBoards'
-  'boardCreationAllowed' => true,
-  'ownedBoards' => $ownedBoards,
-  'groups' => $groups,
-  //'settings'
-  'reportFilter' => array(), // category filters for e-mail notifications
-  // outside spec
-  'username' => $userRes['username'],
-  'publickey' => $userRes['publickey'],
-);
-*/
-
-// seems to be mainly or lib.fe.settings.php
-// maybe it was more stripped down
-// only needed account.publickey, ownedBoards, and groups
-sendResponse(array(
+// could be keyed by our user record in settings
+$settings = getUserSettings();
+// scope it down
+$resp = array(
   'settings' => $settings['settings'],
-  'loggedIn' => $userid ? true : false,
+  'loggedIn' => $settings['loggedin'],
   'session'  => $settings['session'],
+);
+
+if ($settings['loggedin']) {
   // username / pubkey
-  'account' => array(
-    'login' => empty($userRes['username']) ? $userRes['publickey'] : $userRes['username'],
-    'email' => $userRes['email'],
-    'globalRole' => $isAdmin ? 1 : ($isGlobal ? 2 : 99),  
-    'publickey' => $userRes['publickey'],
-    'username' => $userRes['username'],
-    'ownedBoards' => $ownedBoards,
-    'groups' => $groups,
-  ),
   // why are these down here?
   // lynx/account (also has settings)
   //'globalRole' => $isAdmin ? 1 : ($isGlobal ? 2 : 99),
   //'ownedBoards' => $ownedBoards,
   //'groups' => $groups,
   // login / email
-));
+  $userid = $settings['userid'];
+  $userRes = getAccount($userid);
+  $ownedBoards = userBoards($user_id);
+  $groups = getUserGroups($user_id);
+  $isAdmin  = userInGroup($user_id, 'admin');
+  $isGlobal = userInGroup($user_id, 'global');
+
+  // seems to be mainly or lib.fe.settings.php
+  // maybe it was more stripped down
+  // only needed account.publickey, ownedBoards, and groups
+  /*
+  $account = array(
+    'noCaptchaBan' => false,
+    'login' => empty($userRes['username']) ? $userRes['publickey'] : $userRes['username'],
+    'email' => $userRes['email'],
+    'globalRole' => $isAdmin ? 1 : ($isGlobal ? 2 : 99),
+    //'disabledLatestPostings'
+    //'volunteeredBoards'
+    'boardCreationAllowed' => true,
+    'ownedBoards' => $ownedBoards,
+    'groups' => $groups,
+    //'settings'
+    'reportFilter' => array(), // category filters for e-mail notifications
+    // outside spec
+    'username' => $userRes['username'],
+    'publickey' => $userRes['publickey'],
+  );
+  */
+  $resp['account'] = array(
+    'login' => empty($userRes['username']) ? $userRes['publickey'] : $userRes['username'],
+    'email' => $userRes['email'],
+    'globalRole' => $isAdmin ? 1 : ($isGlobal ? 2 : 99),
+    'publickey' => $userRes['publickey'],
+    'username' => $userRes['username'],
+    'ownedBoards' => $ownedBoards,
+    'groups' => $groups,
+  );
+}
+
+sendResponse($resp);
 
 ?>
