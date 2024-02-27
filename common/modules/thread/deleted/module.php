4cf820642a5ecf0bafb7424f2ca4c9b6afdc7f42
odilitime <janesmith@airmail.cc>
2024-02-27 21:24:43 +0000

    add view handler

--- common/modules/thread/deleted/module.php
+++ common/modules/thread/deleted/module.php
@@ -26,6 +26,26 @@ return array(
         //'params' => 'querystring',
       ),
     ),
+    array(
+      'name' => 'view',
+      'params' => array(
+        'endpoint' => 'doubleplus/:uri/threads/deleted/:num',
+        'unwrapData' => true,
+        //'sendSession' => true,
+        'requires' => array('uri', 'num'),
+        'params' => array(
+          'params' => array('uri', 'num'),
+        ),
+        'cacheSettings' => array(
+          // PIPELINE_BOARD_QUERY_MODEL could modify boards
+          // posts/files
+          // well boards will get bumped when there's a bump...
+          // should be good enough for now
+          'databaseTables' => array('user_sessions', 'board_{{uri}}_public_posts',
+            'board_{{uri}}_public_post_files', 'boards'),
+        ),
+      ),
+    ),
   ),
 );
 ?>
