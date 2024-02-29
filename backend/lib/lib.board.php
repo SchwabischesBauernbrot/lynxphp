0766800d443a07d7078b0aa369a0756ceaaaeadf
odilitime <janesmith@airmail.cc>
2024-02-29 06:18:09 +0000

    precreatePost(), priv: add email/password, post: add email/remove password

--- backend/lib/lib.board.php
+++ backend/lib/lib.board.php
@@ -159,13 +159,16 @@ function getPostsModel($boardUri, $options = false) {
       // 'now' => array('type'=>'integer'),
       //'time' => array('type'=>'integer'),
       'name' => array('type'=>'str', 'length'=>128),
+      //'email' => array('type'=>'str', 'length'=>255),
       'trip' => array('type'=>'str', 'length'=>128),
       'capcode' => array('type'=>'str', 'length'=>32),
       'country' => array('type'=>'str', 'length'=>2),
       //'country_name' => array('type'=>'string', 'length'=>128),
       'sub' => array('type'=>'str', 'length'=>128),
       'com' => array('type'=>'text'),
-      'password' => array('type'=>'str'),
+      'email'    => array('type'=>'str', 'length'=>255), // public option to reduce extra query
+      // this is private, not public info
+      //'password' => array('type'=>'str'),
       // 'replies' => array('type'=>'integer'),
       // 'images' => array('type'=>'integer'),
       //'archived' => array('type'=>'bool'),
@@ -205,8 +208,13 @@ function getPrivatePostsModel($boardUri) {
     //'indexes' => array('boardUri'),
     'fields' => array(
       // can just be postid...
-      'post_id' => array('type'=>'integer'),
-      'ip' => array('type'=>'str'),
+      // the internal id is aleady postid
+      // and we expect findById postid to work
+      // pg won't allow it to be postid
+      'post_id'  => array('type'=>'integer'),
+      'ip'       => array('type'=>'str'),
+      'email'    => array('type'=>'str', 'length'=>255),
+      'password' => array('type'=>'str'),
     )
   );
   $db->autoupdate($private_post_model);
@@ -268,6 +276,87 @@ function getPostFilesModel($boardUri, $options = false) {
   );
   $db->autoupdate($public_post_file_model);
   return $public_post_file_model;
+}
+
+// maybe a lib.post in the future...
+// tag and process post
+// privPost could just be passing the optional password and then we'd have to do the getIP here...
+function precreatePost($boardUri, $post, $files, $privPost) {
+
+  // tag post
+  $post['tags'] = tagPost($boardUri, $post, $files, $privPost);
+
+  // FIXME: is board locked? is thread locked?
+  global $pipelines;
+  $reply_allowed_io = array(
+    'p'        => $post,
+    'boardUri' => $boardUri,
+    'allowed'  => true,
+    'issues'   => array(),
+  );
+  $pipelines[PIPELINE_REPLY_ALLOWED]->execute($reply_allowed_io);
+
+  if (!$reply_allowed_io['allowed']) {
+    // maybe a 400 is more appropriate
+    return array(
+      'issues' => array('Reply not allowed: '.join("\n", $reply_allowed_io['issues'])),
+    );
+  }
+
+  // FIXME: make sure threadId exists...
+  $newpost_process_io = array(
+    'boardUri'     => $boardUri,
+    'p'            => $post,
+    'priv'         => $privPost,
+    'files'        => $files,
+    'addToPostsDB' => true,
+    'processFilesDB' => true,
+    'bumpThread' => true,
+    'returnId' => true,
+    'issues'   => array(),
+    'log'      => array(),
+    'createPostOptions' => array('bumpBoard' => true),
+  );
+  //$newpost_process_io['log'][] = $privPost;
+  $pipelines[PIPELINE_NEWPOST_PROCESS]->execute($newpost_process_io);
+  if ($newpost_process_io['addToPostsDB']) {
+    $post = $newpost_process_io['p']; // update post
+    $privPost = $newpost_process_io['priv']; // update privPost
+    $files = $newpost_process_io['files']; // update files
+    //$newpost_process_io['log'][] = $privPost;
+  
+    // can be an array (issues,id) if file errors
+    $data = createPost($boardUri, $post, $files, $privPost, $newpost_process_io['createPostOptions']);
+    
+    // probably should be part of createPost
+    // do we bump the thread?
+    $threadid = $post['threadid'];
+    // issues are usually file upload problems...
+    $hasId = !empty($data['id']);
+    $notSage = empty($newpost_process_io['createPostOptions']['sage']);
+    //echo "[$hasId][$threadid]bump[", $newpost_process_io['bumpThread'], "][$notSage]<br>\n";
+    if ($hasId && $threadid && $newpost_process_io['bumpThread'] && $notSage) {
+      global $db;
+      // bump thread
+      $posts_model = getPostsModel($boardUri);
+      $urow = array();
+      //echo "bumping [$threadid]<br>\n";
+      $db->update($posts_model, $urow, array('criteria'=>array(
+        array('postid', '=', $threadid),
+      )));
+    }
+    //print_r($newpost_process_io['log']);
+    //$data['log'] = $newpost_process_io['log'];
+  } else {
+    $data = array(
+      'id' => $newpost_process_io['returnId']
+    );
+    // inject issues
+    if (count($newpost_process_io['issues'])) {
+      $data['issues'] = $newpost_process_io['issues'];
+    }
+  }
+  return $data;
 }
 
 ?>
