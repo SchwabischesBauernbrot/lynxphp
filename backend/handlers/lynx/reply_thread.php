eb188dd61bfdf3834b17cc13753c60cda13eaefe
odilitime <janesmith@airmail.cc>
2024-02-29 06:20:09 +0000

    more inline with lynxchan, use precreatePost, move pass to priv, add email to priv

--- backend/handlers/lynx/reply_thread.php
+++ backend/handlers/lynx/reply_thread.php
@@ -1,6 +1,5 @@
 <?php
 
-global $db, $models, $now;
 if (!hasPostVars(array('boardUri', 'threadId'))) {
   // hasPostVars already outputs
   return; //sendResponse(array(), 400, 'Requires boardUri and threadId');
@@ -11,14 +10,12 @@ $posts_model = getPostsModel($boardUri);
 $threadid = (int)$_POST['threadId'];
 
 $post = array(
-  // captcha
-  // noFlag, email, password (we have this...), spoiler, flag
+  // noFlag, email, captcha, spoiler, flag
   'threadid' => $threadid,
   'resto' => 0,
   'name' => getOptionalPostField('name'),
   'sub'  => getOptionalPostField('subject'),
   'com'  => getOptionalPostField('message'),
-  'password' => md5(BACKEND_KEY . getOptionalPostField('password')),
   'sticky' => 0,
   'closed' => 0,
   'trip' => '', // role is not a tripcode
@@ -28,78 +25,15 @@ $post = array(
 );
 $privPost = array(
   'ip' => getip(),
+  'email' => getOptionalPostField('email'),
+  'password' => md5(BACKEND_KEY . getOptionalPostField('password')),
 );
-$files = $_POST['files'];
-
-// tag post
-$post['tags'] = tagPost($boardUri, $post, $files, $privPost);
-
-// FIXME: is board locked?
-// is thread locked?
-global $pipelines;
-$reply_allowed_io = array(
-  'p'        => $post,
-  'boardUri' => $boardUri,
-  'allowed'  => true,
-  'issues'   => array(),
-);
-$pipelines[PIPELINE_REPLY_ALLOWED]->execute($reply_allowed_io);
-
-if (!$reply_allowed_io['allowed']) {
-  // maybe a 400 is more appropriate
-  return sendResponse2(array(), array('code' => 200, 'err' => 'Reply is not allowed: '.join("\n", $issues)));
+$data = precreatePost($boardUri, $post, $_POST['files'], $privPost);
+// the only 2 lyxnchan valid responses afaik
+// need to dig into the source... (api has a source, form returns html)
+if (isset($data['id'])) {
+  echo $data['id'];
+  return;
 }
-
-// FIXME: make sure threadId exists...
-
-$newpost_process_io = array(
-  'boardUri'     => $boardUri,
-  'p'            => $post,
-  'priv'         => $privPost,
-  'files'        => $files,
-  'addToPostsDB' => true,
-  'processFilesDB' => true,
-  'bumpThread' => true,
-  'returnId' => true,
-  'issues'   => array(),
-  'createPostOptions' => array('bumpBoard' => true),
-);
-$pipelines[PIPELINE_NEWPOST_PROCESS]->execute($newpost_process_io);
-
-if ($newpost_process_io['addToPostsDB']) {
-  $post = $newpost_process_io['p']; // update post
-  $privPost = $newpost_process_io['priv']; // update privPost
-  $files = $newpost_process_io['files']; // update files
-
-  // can be an array (issues,id) if file errors
-  $data = createPost($boardUri, $post, $files, $privPost, $newpost_process_io['createPostOptions']);
-
-  $noIssues = empty($data['issues']);
-  if (!$noIssues) {
-    return sendResponse2($data);
-  }
-
-  // do we bump the thread?
-  $threadid = $post['threadid'];
-  // issues are usually file upload problems...
-  $hasId = !empty($data['id']);
-  $notSage = empty($newpost_process_io['createPostOptions']['sage']);
-  //echo "[$hasId][$threadid]bump[", $newpost_process_io['bumpThread'], "][$notSage]<br>\n";
-  if ($hasId && $threadid && $newpost_process_io['bumpThread'] && $notSage) {
-    // bump thread
-    $posts_model = getPostsModel($boardUri);
-    $urow = array();
-    //echo "bumping [$threadid]<br>\n";
-    $db->update($posts_model, $urow, array('criteria'=>array(
-      array('postid', '=', $threadid),
-    )));
-  }
-  sendResponse2($data['id']);
-} else {
-  $data = $newpost_process_io['returnId'];
-  // inject error messages
-  if (count($newpost_process_io['issues'])) {
-    $data['issues'] = $newpost_process_io['issues'];
-  }
-  sendResponse2($data);
-}
+// issue could be outputted as a string
+sendJson(json_encode($data));
