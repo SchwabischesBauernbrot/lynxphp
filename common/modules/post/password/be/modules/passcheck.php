dfa9fb505b5f8268d3018c3144a18654c4ce4683
odilitime <janesmith@airmail.cc>
2024-03-04 06:58:33 +0000

    post_priv['password'] is always set

--- common/modules/post/password/be/modules/passcheck.php
+++ common/modules/post/password/be/modules/passcheck.php
@@ -18,7 +18,9 @@ $posts_priv_model = getPrivatePostsModel($boardUri);
 global $db;
 // can't use findById with priv
 $post_priv = $db->find($posts_priv_model, array('post_id' => $io['postid']));
-if ($post_priv['password'] && $post_priv['password'] === md5(BACKEND_KEY . $io['password'])) {
+// ($post_priv['password'] will likely always be set
+// $post_priv['password'] &&
+if ($post_priv['password'] === md5(BACKEND_KEY . $io['password'])) {
   $io['allowDelete'] = true;
 }
 $io['log'][] = array('passcheck' => true, 'postid' => $io['postid'], 'post_priv' => $post_priv, 'db' => $post_priv['password'], 'passed' => $io['password'], 'salted' => md5(BACKEND_KEY . $io['password']));
