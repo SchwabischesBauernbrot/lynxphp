06c73743c274bc11be091e92a2afaca118cc6cb6
odilitime <janesmith@airmail.cc>
2024-04-26 18:25:05 +0000

    clean up

--- common/modules/thread/deleted/fe/modules/board_action.php
+++ common/modules/thread/deleted/fe/modules/board_action.php
@@ -8,29 +8,7 @@ $boardUri = $io['boardUri'];
 
 $io['actions']['bo'][] = array(
   'link' => '/' . $boardUri . '/threads/deleted', 'label' => 'Deleted Threads',
-  'includeWhere' => true,
+  //'includeWhere' => true,
 );
-
-/*
-// FIXME: promote to all later
-// add "add to favorites" link
-// FIXME: are we already in their favorites?
-// ask BE
-
-// FIXME: an EP that gets this included in the normal board data...
-$res = $pkg->useResource('is', array('boardUri' => $boardUri));
-//echo "isRes[", print_r($res, 1), "]<br>\n";
-if ($res && $res['favorite']) {
-  $io['actions']['user'][] = array(
-    'link' => '/' . $boardUri . '/unfavorite?', 'label' => 'Remove favorites',
-    'includeWhere' => true,
-  );
-} else {
-  $io['actions']['user'][] = array(
-    'link' => '/' . $boardUri . '/favorite?', 'label' => 'Add to favorites',
-    'includeWhere' => true,
-  );
-}
-*/
 
 ?>
