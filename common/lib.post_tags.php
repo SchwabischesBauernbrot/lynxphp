cc6c14241cec70b29e73bd3bcf41789a6bee95e5
odilitime <janesmith@airmail.cc>
2024-04-26 08:23:59 +0000

    tag notes

--- common/lib.post_tags.php
+++ common/lib.post_tags.php
@@ -9,6 +9,12 @@ function tagPost_getAll() {
     'tags' => array(
       'thread' => array('description' => 'post is a new thread',),
       'reply'  => array('description' => 'post is a new reply'),
+      // tags per outcome (tho we may want a reason and then map to an outcome)
+      //   tag for refusing post
+      //   tag for filtering? (or all posts filtered...)
+      // tags per post property:
+      //   for contains a banned (hash?) file
+      //   has file, has text, nofile, notext (combos?)
     ),
   );
   // maybe; the order of these matter, why?
@@ -19,6 +25,8 @@ function tagPost_getAll() {
 function tagPost($boardUri, $post, $files, $privPost) {
   global $pipelines;
   $isReply = $post['threadid'] ? true : false;
+  // it would be nice to include boardData for settings...
+  // but being in common maybe we can't rely on that?
   $io = array(
     'boardUri' => $boardUri,
     'p'        => $post,
@@ -30,7 +38,7 @@ function tagPost($boardUri, $post, $files, $privPost) {
     ),
   );
   $pipelines[PIPELINE_NEWPOST_TAG]->execute($io);
-  return $io['tags'];
+  return $io['tags'];   // move io['tags'] into p['tags']?
 }
 
 ?>
