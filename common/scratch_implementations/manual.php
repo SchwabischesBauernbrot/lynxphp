888e6e6d70c05f70be8a590d5dceba9c9b16a5cd
Odilitime <janesmith@airmail.cc>
2024-05-14 20:27:17 +0000

    lock checking, memory cache, unlock()

--- common/scratch_implementations/manual.php
+++ common/scratch_implementations/manual.php
@@ -2,6 +2,9 @@
 
 include_once 'base.php';
 
+// manual file scratch driver
+// mainly for lib.request
+
 // believe this is based off file.php driver
 
 // its slow
@@ -21,7 +24,7 @@ include_once 'base.php';
 // we read a range, total
 // nuke any that have expired...
 // only need to total the current ip and whether it's a read, write, violation
-//
+
 class manual_scratch_driver extends scratch_implementation_base_class {
   function __construct($prefix = '') {
     // we need to protect information, such as IP addresses
@@ -29,23 +32,28 @@ class manual_scratch_driver extends scratch_implementation_base_class {
     // why the manual postfix? just to remind a different driver
     $this->file = '../frontend_storage/'.$prefix.'manual.php';
     $this->lock = '../frontend_storage/'.$prefix.'manual.lock';
+
+    // always ensure the file exists
     if (!file_exists($this->file)) {
       if (!touch($this->file)) {
         echo "Can't create[", getcwd(), '/', $this->file, "] webserver can't create files or write to data?<br>\n";
         exit();
       }
+      $this->checkPerms();
     }
+    $this->changed = false;
+    // we also no longer serialize/unserialize the data
+    // because we only deal with scalars atm
+    $this->memory = false;
   }
 
-  // FIXME: file permissions
   function checkPerms() {
     $username = posix_getpwuid(posix_geteuid())['name'];
     //echo "username[$username]<br>\n";
-    if ($username !== 'www-data') {
-      // we need to fix perms
-      // FIXME: debian only
-      // was USER / USER
-      recurse_chown_chgrp($this->file, 'www-data', 'www-data');
+
+    if ($username !== USER) {
+      // we need to fix ownership perms
+      recurse_chown_chgrp($this->file, USER, USER);
     }
   }
 
@@ -69,7 +77,8 @@ class manual_scratch_driver extends scratch_implementation_base_class {
       }
     }
     */
-    // try to acquire lock
+
+    // try to acquire lock by just doing
     $stilllocked = 1;
     for($i = 0; $i < 1000; $i++) {
       if (@mkdir($lock)) {
@@ -80,128 +89,109 @@ class manual_scratch_driver extends scratch_implementation_base_class {
       usleep(100);
       # may want to return early if reach a max number of tries to get lock
     }
+    $lockfile = $lock . '/lock';
+    global $now;
     if ($stilllocked) {
-      echo "can't get lock[$lock] <!-- ", gettrace(), " --><br>\n";
-      return false;
+      // well lets do some additional investigation
+      if (file_exists($lockfile)) {
+        // read it and see if it's expired
+        $str = file_get_contents($lockfile);
+        list($then, $pid) = explode('_', $str, 2);
+        $diff = $now - $then;
+        if ($diff > 300) {
+          // stale lock move forward
+          $this->unlock();
+          // move forward properly
+          return $this->getlock();
+        } else {
+          // recent lock
+          echo "scratch_manual: can't get lock[$lock]<br>\n";
+          return false;
+        }
+      } else {
+        // maybe about to release lock or stuck
+        // maybe if this fails then return false;
+        if (!rmdir($this->lock)) {
+          echo "scratch_manual: can't remove lockdir[$lock]<br>\n";
+          return false;
+        }
+        // move forward properly
+        return $this->getlock();
+      }
     }
     // acquire lock
-    global $now;
-    file_put_contents($lock . '/lock', $now . '_' . posix_getpid());
+    file_put_contents($lockfile, $now . '_' . posix_getpid());
     // why bother checking the perms on the non-lock file?
     //$this->checkPerms();
-    if (!file_exists($lock . '/lock')) {
-      echo "cant create lock[$lock]<br>\n";
+    // we'll also check it on unlock
+    if (!file_exists($lockfile)) {
+      echo "scratch_manual: cant create lockfile[$lockfile]<br>\n";
       return false;
     }
     // could check lock to make sure we got it
+    $lines = file($this->file);
+    $this->memory = array();
+    foreach($lines as $l) {
+      $tline = trim($l);
+      list($lKey, $sValue) = explode('_:_', $tline, 2);
+      // don't deserialize here, well do when asked
+      $this->memory[$lKey] = $sValue;
+    }
+    $this->changed = false;
     return true;
   }
 
-  function waitForFileExists() {
-    if (!file_exists($this->file)) {
-      $dne = 1;
-      // up to 3sec wait
-      for($i = 0; $i < 30; $i++) {
-        if (file_exists($this->file)) {
-          return true;
-          break;
-        }
-        usleep(100);
+  // 2 functions or two return codes?
+  function unlock() {
+    if ($this->memory !== false && is_array($this->memory)) {
+      $tmpfname = tempnam('/tmp', 'doubleplus');
+      $data = '';
+      foreach($this->memory as $k => $v) {
+        $data .= $k . '_:_' . $v . "\n";
       }
+      file_put_contents($tmpfname, $data);
+      /*
+      $wfp = fopen($tmpfname, 'w');
+      if (!$wfp) {
+        return false;
+      }
+      foreach($this->memory as $k => $v) {
+        fputs($wfp, $k . '_:_' . $v . "\n");
+      }
+      fclose($wfp);
+      */
+      unlink($this->file);
+      // rename is NFS safe but can't move across parition boundaries
+      copy($tmpfname, $this->file);
+      unlink($tmpfname);
+      $this->checkPerms();
+    } // else there's an issue
+    unlink($this->lock . '/lock');
+    if (!rmdir($this->lock)) {
+      echo "scratch_manual: can't remove lockdir[$lock]<br>\n";
       return false;
     }
     return true;
   }
+
+  // non of these call getlock
+  // because we do manual locking
+  // so we can basically do a transaction to the file
 
   function clear($key) {
-    if (!$this->waitForFileExists()) {
-      return false;
-    }
-    $fp = fopen($this->file, 'r');
-    if (!$fp) {
-      return false;
-    }
-    $tmpfname = tempnam('/tmp', 'lynxphp');
-    $wfp = fopen($tmpfname, 'w');
-    if (!$wfp) {
-      return false;
-    }
-    $found = 0;
-    while(($line = fgets($fp)) !== false) {
-      $tline = trim($line);
-      list($lKey, $sValue) = explode('_:_', $tline, 2);
-      if ($lKey !== $key) {
-        fputs($wfp, $line);
-      }
-    }
-    fclose($fp);
-    fclose($wfp);
-    unlink($this->file);
-    copy($tmpfname, $this->file);
-    unlink($tmpfname);
-    $this->checkPerms();
-    return true;
+    $this->changed = true;
+    unset($this->memory[$key]);
   }
 
   function get($key) {
-    if (!$this->waitForFileExists()) {
-      return false;
-    }
-    $fp = fopen($this->file, 'r');
-    //$lines=file($this->file);
-    //foreach($lines as $line) {
-    if (!$fp) return false;
-    while(($line = fgets($fp)) !== false) {
-      $tline = trim($line);
-      list($lKey, $sValue) = explode('_:_', $tline, 2);
-      if ($lKey === $key) {
-        $data = unserialize($sValue);
-        fclose($fp);
-        return $data;
-      }
-    }
-    fclose($fp);
-    return false;
+    // we would deserialize here
+    return empty($this->memory[$key]) ? false : $this->memory[$key];
   }
 
   function set($key, $val) {
-    if (!$this->waitForFileExists()) {
-      return false;
-    }
-    $fp = fopen($this->file, 'r');
-    if (!$fp) {
-      return false;
-    }
-    $tmpfname = tempnam('/tmp', 'doubleplus');
-    $wfp = fopen($tmpfname, 'w');
-    if (!$wfp) {
-      return false;
-    }
-    $found = 0;
-    while(($line = fgets($fp)) !== false) {
-      $tline = trim($line);
-      list($lKey, $sValue) = explode('_:_', $tline, 2);
-      if ($lKey !== $key) {
-        fputs($wfp, $line);
-      } else {
-        // if key exists, replace it with new info
-        //$data = unserialize($tline);
-        fputs($wfp, $key . '_:_' . serialize($val) . "\n");
-        $found = 1;
-      }
-    }
-    fclose($fp);
-    if (!$found) {
-      // insert key at the end
-      fputs($wfp, $key . '_:_' . serialize($val) . "\n");
-    }
-    fclose($wfp);
-    unlink($this->file);
-    // NFS safe
-    copy($tmpfname, $this->file);
-    unlink($tmpfname);
-    $this->checkPerms();
-    return true;
+    $this->changed = true;
+    // we would serialize here
+    $this->memory[$key] = $val;
   }
 }
 
